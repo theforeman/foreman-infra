9d2e7e53d7962f0ddaa399c5e8b58a839ff5f5de
Sam Kottler <shk@redhat.com>
2012-10-21 22:25:17 -0400

    Moved the account definition into its own manifest in the users module

---
+++ puppet/modules/users/manifests/account.pp
@@ -0,0 +1,35 @@
+define users::account($fullname) {
+  user { $name:
+    ensure => present,
+    comment => $fullname,
+    home => "/home/$name",
+    managehome => true
+  }
+
+  file { "/home/$name":
+    ensure => directory
+  }
+
+  file { "/home/$name/.vimrc":
+    source => "puppet:///modules/users/vimrc",
+    ensure => present,
+    owner => $name,
+    group => $name,
+    require => File["/home/$name"]
+  }
+   
+  file { "/home/$name/.ssh":
+    ensure => directory,
+    owner => $name,
+    group => $name,
+    require => [ User["$name"], File["/home/$name"] ]
+  }
+
+  file { "/home/$name/.ssh/authorized_keys":
+    ensure => present,
+    source => "puppet:///modules/users/$name-authorized_keys",
+    owner => $name,
+    group => $name,
+    require => File["/home/$name/.ssh"]
+  }
+}
--- puppet/modules/users/manifests/init.pp
+++ puppet/modules/users/manifests/init.pp
@@ -1,58 +1,22 @@
 class users {
-  define ssh_user($fullname) {
-    user { $name:
-      ensure => present,
-      comment => $fullname,
-      home => "/home/$name",
-      managehome => true
-    }
-
-    file { "/home/$name":
-      ensure => directory
-    }
-
-    file { "/home/$name/.vimrc":
-      source => "puppet:///modules/users/vimrc",
-      ensure => present,
-      owner => $name,
-      group => $name,
-      require => File["/home/$name"]
-    }
-   
-    file { "/home/$name/.ssh":
-      ensure => directory,
-      owner => $name,
-      group => $name,
-      require => [ User["$name"], File["/home/$name"] ]
-    }
-
-    file { "/home/$name/.ssh/authorized_keys":
-      ensure => present,
-      source => "puppet:///modules/users/$name-authorized_keys",
-      owner => $name,
-      group => $name,
-      require => File["/home/$name/.ssh"]
-    }
-  }
-
   file { "/root/.vimrc":
     ensure => present,
     source => "puppet:///modules/users/vimrc",
   }
 
-  ssh_user { "samkottler":
+  users::account { "samkottler":
     fullname => "Sam Kottler"
   }
 
-  ssh_user { "gregsutcliffe":
+  users::account { "gregsutcliffe":
     fullname => "Greg Sutcliffe"
   }
 
-  ssh_user { "ohadlevy":
+  users::account { "ohadlevy":
     fullname => "Ohad Levy"
   }
 
-  ssh_user { "bgupta":
+  users::account { "bgupta":
     fullname => "Brian Gupta"
   }
 }
