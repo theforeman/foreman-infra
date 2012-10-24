121206bebee4671cbc04f884283b8351bc43a38d
Sam Kottler <shk@redhat.com>
2012-10-24 15:18:28 -0400

    Fixed java class to work on f17

--- puppet/modules/java/manifests/init.pp
+++ puppet/modules/java/manifests/init.pp
@@ -2,7 +2,10 @@ class java {
   package { "java":
     ensure => present,
     name => $osfamily ? {
-      RedHat => "java-1.6.0-openjdk",
+      RedHat => $operatingsystemrelease ? {
+        17 => "java-1.7.0-openjdk",
+        default => "java-1.6.0-openjdk"
+      },
       default => "openjdk-6-jdk"
     }
   }
