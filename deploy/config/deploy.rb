41c206ca2b22034e4fd4e659b97a1f67dd04fe10
Sam Kottler <shk@redhat.com>
2013-01-11 12:18:03 -0500

    Use certain SSH keys if they are available when deploying

--- deploy/config/deploy.rb
+++ deploy/config/deploy.rb
@@ -2,6 +2,12 @@ require 'capistrano_colors'
 set :stages, %w(web puppetmaster)
 require 'capistrano/ext/multistage'
 
+ssh_options[:keys] = [
+  File.join(ENV["HOME"], ".ssh", "id_rsa"),
+  File.join(ENV["HOME"], ".ssh", "web_id_rsa"),
+  File.join(ENV["HOME"], ".ssh", "deploy_id_rsa")
+]
+
 namespace :deploy do
   [:start, :stop, :restart, :finalize_update].each do |noop|
     desc "#{noop} is not needed"
