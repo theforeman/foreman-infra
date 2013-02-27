a2a64e43c7b356dbbdc331d271fa8e1ada4b0bef
Sam Kottler <shk@redhat.com>
2013-02-27 09:54:08 -0500

    Wrapped the mock configuration in a case statement so it only tries to install configs on the right OS types

--- puppet/modules/mock/manifests/init.pp
+++ puppet/modules/mock/manifests/init.pp
@@ -1,59 +1,63 @@
 class mock {
-  package { "mock":
-    ensure => present
-  }
+  case $operatingsystem {
+    'RedHat', 'CentOS', 'Fedora': {
+      package { "mock":
+        ensure => present
+      }
 
-  user { "mock":
-    ensure => present,
-    managehome => true,
-    require => Package["mock"]
-  }
+      user { "mock":
+        ensure => present,
+        managehome => true,
+        require => Package["mock"]
+      }
 
-  if(!defined(User["jenkins"])) {
-    user { "jenkins":
-      ensure => present,
-      groups => ["mock"],
-      require => Package["mock"]
+      if(!defined(User["jenkins"])) {
+        user { "jenkins":
+          ensure => present,
+          groups => ["mock"],
+          require => Package["mock"]
+        }
+      }
+
+      mock::config { "el5-i386":
+        version => "5",
+        architecture => "i386"
+      }
+
+      mock::config { "el5-x86_64":
+        version => "5",
+        architecture => "x86_64"
+      }
+
+      mock::config { "el6-i386":
+        version => "6",
+        architecture => "i386"
+      }
+
+      mock::config { "el6-x86_64":
+        version => "6",
+        architecture => "x86_64"
+      }
+
+      mock::config { "f16-i386":
+        version => "16",
+        architecture => "i386"
+      }
+
+      mock::config { "f16-x86_64":
+        version => "16",
+        architecture => "x86_64"
+      }
+
+      mock::config { "f17-i386":
+        version => "17",
+        architecture => "i386"
+      }
+
+      mock::config { "f17-x86_64":
+        version => "17",
+        architecture => "x86_64"
+       }
     }
-  }
-
-  mock::config { "el5-i386":
-    version => "5",
-    architecture => "i386"
-  }
-
-  mock::config { "el5-x86_64":
-    version => "5",
-    architecture => "x86_64"
-  }
-
-  mock::config { "el6-i386":
-    version => "6",
-    architecture => "i386"
-  }
-
-  mock::config { "el6-x86_64":
-    version => "6",
-    architecture => "x86_64"
-  }
-
-  mock::config { "f16-i386":
-    version => "16",
-    architecture => "i386"
-  }
-
-  mock::config { "f16-x86_64":
-    version => "16",
-    architecture => "x86_64"
-  }
-
-  mock::config { "f17-i386":
-    version => "17",
-    architecture => "i386"
-  }
-
-  mock::config { "f17-x86_64":
-    version => "17",
-    architecture => "x86_64"
   }
 }
