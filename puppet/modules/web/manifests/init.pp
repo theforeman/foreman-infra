05e5ed1b00908da9e1b83e01ef52621c143c4649
Dominic Cleal <dcleal@redhat.com>
2013-03-06 12:27:07 +0000

    Change theforeman.org vhost to point to cap site deployment

--- puppet/modules/web/manifests/init.pp
+++ puppet/modules/web/manifests/init.pp
@@ -1,6 +1,12 @@
 class web {
+  file { "/var/www/vhosts/web_theforeman.org":
+    ensure => link,
+    target => "/var/www/cap/theforeman.org/current/_site/",
+  }
+
   apache::vhost { "web":
-    ensure => present,
-    config_file => "puppet:///modules/web/web.conf"
+    ensure      => present,
+    config_file => "puppet:///modules/web/web.conf",
+    require     => File["/var/www/vhosts/web_theforeman.org"],
   }
 }
