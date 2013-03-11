2cbb8779f714d11eef5fb792b380ddae74fcba66
Greg Sutcliffe <gsutclif@redhat.com>
2013-03-11 15:11:33 +0000

    Use a comma, not a space, for multiple ssh-key options

--- puppet/modules/freight2/manifests/user.pp
+++ puppet/modules/freight2/manifests/user.pp
@@ -33,7 +33,7 @@ class freight2::user (
     owner   => $user,
     group   => $user,
     mode    => 0644,
-    content => "from=\"5.9.188.104\" command=\"${home}/bin/freight_rsync\" ssh-rsa ${pub_key} freight_key\n",
+    content => "from=\"5.9.188.104\",command=\"${home}/bin/freight_rsync\" ssh-rsa ${pub_key} freight_key\n",
   }
 
   # Create validation script for rsync connections only
