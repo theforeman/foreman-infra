48eac2471c787f5c0dea96d724e83345825fb4a7
Greg Sutcliffe <gsutclif@redhat.com>
2013-04-16 16:42:46 +0100

    Define logdir for munin-node

--- puppet/modules/munin/manifests/client.pp
+++ puppet/modules/munin/manifests/client.pp
@@ -3,21 +3,26 @@ class munin::client {
     ensure => present
   }
 
-  service { 'munin-node':
-    enable => true,
-    ensure => running,
-    require => Package['munin-node']
+  file { '/var/log/munin-node':
+   ensure => directory
   }
 
   file { '/etc/munin/munin-node.conf':
     ensure => present,
     source => "puppet:///modules/munin/munin-node.conf",
     notify => Service['munin-node'],
-    require => Package['munin-node']
+    require => [Package['munin-node'],File['/var/log/munin-node']],
   }
 
   @@file { "/etc/munin/conf.d/$hostname":
      content => template("munin/munin-node-exported.cfg.erb"),
      tag => "munin",
   }
+
+  service { 'munin-node':
+    enable => true,
+    ensure => running,
+    require => Package['munin-node']
+  }
+
 }
