aa66f1c68ee6781b0747edf60b9ae65c383320ae
Sam Kottler <shk@redhat.com>
2013-04-16 11:31:23 -0400

    Just use the name variable

--- puppet/modules/nagios/manifests/server.pp
+++ puppet/modules/nagios/manifests/server.pp
@@ -12,18 +12,16 @@ class nagios::server {
   }
 
   define monitored_slave {
-    $address = $name
-
-    nagios_host { $address:
+    nagios_host { $name:
       ensure => present,
-      address => $address,
+      address => $name,
       use => "generic-host",
     }
 
-    nagios_service { "$address_check_ping":
+    nagios_service { "${name}_check_ping":
       check_command => "check_ping!100.0,20%!500.0,60%",
       use => "generic-service",
-      host_name => $address,
+      host_name => $name,
       notification_period => "24x7",
       service_description => "Check Ping"
     }
