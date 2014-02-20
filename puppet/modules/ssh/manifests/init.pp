65292d8097d4e1e861de512411c4b3cd78bacd23
Dominic Cleal <dcleal@redhat.com>
2014-02-20 16:06:05 +0000

    Tighten some sshd_config settings

--- puppet/modules/ssh/manifests/init.pp
+++ puppet/modules/ssh/manifests/init.pp
@@ -31,4 +31,27 @@ class ssh {
     ensure => running,
     enable => true
   }
+
+  Sshd_config <| |> ~> Service[$ssh_service]
+
+  sshd_config { 'PermitRootLogin':
+    ensure => present,
+    value  => 'without-password',
+  }
+
+  sshd_config { 'PasswordAuthentication':
+    ensure => present,
+    value  => 'no',
+  }
+
+  sshd_config { 'StrictModes':
+    ensure => present,
+    value  => 'yes',
+  }
+
+  # Log SSH key fingerprints
+  sshd_config { 'LogLevel':
+    ensure => present,
+    value  => 'VERBOSE',
+  }
 }
