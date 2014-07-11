5ed0e3067cd5fe71c21cf3457392241c0874a083
Greg Sutcliffe <greg.sutcliffe@gmail.com>
2014-07-11 11:07:53 +0100

    Refactor freight to user secure_rsync

---
+++ puppet/modules/freight/manifests/uploader.pp
@@ -0,0 +1,12 @@
+# Cheap class to deploy an SSH private key for use in contacting the
+# freight server to upload deb packages for signing
+#
+class freight::uploader {
+
+  secure_rsync::uploader_key { 'freight':
+    user       => 'jenkins',
+    dir        => '/var/lib/workspace/workspace',
+    manage_dir => true,
+  }
+
+}
--- puppet/modules/freight/manifests/user.pp
+++ puppet/modules/freight/manifests/user.pp
@@ -4,96 +4,46 @@ define freight::user (
   $webdir       = "${home}/web",
   $stagedir     = "${home}/staged",
   $vhost        = 'deb',
-  $cron_matches,
+  $cron_matches = 'all',
 ) {
-  include rsync::server
 
-  # Disable password, we want this to be keys only
-  user { $user:
-    ensure     => present,
-    home       => $home,
-    managehome => true,
-    password   => '!',
-  }
-
-  file { $home:
-    ensure => directory,
-    owner  => $user,
-    group  => $user,
-    mode   => 0755,
-  }
-
-  file { "${home}/.ssh":
-    ensure => directory,
-    owner  => $user,
-    group  => $user,
-    mode   => 0700,
+  secure_rsync::receiver_setup { 'web':
+    user           => $user,
+    foreman_search => 'host.hostgroup = Debian and name = ipaddress',
+    script_content => template('freight/rsync.erb'),
   }
 
   file { "${home}/freight.conf":
     ensure  => present,
-    mode    => 644,
+    mode    => '0644',
     content => template('freight/freight.conf.erb'),
     require => Package['freight'],
   }
 
   # $webdir should be created too, but since we normally override to point at
   # the vhost, we'll get a duplicate definition
-  file { [$stagedir, "${home}/rsync_cache"]:
+  file { $stagedir:
     ensure => directory,
     owner  => $user,
     group  => $user,
-  }
-
-  # Read the freight key from the puppetmaster
-  $pub_key  = ssh_keygen({name => 'freight_key', public => 'public'})
-
-  # Get the IPs of the Debian slaves from foreman
-  $ip_data=foreman({
-    'item'         => 'fact_values',
-    'search'       => 'host.hostgroup = Debian and name = ipaddress',
-    'foreman_user' => $::foreman_api_user,
-    'foreman_pass' => $::foreman_api_password,
-  })
-
-  file { "${home}/.ssh/authorized_keys":
-    ensure  => present,
-    owner   => $user,
-    group   => $user,
-    mode    => 0644,
-    content => template('freight/keys.erb'),
-  }
-
-  # Create validation script for rsync connections only
-  file { "${home}/bin":
-    ensure => directory,
-    owner  => $user,
-    group  => $user,
-    mode   => 0755,
-  }
-
-  file { "${home}/bin/freight_rsync":
-    ensure  => present,
-    owner   => $user,
-    group   => $user,
-    mode    => 0755,
-    content => template('freight/rsync.erb'),
   }
 
   # Cleanup old stuff
   file { "/etc/cron.daily/${user}":
-    mode    => 755,
+    mode    => '0755',
     content => template('freight/cron.erb'),
   }
 
+  # Website resources
   apache::vhost { $vhost:
     ensure         => present,
-    config_content => template("freight/vhost.erb"),
+    config_content => template('freight/vhost.erb'),
     user           => $user,
     group          => $user,
     mode           => 0755,
   }
 
+  include rsync::server
   rsync::server::module { $vhost:
     path      => $webdir,
     list      => true,
@@ -107,7 +57,7 @@ define freight::user (
     ensure => present,
     owner  => 'root',
     group  => 'root',
-    mode   => 0644,
+    mode   => '0644',
     source => "puppet:///modules/freight/${vhost}-HEADER.html",
   }
   file { "${webdir}/foreman.asc":
