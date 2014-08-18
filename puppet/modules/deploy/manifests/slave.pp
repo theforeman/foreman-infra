64465a6905833c2980a6b23b94aa978c3aa9869a
Dominic Cleal <dcleal@redhat.com>
2014-08-18 19:16:49 +0100

    Move deploy to use secure_ssh

--- puppet/modules/deploy/manifests/slave.pp
+++ puppet/modules/deploy/manifests/slave.pp
@@ -1,26 +1,6 @@
 class deploy::slave {
-  # Adapted from secure_rsync for a pure ssh solution
-  $user = 'jenkins'
-  $dir  = '/var/lib/workspace/workspace/deploy_key'
-
-  $pub_key  = ssh_keygen({name => 'deploy_key', public => 'public'})
-  $priv_key = ssh_keygen({name => 'deploy_key'})
-
-  file { $dir:
-    ensure => directory,
-    owner  => $user,
-    mode   => '0700',
-  }
-
-  file { "${dir}/deploy_key":
-    owner   => $user,
-    mode    => '0400',
-    content => $priv_key,
-  }
-
-  file { "${dir}/deploy_key.pub":
-    owner   => $user,
-    mode    => '0644',
-    content => "ssh-rsa ${pub_key} deploy_key from puppetmaster\n",
+  secure_ssh::uploader_key { 'deploy':
+    user => 'jenkins',
+    dir  => '/var/lib/workspace/workspace/deploy_key',
   }
 }
