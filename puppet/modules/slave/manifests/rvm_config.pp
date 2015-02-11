0524c712bfaf9624cd35afd7fb4cc9fc4dab5ae6
Dominic Cleal <dcleal@redhat.com>
2015-02-11 12:05:51 +0000

    Manage and update rubygems version in rvm environments

--- puppet/modules/slave/manifests/rvm_config.pp
+++ puppet/modules/slave/manifests/rvm_config.pp
@@ -1,13 +1,20 @@
-define slave::rvm_config($version) {
+define slave::rvm_config($version, $rubygems_version = '2.4.5') {
   $alias = $title
 
   rvm_system_ruby { $version:
     ensure => present,
-  }
-
+  } ->
   rvm_alias { $alias:
     ensure      => present,
     target_ruby => $version,
-    require     => Rvm_system_ruby[$version],
+  } ->
+  rvm_gem { "${version}/rubygems-update":
+    ensure => $rubygems_version,
+  } ~>
+  exec { "${version}/update_rubygems":
+    command  => "rvm ${version} do update_rubygems",
+    unless   => "test `rvm ${version} do gem -v` = ${rubygems_version}",
+    path     => '/usr/local/rvm/bin:/usr/bin:/bin',
+    provider => 'shell',
   }
 }
