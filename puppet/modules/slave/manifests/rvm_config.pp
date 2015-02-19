4f01beed12c69b76cf2f4c9883ae12d0947f1374
Dominic Cleal <dcleal@redhat.com>
2015-02-19 11:43:35 +0000

    Use rvm's built in rubygems support, works in latest version
    Somehow it was updating rubygems outside of the rvm environment, breaking the
    system Ruby.

--- puppet/modules/slave/manifests/rvm_config.pp
+++ puppet/modules/slave/manifests/rvm_config.pp
@@ -8,11 +8,8 @@ define slave::rvm_config($version, $rubygems_version = '2.4.5') {
     ensure      => present,
     target_ruby => $version,
   } ->
-  rvm_gem { "${version}/rubygems-update":
-    ensure => $rubygems_version,
-  } ~>
   exec { "${version}/update_rubygems":
-    command  => "rvm ${version} do update_rubygems",
+    command  => "rvm ${version} rubygems ${rubygems_version}",
     unless   => "test `rvm ${version} do gem -v` = ${rubygems_version}",
     path     => '/usr/local/rvm/bin:/usr/bin:/bin',
     provider => 'shell',
