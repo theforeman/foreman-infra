c1cade0f13795b723cfa486beefafae5c8d0df58
Dominic Cleal <dcleal@redhat.com>
2015-03-24 15:57:11 +0000

    Remove duplicate gemset_cleanup script

--- puppet/modules/jenkins_job_builder/files/config/scripts/gemset_cleanup.sh
+++ puppet/modules/jenkins_job_builder/files/config/scripts/gemset_cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ruby=2.0.0
+[ -z "$ruby" ] && ruby=2.0.0
 # Clean gemset and database
 . /etc/profile.d/rvm.sh
 gemset=$(echo ${JOB_NAME} | cut -d/ -f1)-${EXECUTOR_NUMBER}
--- puppet/modules/jenkins_job_builder/files/config/scripts/gemset_cleanup_proxy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-# Clean RVM Ruby environment
-. /etc/profile.d/rvm.sh
-gemset=$(echo ${JOB_NAME} | cut -d/ -f1)-${EXECUTOR_NUMBER}
-rvm use ruby-${ruby}
-rvm gemset delete ${gemset} --force
