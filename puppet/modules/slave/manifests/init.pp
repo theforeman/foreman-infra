f3c57b7566290582bbea4fcbfc7b844a7b3be145
Dominic Cleal <dcleal@redhat.com>
2015-09-24 13:43:55 +0100

    Only install older Rubies under supported archs

--- puppet/modules/slave/manifests/init.pp
+++ puppet/modules/slave/manifests/init.pp
@@ -213,17 +213,20 @@ class slave($github_user = undef,
   }
   if $rvm_installed == "true" {
     rvm::system_user { 'jenkins': }
-    slave::rvm_config { 'ruby-1.8.7':
-      version => 'ruby-1.8.7-p371',
-    }
-    slave::rvm_config { 'ruby-1.9.2':
-      version => 'ruby-1.9.2-p320',
-    }
-    slave::rvm_config { 'ruby-1.9.3':
-      version => 'ruby-1.9.3-p392',
-    }
-    slave::rvm_config { 'ruby-2.0.0':
-      version => 'ruby-2.0.0-p353',
+
+    if $architecture == 'x86_64' or $architecture == 'amd64' {
+      slave::rvm_config { 'ruby-1.8.7':
+        version => 'ruby-1.8.7-p371',
+      }
+      slave::rvm_config { 'ruby-1.9.2':
+        version => 'ruby-1.9.2-p320',
+      }
+      slave::rvm_config { 'ruby-1.9.3':
+        version => 'ruby-1.9.3-p392',
+      }
+      slave::rvm_config { 'ruby-2.0.0':
+        version => 'ruby-2.0.0-p353',
+      }
     }
     slave::rvm_config { 'ruby-2.1':
       version => 'ruby-2.1.5',
