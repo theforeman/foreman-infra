de27d18bf6b60eaa9c59332502d6307e099008dd
Eric D. Helms <ericdhelms@gmail.com>
2015-09-24 20:07:42 +0100

    Add test_develop configuration

---
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/scripts/test/test_develop.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -ex
+
+APP_ROOT=`pwd`
+
+# setup basic settings file
+sed -e 's/:locations_enabled: false/:locations_enabled: true/' $APP_ROOT/config/settings.yaml.example > $APP_ROOT/config/settings.yaml
+sed -i 's/:organizations_enabled: false/:organizations_enabled: true/' $APP_ROOT/config/settings.yaml
+
+# RVM Ruby environment
+. /etc/profile.d/rvm.sh
+# Use a gemset unique to each executor to enable parallel builds
+gemset=$(echo ${JOB_NAME} | cut -d/ -f1)-${EXECUTOR_NUMBER}
+rvm use ruby-${ruby}@${gemset} --create
+rvm gemset empty --force
+#gem update --no-ri --no-rdoc
+gem install bundler --no-ri --no-rdoc
+
+# Retry as rubygems (being external to us) can be intermittent
+while ! bundle install --without=development -j 5; do
+  (( c += 1 ))
+  if [ $c -ge 5 ]; then
+    echo "bundle install continually failed" >&2
+    exit 1
+  fi
+done
+
+# Database environment
+(
+  sed "s/^test:/development:/; s/database:.*/database: ${gemset}-dev/" $HOME/${database}.db.yaml
+  echo
+  sed "s/database:.*/database: ${gemset}-test/" $HOME/${database}.db.yaml
+) > $APP_ROOT/config/database.yml
+
+# Create DB first in development as migrate behaviour can change
+bundle exec rake db:drop db:create db:migrate
+
+tasks="pkg:generate_source jenkins:unit"
+[ ${database} = postgresql ] && tasks="$tasks jenkins:integration"
+bundle exec rake $tasks TESTOPTS="-v"
