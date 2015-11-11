a33644ab45d8684eb9ec3180a32cbd33a8a15611
Dominic Cleal <dcleal@redhat.com>
2015-11-11 15:31:13 +0000

    Require Java 7 on slaves for Jenkins 1.612+

--- puppet/modules/java/manifests/init.pp
+++ puppet/modules/java/manifests/init.pp
@@ -2,11 +2,8 @@ class java {
   package { "java":
     ensure => present,
     name => $osfamily ? {
-      RedHat => $operatingsystemrelease ? {
-        17 => "java-1.7.0-openjdk",
-        default => "java-1.6.0-openjdk"
-      },
-      default => "openjdk-6-jdk"
+      RedHat => "java-1.7.0-openjdk",
+      default => "openjdk-7-jdk",
     }
   }
 }
