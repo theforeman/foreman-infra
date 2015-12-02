2642c0bef20fce8441b6bb5f4bd038832db8acff
Dominic Cleal <dcleal@redhat.com>
2015-12-02 09:58:56 +0000

    Verify PR scanner JSON config files

---
+++ .travis.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -xe
+
+# Test JJB configs
+for d in puppet/modules/jenkins_job_builder/files/*; do
+  cd $d
+  jenkins-jobs -l debug test -r .
+  cd -
+done
+
+# Test PR scanner configs
+for f in puppet/modules/slave/templates/*.json.erb; do
+  echo $f
+  json_verify < $f
+done
+
+cd puppet
+rake syntax
