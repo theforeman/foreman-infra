f1f1c37ba14dbf4ec9ece67383d4800c88e5a9ab
Eric D. Helms <ericdhelms@gmail.com>
2015-12-08 16:14:24 -0500

    Add test_hammer_cli_foreman job configuration

---
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/scripts/test/test_hammer_cli_foreman.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -ex
+
+# RVM Ruby environment
+. /etc/profile.d/rvm.sh
+# Use a gemset unique to each executor to enable parallel builds
+gemset=$(echo ${JOB_NAME} | cut -d/ -f1)-${EXECUTOR_NUMBER}
+rvm use ruby-${ruby}@${gemset} --create
+rvm gemset empty --force
+gem update --no-ri --no-rdoc
+gem install bundler --no-ri --no-rdoc
+
+# Link hammer_cli from github
+echo 'gem "hammer_cli", :github => "theforeman/hammer-cli"' > Gemfile.local
+
+# Retry as rubygems (being external to us) can be intermittent
+while ! bundle install --without=development; do
+  (( c += 1 ))
+  if [ $c -ge 5 ]; then
+    echo "bundle install continually failed" >&2
+    exit 1
+  fi
+done
+
+bundle exec rake pkg:generate_source ci:setup:minitest test TESTOPTS="-v"
