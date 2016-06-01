009cfae71d00d9b92f55ec5f5bef3015a3487d99
Eric D Helms <ericdhelms@gmail.com>
2016-06-01 14:04:27 -0400

    Fix data_dir detection for katello site rsync

--- puppet/modules/jenkins_job_builder/files/theforeman.org/scripts/deploy_katello_site.sh
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/scripts/deploy_katello_site.sh
@@ -18,5 +18,6 @@ git reset origin/deploy --hard
 
 ./deploy.rb
 
-data_dir=$(ssh 53fe3c0b5973ca67f1000266@site-katelloproject.rhcloud.com 'echo $OPENSHIFT_DATA_DIR')
-rsync --delete-after -avzhe 'ssh -i /var/lib/workspace/workspace/deploy_katello_repos_key/deploy_katello_repos_key' public/ 53fe3c0b5973ca67f1000266@site-katelloproject.rhcloud.com/:/$data_dir
+data_dir=$(ssh -i '/var/lib/workspace/workspace/deploy_katello_repos_key/deploy_katello_repos_key' 53fe3c0b5973ca67f1000266@site-katelloproject.rhcloud.com 'echo $OPENSHIFT_DATA_DIR')
+rsync --delete-after -rvzhe 'ssh -i /var/lib/workspace/workspace/deploy_katello_repos_key/deploy_katello_repos_key' public/ 53fe3c0b5973ca67f1000266@site-katelloproject.rhcloud.com:/$data_dir/public
+
