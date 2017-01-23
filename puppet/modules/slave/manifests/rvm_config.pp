ce4f261031c27a56fa217011654fe7b2ef3f6f04
Dominic Cleal <dominic@cleal.org>
2017-01-23 09:53:08 +0000

    Update rubygems to 2.6.9

--- puppet/modules/slave/manifests/rvm_config.pp
+++ puppet/modules/slave/manifests/rvm_config.pp
@@ -1,4 +1,4 @@
-define slave::rvm_config($version, $rubygems_version = '2.5.2') {
+define slave::rvm_config($version, $rubygems_version = '2.6.9') {
   $alias = $title
 
   rvm_system_ruby { $version:
