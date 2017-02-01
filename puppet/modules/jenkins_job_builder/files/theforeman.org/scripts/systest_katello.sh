d9e2aa1755e99adc24d1b734dca1288a26480061
Eric D Helms <ericdhelms@gmail.com>
2017-02-01 09:52:58 -0500

    Update Katello systest for new bats file locations

--- puppet/modules/jenkins_job_builder/files/theforeman.org/scripts/systest_katello.sh
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/scripts/systest_katello.sh
@@ -8,14 +8,15 @@ export VAGRANT_DEFAULT_PROVIDER=rackspace
 
 trap "vagrant destroy $os_vagrant" EXIT ERR
 
-vagrant up $os_vagrant
+vagrant up $os_vagrant || true
 
 [ -e debug ] && rm -rf debug/
 mkdir debug
 vagrant ssh-config $os_vagrant > ssh_config
+scp -F ssh_config $os_vagrant:/root/bats_results/*.tap debug/ || true
 scp -F ssh_config $os_vagrant:/root/last_logs debug/ || true
 scp -F ssh_config $os_vagrant:/root/sosreport* debug/ || true
-scp -F ssh_config $os_vagrant:/root/foreman-debug* debug/ || true
+scp -F ssh_config $os_vagrant:/root/foreman-debug.tar.xz debug/
 scp -F ssh_config $os_vagrant:/var/log/foreman-installer/katello.log* debug/ || true
 
 exit 0
