7c649989f98b3dc931dd017b608e827b918b05c9
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2017-06-27 23:44:02 +0200

    Use the new freight repositories on Debian

--- puppet/modules/freight/manifests/install.pp
+++ puppet/modules/freight/manifests/install.pp
@@ -2,11 +2,11 @@ class freight::install {
 
   if $::osfamily == 'Debian' {
     apt::source { 'freight':
-      location    => 'http://packages.rcrowley.org',
+      location    => 'http://build.openvpn.net/debian/freight_team',
       release     => $::lsbdistcodename,
       repos       => 'main',
-      key         => '7DF49CEF',
-      key_source  => 'http://packages.rcrowley.org/keyring.gpg',
+      key         => '30EBF4E73CCE63EEE124DD278E6DA8B4E158C569',
+      key_source  => 'https://swupdate.openvpn.net/repos/repo-public.gpg',
       include_src => false,
     }->
     package { 'freight':
