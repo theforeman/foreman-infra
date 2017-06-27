888ef6091d54d1d25c01102e222c8b32d962baed
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2017-06-27 20:46:02 +0200

    Fix freight repository handling

--- puppet/modules/freight/manifests/install.pp
+++ puppet/modules/freight/manifests/install.pp
@@ -3,7 +3,7 @@ class freight::install {
   if $::operatingsystem == 'Debian' {
     apt::source { 'freight':
       location    => 'http://packages.rcrowley.org',
-      release     => 'wheezy',
+      release     => $::lsbdistcodename,
       repos       => 'main',
       key         => '7DF49CEF',
       key_source  => 'http://packages.rcrowley.org/keyring.gpg',
@@ -13,12 +13,11 @@ class freight::install {
       ensure => installed,
     }
   } else {
+    # There used to be a copr for this but it's in EPEL now
     yumrepo { 'freight':
-      descr    => 'Freight',
-      baseurl  => 'http://copr-be.cloud.fedoraproject.org/results/domcleal/freight/epel-6-$basearch/',
-      gpgcheck => '0',
-      enabled  => '1',
-    } ->
+      ensure => absent,
+    }
+
     package { 'freight':
       ensure => 'latest',
     }
