8e03f97df8f4d4abe5894ebf3f1603b3a9b19401
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2017-09-25 21:34:52 +0200

    Allow ensuring users are absent

--- puppet/modules/users/manifests/account.pp
+++ puppet/modules/users/manifests/account.pp
@@ -1,11 +1,12 @@
 define users::account(
+  $ensure = 'present',
   $fullname = undef,
   $passwd = undef,
   $homedir = "/home/${title}",
   $sudo = "ALL=(ALL) ALL",
 ) {
   user { $name:
-    ensure     => present,
+    ensure     => $ensure,
     comment    => $fullname,
     home       => $homedir,
     managehome => true,
@@ -13,29 +14,32 @@ define users::account(
     password   => $passwd,
   }
 
-  file { $homedir:
-    ensure => directory,
-    owner  => $name,
-    group  => $name,
-    mode   => '0755',
-  }
+  if $ensure == 'present' {
+    file { $homedir:
+      ensure => directory,
+      owner  => $name,
+      group  => $name,
+      mode   => '0755',
+    }
 
-  file { "${homedir}/.ssh":
-    ensure => directory,
-    owner  => $name,
-    group  => $name,
-    mode   => '0700',
-  }
+    file { "${homedir}/.ssh":
+      ensure => directory,
+      owner  => $name,
+      group  => $name,
+      mode   => '0700',
+    }
 
-  file { "${homedir}/.ssh/authorized_keys":
-    ensure  => file,
-    content => file("${module_name}/${name}-authorized_keys"),
-    owner   => $name,
-    group   => $name,
-    mode    => '0600',
+    file { "${homedir}/.ssh/authorized_keys":
+      ensure  => file,
+      content => file("${module_name}/${name}-authorized_keys"),
+      owner   => $name,
+      group   => $name,
+      mode    => '0600',
+    }
   }
 
   sudo::conf { "sudo-puppet-${name}":
+    ensure  => $ensure,
     content => "${name} ${sudo}",
   }
 
