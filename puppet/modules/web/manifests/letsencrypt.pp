a226a1859d082b09df404530f3ffab52ff9b0316
Ewoud Kohl van Wijngaarden <github@kohlvanwijngaarden.nl>
2017-10-14 16:04:06 +0200

    Fix letsencrypt cron for VCS installs

--- puppet/modules/web/manifests/letsencrypt.pp
+++ puppet/modules/web/manifests/letsencrypt.pp
@@ -7,7 +7,7 @@ class web::letsencrypt(
   }
 
   cron { 'letsencrypt_renew':
-    command => '/usr/bin/certbot renew --renew-hook "/sbin/service httpd reload"',
+    command => "${::letsencrypt::command} renew --renew-hook '/sbin/service httpd reload'",
     user    => 'root',
     weekday => '6',
     hour    => '3',
