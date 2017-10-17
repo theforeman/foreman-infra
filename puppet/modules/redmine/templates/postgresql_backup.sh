40a54d44d8e7223c111b601ae350df6f191b1839
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2017-10-17 09:38:17 +0200

    Fix redmine backup script
    This was still dumping the database to the old location. It also adds
    the .erb extension and introduces the DBNAME variable

--- puppet/modules/redmine/templates/postgresql_backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Backs up the OpenShift PostgreSQL database for this application
- 
-FILENAME="/usr/share/redmine_data/redmine.backup.sql.gz"
-
-cd /tmp
-sudo -u postgres pg_dump <%= @db_name %> | gzip > $FILENAME
