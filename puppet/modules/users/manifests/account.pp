7c4e69995f9207916d25109e79a3fd82d88cabcd
Greg Sutcliffe <greg.Sutcliffe@gmail.com>
2017-11-10 13:48:14 +0000

    Enable cleanup of old user accounts

--- puppet/modules/users/manifests/account.pp
+++ puppet/modules/users/manifests/account.pp
@@ -36,6 +36,11 @@ define users::account(
       group   => $name,
       mode    => '0600',
     }
+  } elsif $ensure == 'absent' {
+    # Allow to revoke users access for cleanup
+    file { "${homedir}/.ssh/authorized_keys":
+      ensure  => absent,
+    }
   }
 
   sudo::conf { "sudo-puppet-${name}":
