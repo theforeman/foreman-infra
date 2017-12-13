b9945e036ca9cf2950d67181508e4b6ed2274cf6
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2017-12-13 17:22:12 +0100

    Rewrite systests to forklift

--- puppet/modules/jenkins_job_builder/files/theforeman.org/scripts/systest_foreman.sh
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/scripts/systest_foreman.sh
@@ -1,105 +1,56 @@
 #!/bin/bash -xe
 
-rm -f *.bats.out || true
-
-if [ ${os} = f19 -o ${os} = f21 -o ${os} = f24 ]; then
-  # https://github.com/mitchellh/vagrant-rackspace/issues/132
-  echo "skipping f19/f21/f24, no Rackspace image"
-  echo "1..0 # Skipped: no Rackspace image for f19/f21/f24" > skipped.bats.out
-  exit 0
+if [[ -z $os ]] ; then
+	echo "Operating system not set"
+	exit 1
 fi
 
-if [ ${os#f} != $os ]; then
-  [ $repo != nightly ] && FOREMAN_REPO=releases/$repo
-  args="FOREMAN_REPO=$FOREMAN_REPO"
-  [ $staging = true ] && args="FOREMAN_CUSTOM_URL=http://koji.katello.org/releases/yum/foreman-${repo}/Fedora/${os#f}/x86_64/ $args"
-elif [ ${os#el} != $os ]; then
-  [ $repo != nightly ] && FOREMAN_REPO=releases/$repo
-  args="FOREMAN_REPO=$FOREMAN_REPO"
-  if [ $staging = true ]; then
-    foreman_url="FOREMAN_CUSTOM_URL=http://koji.katello.org/releases/yum/foreman-${repo}/RHEL/${os#el}/x86_64/"
-    foreman_plugins_url="FOREMAN_CUSTOM_PLUGINS_URL=http://koji.katello.org/releases/yum/foreman-plugins-${repo}/RHEL/${os#el}/x86_64/"
-    args="$foreman_url $foreman_plugins_url $args"
-  fi
-else
-  if [ $staging = true ]; then
-    args="FOREMAN_CUSTOM_URL=http://stagingdeb.theforeman.org/ FOREMAN_REPO=theforeman-${repo}"
-  else
-    args="FOREMAN_REPO=${repo}"
-  fi
+box_name="systest-foreman-${os}${BUILD_NUMBER:+-$BUILD_NUMBER}"
+filename="boxes.d/80-tmp-${box_name}.yaml"
+
+args=""
+
+if [[ $run_hammer_tests == true ]]; then
+  args+=" --foreman-organizations-enabled=true --foreman-locations-enabled=true --foreman-log-level=debug"
 fi
 
-if [ $run_hammer_tests = true ]; then
-  args="FOREMAN_USE_ORGANIZATIONS=true FOREMAN_USE_LOCATIONS=true $args"
+if [[ -n "${db_type}" ]]; then
+  args+=" --foreman-db-type=${db_type}"
 fi
 
-if [ -n "${db_type}" ]; then
-  args="FOREMAN_DB_TYPE=${db_type} $args"
-fi
-
-if [ -n "${expected_version}" ]; then
-  args="FOREMAN_EXPECTED_VERSION=${expected_version} $args"
-fi
-
-if [ $pl_puppet = auto ]; then
-  pl_puppet=pc1
-fi
-
-# no puppet server for Stretch in PC1 repo, use Puppet5 instead
-if [ $os = stretch ]; then
-  pl_puppet=puppet5
-fi
+cat > "$filename" <<-EOF
+${box_name}:
+  box: ${os}
+  ansible:
+    playbook: 'playbooks/bats_pipeline_foreman_nightly.yml'
+    group: 'bats'
+    variables:
+      bats_environment:
+      - FOREMAN_EXPECTED_VERSION: ${expected_version}
+      ${args:+foreman_installer_options:${args}}
+      ${umask:+umask_mode: ${umask}}
+      ${repo:+foreman_repositories_version: ${repo}}
+      ${repo_environment:+foreman_repositories_environment: ${repo_environment}}
+      ${pl_puppet:+puppet_repositories_version: ${pl_puppet}}
+      ${run_hammer_tests:+foreman_testing_hammer_tests: ${run_hammer_tests}}
+EOF
 
 export VAGRANT_DEFAULT_PROVIDER=rackspace
 
-trap "vagrant destroy" EXIT ERR
+trap "vagrant destroy $box_name" EXIT ERR
 
-PUPPET_REPO=${pl_puppet} vagrant up $os
-
-# Workaround Rackspace image issue installing tzdata-java, as tzdata is pre-installed from
-# jessie-updates and the two versions don't match. Can be removed after Debian 8.6 release.
-if [ $os = jessie ]; then
-  echo "echo 'deb http://httpredir.debian.org/debian jessie-updates main' >> /etc/apt/sources.list" | vagrant ssh $os
-  echo "apt-get update" | vagrant ssh $os
-fi
-
-if [ -n "$umask" ]; then
-  echo fb-setup-umask.bats | vagrant ssh $os | tee fb-setup-umask.bats.out
-fi
-
-if [ $pl_puppet != false ]; then
-  echo PUPPET_REPO=${pl_puppet} fb-install-plpuppet.bats | vagrant ssh $os | tee fb-install-plpuppet.bats.out
-fi
-
-if [ $os = el7 ]; then
-  args="INSTALLER_OPTIONS=-s $args"
-fi
-
-echo ${args} fb-install-foreman.bats | vagrant ssh $os | tee fb-install-foreman.bats.out
-
-if [ $run_puppet_tests = true ]; then
-  echo PUPPET_REPO=${pl_puppet} fb-puppet-tests.bats | vagrant ssh $os | tee fb-puppet-tests.bats.out
-fi
-
-if [ $run_hammer_tests = true ]; then
-  if [ "x$hammer_deps" != "x" ]; then
-    echo "cd ~/ && wget https://gist.githubusercontent.com/tstrachota/52b86d7ccf835a11dc99/raw/install_gems.sh" | vagrant ssh $os
-    hammer_deps=$(echo -e $hammer_deps | tr "\n" " ")
-    echo "Hammer deps: ${hammer_deps}"
-    echo "bash ~/install_gems.sh ${hammer_deps}" | vagrant ssh $os
-  fi
-
-  hammer_test_args="HAMMER_TEST_BRANCH=${hammer_tests_branch}"
-  hammer_test_args="HAMMER_TEST_REPO=https://github.com/${hammer_tests_owner}/hammer-tests.git ${hammer_test_args}"
-  echo $hammer_test_args fb-hammer-tests.bats | vagrant ssh $os | tee fb-hammer-tests.bats.out
-fi
+vagrant up $box_name || true
 
 [ -e debug ] && rm -rf debug/
 mkdir debug
-vagrant ssh-config $os > ssh_config
-scp -F ssh_config ${os}:/root/last_logs debug/ || true
-scp -F ssh_config ${os}:/root/sosreport* debug/ || true
-scp -F ssh_config ${os}:/root/foreman-debug* debug/ || true
-scp -F ssh_config ${os}:/root/hammer_test_logs/* debug/ || true
+cp "$filename" debug/
+vagrant ssh-config $box_name > ssh_config
+scp -F ssh_config ${box_name}:/root/bats_results/*.tap debug/ || true
+scp -F ssh_config ${box_name}:/root/last_logs debug/ || true
+scp -F ssh_config ${box_name}:/root/sosreport* debug/ || true
+scp -F ssh_config ${box_name}:/root/foreman-debug* debug/ || true
+if [[ $run_hammer_tests == true ]] ; then
+  scp -F ssh_config ${box_name}:/root/hammer_test_logs/* debug/ || true
+fi
 
 exit 0
