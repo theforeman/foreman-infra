7050c0ff9aca919382ba2a48ae34b3df91244994
Michael Moll <kvedulv@kvedulv.de>
2017-12-29 19:15:59 +0100

    stop installing 1.x Rubies

--- puppet/modules/slave/manifests/init.pp
+++ puppet/modules/slave/manifests/init.pp
@@ -328,15 +328,6 @@ class slave (
     }
 
     if $::architecture == 'x86_64' or $::architecture == 'amd64' {
-      slave::rvm_config { 'ruby-1.8.7':
-        version => 'ruby-1.8.7-p371',
-      }
-      slave::rvm_config { 'ruby-1.9.2':
-        version => 'ruby-1.9.2-p320',
-      }
-      slave::rvm_config { 'ruby-1.9.3':
-        version => 'ruby-1.9.3-p392',
-      }
       slave::rvm_config { 'ruby-2.0.0':
         version => 'ruby-2.0.0-p643',
       }
