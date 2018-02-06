c0cfe548580268ffca3ed325c9184e09e037d1a9
Michael Moll <kvedulv@kvedulv.de>
2018-02-06 21:27:13 +0100

    update rubygems to match Ruby 2.5 version

--- puppet/modules/slave/manifests/rvm_config.pp
+++ puppet/modules/slave/manifests/rvm_config.pp
@@ -1,4 +1,4 @@
-define slave::rvm_config($version, $rubygems_version = '2.6.14') {
+define slave::rvm_config($version, $rubygems_version = '2.7.3') {
   $alias = $title
 
   rvm_system_ruby { $version:
