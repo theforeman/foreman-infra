5856420f629958523be42ef433982714bf4f0e39
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-02-14 17:27:47 +0100

    Optimize the building of RPM package RPMs
    In the new directory layout we can determine the releaser based on the
    directory the package is in. This means we need fewer jobs reducing load
    and making it easier to see what changed.

--- puppet/modules/jenkins_job_builder/files/theforeman.org/scripts/test/packaging_test_pull_request_rpm.sh
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/scripts/test/packaging_test_pull_request_rpm.sh
@@ -19,6 +19,7 @@ for p in $to_build; do
   echo ${p}
 
   project=$(dirname $p)
+  subdir=$(basename $(dirname $project))
   p=$(basename $p)
   p=${p/.spec}
   mkdir -p test_builds/rpm/${p}
@@ -52,28 +53,51 @@ for p in $to_build; do
         echo "gitrelease=false" >> test_builds/rpm/${p}.properties
         ;;
       *)
-        r=""
+        if [[ -d projects ]] ; then
+          echo "gitrelease=true" >> test_builds/rpm/${p}.properties
+          if [[ $subdir == "katello" ]] ; then
+            r=koji-katello
+          elif [[ $subdir == "plugins" ]] ; then
+            r=koji-foreman-plugins
+          else
+            r=koji-foreman
+          fi
+        else
+          r=""
+        fi
         ;;
     esac
   fi
 
-  if [[ $project == *"katello"* && -z "$r" ]]; then
-    echo "project=${project}" >> test_builds/rpm/${p}.properties
-    mv test_builds/rpm/${p}.properties test_builds/rpm/${p}-katello.properties
-    echo "releaser=koji-katello" >> test_builds/rpm/${p}-katello.properties
-    echo "releaser=koji-katello-client" >> test_builds/rpm/${p}-katello.properties
-    echo "gitrelease=true" >> test_builds/rpm/${p}-katello.properties
-  elif [ -z "$r" ]; then
-    # build once for main releaser, once for plugins (one or both may be no-ops)
-    echo "project=${project}" >> test_builds/rpm/${p}.properties
-    cp test_builds/rpm/${p}.properties test_builds/rpm/${p}-plugins.properties
-    cp test_builds/rpm/${p}.properties test_builds/rpm/${p}-katello.properties
-    echo "releaser=koji-foreman" >> test_builds/rpm/${p}.properties
-    echo "releaser=koji-foreman-plugins" >> test_builds/rpm/${p}-plugins.properties
-    echo "gitrelease=true" >> test_builds/rpm/${p}.properties
-    echo "gitrelease=true" >> test_builds/rpm/${p}-plugins.properties
-  else
+  if [[ -d projects ]] ; then
     echo "project=${project}" >> test_builds/rpm/${p}.properties
     echo "releaser=${r}" >> test_builds/rpm/${p}.properties
+
+    if [[ $subdir == "katello" ]] ; then
+      mv test_builds/rpm/${p}.properties test_builds/rpm/${p}-katello.properties
+      echo "releaser=koji-katello-client" >> test_builds/rpm/${p}-katello.properties
+    elif [[ $subdir == "plugins" ]] ; then
+      mv test_builds/rpm/${p}.properties test_builds/rpm/${p}-plugins.properties
+    fi
+  else
+    if [[ $project == *"katello"* && -z "$r" ]]; then
+      echo "project=${project}" >> test_builds/rpm/${p}.properties
+      mv test_builds/rpm/${p}.properties test_builds/rpm/${p}-katello.properties
+      echo "releaser=koji-katello" >> test_builds/rpm/${p}-katello.properties
+      echo "releaser=koji-katello-client" >> test_builds/rpm/${p}-katello.properties
+      echo "gitrelease=true" >> test_builds/rpm/${p}-katello.properties
+    elif [ -z "$r" ]; then
+      # build once for main releaser, once for plugins (one or both may be no-ops)
+      echo "project=${project}" >> test_builds/rpm/${p}.properties
+      cp test_builds/rpm/${p}.properties test_builds/rpm/${p}-plugins.properties
+      cp test_builds/rpm/${p}.properties test_builds/rpm/${p}-katello.properties
+      echo "releaser=koji-foreman" >> test_builds/rpm/${p}.properties
+      echo "releaser=koji-foreman-plugins" >> test_builds/rpm/${p}-plugins.properties
+      echo "gitrelease=true" >> test_builds/rpm/${p}.properties
+      echo "gitrelease=true" >> test_builds/rpm/${p}-plugins.properties
+    else
+      echo "project=${project}" >> test_builds/rpm/${p}.properties
+      echo "releaser=${r}" >> test_builds/rpm/${p}.properties
+    fi
   fi
 done
