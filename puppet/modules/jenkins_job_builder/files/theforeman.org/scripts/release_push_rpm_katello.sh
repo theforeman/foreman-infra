224bf850790174ae6bb71fc019cb2f837ef1c40d
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-02-22 17:58:45 +0100

    Remove unused release_*_katello_* jobs
    This jobs have never ran and have been replaced by the release pipeline.

--- puppet/modules/jenkins_job_builder/files/theforeman.org/scripts/release_push_rpm_katello.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -xe
-
-ssh -i /var/lib/workspace/workspace/deploy_katello_repos_key/deploy_katello_repos_key katelloproject@fedorapeople.org "cd /project/katello/bin && sh rsync-repos-from-koji $RELEASE"
