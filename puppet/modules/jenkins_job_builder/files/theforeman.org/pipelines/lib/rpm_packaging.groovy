230c75c178eac18c44e8fc4c5873055aefda0336
Evgeni Golov <evgeni@golov.de>
2018-02-27 14:46:17 +0100

    add koji links as status to the GitHub PR

--- puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/lib/rpm_packaging.groovy
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/lib/rpm_packaging.groovy
@@ -44,6 +44,27 @@ pipeline {
             }
         }
     }
+
+    post {
+        always {
+            status_koji_links("${currentBuild.getCurrentResult()}")
+        }
+    }
+}
+
+def status_koji_links(build_status) {
+    def tasks = get_koji_tasks()
+    for (String task: tasks) {
+        githubNotify credentialsId: 'github-token', account: 'theforeman', repo: 'foreman-packaging', sha: "${ghprbActualCommit}", context: "koji/${task}", description: "koji task #${task}" , status: build_status, targetUrl: "http://koji.katello.org/koji/taskinfo?taskID=${task}"
+    }
+}
+
+def get_koji_tasks() {
+    def tasks = []
+    if(fileExists('kojilogs')) {
+        tasks = sh(returnStdout: true, script: "ls kojilogs -1 |grep -o '[0-9]*\$'").trim().split()
+    }
+    return tasks
 }
 
 def update_build_description_from_packages(packages_to_build) {
