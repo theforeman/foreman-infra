f2b95b879155ffd0c627dcd3258f2cfdfaebb829
Eric D. Helms <ericdhelms@gmail.com>
2018-03-02 12:24:49 -0500

    Use sha1 for pull request builder

--- puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/lib/git.groovy
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/lib/git.groovy
@@ -1,7 +1,7 @@
 def ghprb_git_checkout() {
     checkout changelog: true, poll: false, scm: [
         $class: 'GitSCM',
-        branches: [[name: '${ghprbActualCommit}']],
+        branches: [[name: '${sha1}']],
         doGenerateSubmoduleConfigurations: false,
         extensions: [[$class: 'PreBuildMerge', options: [fastForwardMode: 'FF', mergeRemote: 'origin', mergeStrategy: 'default', mergeTarget: '${ghprbTargetBranch}']]],
         submoduleCfg: [],
