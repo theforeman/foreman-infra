333632362eca2ec529393db5ef559c7820f619ff
Eric D. Helms <ericdhelms@gmail.com>
2018-03-06 13:19:22 -0500

    Use distro docker

--- puppet/modules/slave/manifests/docker.pp
+++ puppet/modules/slave/manifests/docker.pp
@@ -1,5 +1,19 @@
 class slave::docker {
 
-  include docker
+  file { '/etc/systemd/system/docker.service.d/service-overrides.conf':
+    ensure => absent,
+  }
+
+  package { 'docker-ce':
+    ensure => absent,
+  } ->
+  file { '/etc/yum.repos.d/docker.repo':
+    ensure => absent,
+  } ->
+  class { 'docker':
+    use_upstream_package_source => false,
+    service_overrides_template  => false,
+    docker_ce_package_name      => 'docker',
+  }
 
 }
