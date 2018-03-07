c84b7e4457fddb048671cb825ceebc8c2e956fbc
Adam Price <komidore64@gmail.com>
2018-03-07 13:17:42 -0500

    move git checkout into the pipeline

--- puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/release/deployPuppet.groovy
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/release/deployPuppet.groovy
@@ -1,14 +1,17 @@
 pipeline {
     agent { label 'admin && sshkey' }
-
     options {
         timestamps()
         disableConcurrentBuilds()
         ansiColor('xterm')
     }
-
     stages {
-        stage('Execute shell') {
+        stage('Setup Environment') {
+            steps {
+                git url: 'https://github.com/theforeman/foreman-infra'
+            }
+        }
+        stage('Deploy') {
             steps {
                 sh 'ssh -p 8122 deploypuppet@puppetmaster.theforeman.org -i /var/lib/workspace/workspace/deploy_key/deploy_key'
             }
