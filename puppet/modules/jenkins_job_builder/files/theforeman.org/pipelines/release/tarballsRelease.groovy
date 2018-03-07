b8758bfc69597b45752eef5d7137c08ebbff2d6d
Adam Price <komidore64@gmail.com>
2018-03-07 17:20:31 -0500

    convert `release_tarballs` to JJB and workflow

---
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/release/tarballsRelease.groovy
@@ -0,0 +1,94 @@
+pipeline {
+    agent { label 'admin && sshkey' }
+
+    options {
+        timestamps()
+        timeout(time: 2, unit: 'HOURS')
+        disableConcurrentBuilds()
+        ansiColor('xterm')
+    }
+
+    def version = env.getProperty('version')
+    def major_version = env.getProperty('major_version')
+    def ruby_ver = '2.4.0'
+
+    stages {
+        stage('Parallel') {
+
+            parallel {
+                stage('Foreman') {
+                    steps {
+                        verify_tag('foreman', version)
+                        build_tarball('foreman', version, ruby_ver)
+                    }
+                }
+                stage('Smart Proxy') {
+                    steps {
+                        verify_tag('smart-proxy', version)
+                        build_tarball('smart-proxy', version, ruby_ver)
+                    }
+                }
+                stage('Foreman SELinux') {
+                    steps {
+                        verify_tag('foreman-selinux', version)
+                        build_tarball('foreman-selinux', version, ruby_ver)
+                    }
+                }
+                stage('Foreman Installer') {
+                    steps {
+                        verify_tag('foreman-installer', version)
+                        build_tarball('foreman-installer', version, ruby_ver)
+                    }
+                }
+            }
+        }
+    }
+
+    post {
+        success {
+            build job: 'release_packages', parameters: [
+                [$class: 'StringParameterValue', name: 'version', value: version],
+                [$class: 'StringParameterValue', name: 'major_version', value: major_version]
+            ]
+        }
+    }
+}
+
+void verify_tag(project, version) {
+    dir(project) {
+        git url: "https://github.com/theforeman/${project}", branch: 'develop'
+        sh "test \"$(git tag -l ${version} | wc -l)\" -eq 1"
+    }
+}
+
+void build_tarball(project, version, ruby_ver) {
+    def base_dir = "/var/www/vhosts/downloads/htdocs/foreman"
+    def rake = "rake"
+
+    dir(project) {
+        checkout scm: [$class: 'GitSCM',
+            userRemoteConfigs: [[url: "git@github.com:theforeman/${project}.git"]],
+            branches: [[name: "refs/tags/${version}"]]], changelog: false, poll: false
+
+        configureRVM(ruby_ver)
+
+        if (project == 'foreman') {
+            sh "cat config/settings.yaml.example > config/settings.yaml"
+            sh "cat config/database.yml.example > config/database.yml"
+        }
+
+        env.setProperty('DEBUG_RESOLVER', '1')
+
+        if (fileExists("Gemfile")) {
+            withRVM(["bundle install --without=development --retry=5"], ruby_ver)
+            rake = "bundle exec rake"
+        }
+
+        withRVM(["${rake} pkg:generate_source", ruby_ver)
+
+        sh "ssh root@theforeman.org \"mkdir -p ${base_dir}/\" || true"
+        sh "rsync -v --ignore-existing pkg/* root@theforeman.org:${base_dir}/"
+
+        cleanupRVM('', ruby_ver)
+    }
+}
