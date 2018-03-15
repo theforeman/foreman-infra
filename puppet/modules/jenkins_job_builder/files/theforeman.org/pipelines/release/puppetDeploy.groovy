fb193a32a7990ade8237808cd90e52e61693b132
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-03-15 15:57:06 +0100

    Fix SCM polling in puppet-deploy
    Without this Jenkins doesn't know what to poll.

--- puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/release/puppetDeploy.groovy
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/release/puppetDeploy.groovy
@@ -11,6 +11,7 @@ pipeline {
     stages {
         stage('Execute shell') {
             steps {
+                git url: 'https://github.com/theforeman/foreman-infra.git'
                 sh 'ssh -p 8122 deploypuppet@puppetmaster.theforeman.org -i /var/lib/workspace/workspace/deploy_key/deploy_key'
             }
         }
