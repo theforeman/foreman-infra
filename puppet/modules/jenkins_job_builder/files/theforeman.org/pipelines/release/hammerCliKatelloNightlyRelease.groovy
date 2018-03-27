42d09b225c9120f95cee14e09c09708e3ce25b6b
Zjhuntin <zhunting@redhat.com>
2018-03-27 10:10:46 -0400

    Add build hammer_cli_katello_nightly_release job

---
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/release/hammerCliKatelloNightlyRelease.groovy
@@ -0,0 +1,34 @@
+pipeline {
+    agent { label 'admin' }
+
+    option {
+        timestamps()
+        timeout(time: 2, unit: 'HOURS')
+        disableConcurrentBuilds()
+        ansiColor('xterm')
+    }
+
+    stages {
+        stage ("Build Gem") {
+            steps {
+                dir('hammer_cli_katello') {
+                    git url: "https://github.com/katello/hammer-cli-katello", branch: 'master'
+                    sh "gem build hammer_cli_katello.gemspec"
+                    archiveArtifacts artifacts '*.gem'
+                }
+            }
+        }
+
+        stage('Trigger RPM Build') {
+            steps {
+                build job: 'packaging_build_rpm', propagate: true, parameters: [
+                    string(name: 'project', value: 'packages/katello/hammer_cli_katello')
+                    booleanParam(name: 'gitrelease', value: false),
+                    booleanParam(name: 'scratch', value: false),
+                    string(name: 'nightly_jenkins_job', value: env.getProperty('JOB_NAME')),
+                    string(name: 'nightly_jenkins_job_id', value: env.getProperty('BUILD_ID'))
+                ]
+            }
+        }
+    }
+}
