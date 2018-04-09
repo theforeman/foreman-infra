f5ddea56b0914db5513a700fe96399467a575f90
Adam Price <komidore64@gmail.com>
2018-04-09 19:18:32 -0400

    use bundle retry instead of bash while loop

--- puppet/modules/jenkins_job_builder/files/theforeman.org/scripts/test/test_hammer_cli_foreman.sh
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/scripts/test/test_hammer_cli_foreman.sh
@@ -16,13 +16,5 @@ gem install bundler --no-ri --no-rdoc
 # Link hammer_cli from github
 echo 'gem "hammer_cli", :github => "theforeman/hammer-cli"' > Gemfile.local
 
-# Retry as rubygems (being external to us) can be intermittent
-while ! bundle install --without=development; do
-  (( c += 1 ))
-  if [ $c -ge 5 ]; then
-    echo "bundle install continually failed" >&2
-    exit 1
-  fi
-done
-
+bundle install --without development --retry 5
 bundle exec rake pkg:generate_source ci:setup:minitest test TESTOPTS="-v" --trace
