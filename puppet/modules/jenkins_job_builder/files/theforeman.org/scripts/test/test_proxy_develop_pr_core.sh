86ec3244caf7513a5934c5271910342dc660921d
Adam Price <komidore64@gmail.com>
2018-04-21 20:16:06 -0400

    test_proxy_develop_pr_core - convert to JJB

---
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/scripts/test/test_proxy_develop_pr_core.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -ex
+
+APP_ROOT=`pwd`
+
+# setup basic settings file
+cp $APP_ROOT/config/settings.yml.example $APP_ROOT/config/settings.yml
+
+# RVM Ruby environment
+. /etc/profile.d/rvm.sh
+# Use a gemset unique to each executor to enable parallel builds
+gemset=$(echo ${JOB_NAME} | cut -d/ -f1)-${EXECUTOR_NUMBER}
+rvm use ruby-${ruby}@${gemset} --create
+rvm gemset empty --force
+# Update any gems from the global gemset
+#gem update --no-ri --no-rdoc
+gem install bundler --no-ri --no-rdoc
+
+# Puppet environment
+sed -i "/^\s*gem.*puppet/ s/\$/, '~> $puppet'/" $APP_ROOT/bundler.d/puppet.rb
+
+bundle install --without=development --retry 5
+bundle exec rake pkg:generate_source jenkins:unit
