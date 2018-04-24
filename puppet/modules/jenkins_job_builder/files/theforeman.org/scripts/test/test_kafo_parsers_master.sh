06edeea2761732138ae4dbbd8f82ca8443786fd3
Adam Price <komidore64@gmail.com>
2018-04-24 11:44:08 -0400

    test_kafo_parsers_master* - trim matrix even more!

--- puppet/modules/jenkins_job_builder/files/theforeman.org/scripts/test/test_kafo_parsers_master.sh
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/scripts/test/test_kafo_parsers_master.sh
@@ -8,10 +8,6 @@ APP_ROOT=`pwd`
 gemset=$(echo ${JOB_NAME} | cut -d/ -f1)-${EXECUTOR_NUMBER}
 rvm use ruby-${ruby}@${gemset} --create
 rvm gemset empty --force
-# Don't update gems from the global gemset for Ruby 1.9*, io-console and json requires Ruby 2.0+
-if [[ ${ruby} == 2* ]]; then
-  gem update --no-ri --no-rdoc
-fi
 gem install bundler --no-ri --no-rdoc
 
 # rename axis for Gemfile env var
