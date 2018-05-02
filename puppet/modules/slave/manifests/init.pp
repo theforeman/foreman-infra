d84c1176410a6002cad1735b3ffccc4c41eb6d37
Adam Price <komidore64@gmail.com>
2018-05-02 18:50:01 -0400

    foreman-plugins-pull-request - add foreman_templates

--- puppet/modules/slave/manifests/init.pp
+++ puppet/modules/slave/manifests/init.pp
@@ -51,7 +51,6 @@ class slave (
   }
   slave::pr_test_config { [
     'foreman_packaging',
-    'foreman_templates',
     'foreman_userdata',
     'katello_packaging',
     'puppetdb_foreman',
@@ -85,6 +84,7 @@ class slave (
       'foreman_salt',
       'foreman_setup',
       'foreman_tasks',
+      'foreman_templates',
       'hammer_cli',
       'hammer_cli_foreman',
       'hammer_cli_foreman_discovery',
