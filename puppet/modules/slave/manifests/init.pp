5d279687591bcaa58679bc335b585bc2b2cb023d
Adam Price <komidore64@gmail.com>
2018-05-02 18:45:43 -0400

    foreman-plugins-pull-request - add foreman_remote_execution

--- puppet/modules/slave/manifests/init.pp
+++ puppet/modules/slave/manifests/init.pp
@@ -51,7 +51,6 @@ class slave (
   }
   slave::pr_test_config { [
     'foreman_packaging',
-    'foreman_remote_execution',
     'foreman_salt',
     'foreman_setup',
     'foreman_tasks',
@@ -85,6 +84,7 @@ class slave (
       'foreman_omaha',
       'foreman_openscap',
       'foreman_pipeline',
+      'foreman_remote_execution',
       'hammer_cli',
       'hammer_cli_foreman',
       'hammer_cli_foreman_discovery',
