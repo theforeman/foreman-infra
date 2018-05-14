672dbc31ff4bf3a24581382d694900a15d254413
Evgeni Golov <evgeni@golov.de>
2018-05-14 12:38:22 +0200

    don't set mergeStrategy: default
    newer versions of the git plugin do not like that. not setting it does
    the right thing: uses the default

--- puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/lib/git.groovy
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/lib/git.groovy
@@ -3,7 +3,7 @@ def ghprb_git_checkout() {
         $class: 'GitSCM',
         branches: [[name: '${sha1}']],
         doGenerateSubmoduleConfigurations: false,
-        extensions: [[$class: 'PreBuildMerge', options: [fastForwardMode: 'FF', mergeRemote: 'origin', mergeStrategy: 'default', mergeTarget: '${ghprbTargetBranch}']]],
+        extensions: [[$class: 'PreBuildMerge', options: [fastForwardMode: 'FF', mergeRemote: 'origin', mergeTarget: '${ghprbTargetBranch}']]],
         submoduleCfg: [],
         userRemoteConfigs: [
             [refspec: '+refs/heads/${ghprbTargetBranch}:refs/remotes/origin/${ghprbTargetBranch} +refs/pull/${ghprbPullId}/*:refs/remotes/origin/pr/${ghprbPullId}/*', url: 'https://github.com/${ghprbGhRepository}']
