768c53c7c91f01155e0760723b0a6729d638326e
Adam Price <komidore64@gmail.com>
2018-05-16 16:43:09 -0400

    test_proxy_develop_pr_rubocop.sh - silence RVM

--- puppet/modules/jenkins_job_builder/files/theforeman.org/scripts/test/test_proxy_develop_pr_rubocop.sh
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/scripts/test/test_proxy_develop_pr_rubocop.sh
@@ -5,12 +5,15 @@ APP_ROOT=`pwd`
 # setup basic settings file
 cp $APP_ROOT/config/settings.yml.example $APP_ROOT/config/settings.yml
 
+set +x
 # RVM Ruby environment
 . /etc/profile.d/rvm.sh
 # Use a gemset unique to each executor to enable parallel builds
 gemset=$(echo ${JOB_NAME} | cut -d/ -f1)-${EXECUTOR_NUMBER}
 rvm use ruby-${ruby}@${gemset} --create
 rvm gemset empty --force
+set -x
+
 # Update any gems from the global gemset
 gem update --no-ri --no-rdoc
 gem install bundler --no-ri --no-rdoc
