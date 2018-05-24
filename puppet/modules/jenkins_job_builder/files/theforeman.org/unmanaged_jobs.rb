ff8c3aadd58f636763442394fe0fb185973b0ca2
Adam Price <komidore64@gmail.com>
2018-05-24 15:54:20 -0400

    tell unmanaged_jobs.rb what our ini file is

--- puppet/modules/jenkins_job_builder/files/theforeman.org/unmanaged_jobs.rb
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/unmanaged_jobs.rb
@@ -3,6 +3,9 @@
 require 'net/http'
 require 'json'
 
-payload = Net::HTTP.get('ci.theforeman.org', '/api/json?tree=jobs[name,description]')
+url = File.open(ARGV.pop) { |file| file.readlines }.select { |line| line.include?('url=') }.first.gsub(/url=(.*\/\/)?/, '').chomp
+
+payload = Net::HTTP.get(url, '/api/json?tree=jobs[name,description]')
 
 puts JSON.parse(payload)['jobs'].select { |job| !job['description'].include?('<!-- Managed by Jenkins Job Builder -->') }.map { |job| job['name'] }.join(' ')
+
