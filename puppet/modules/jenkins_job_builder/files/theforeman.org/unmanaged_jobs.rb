2ebc05805ed0f3f9abed7ab726dee386bf65379b
Adam Price <komidore64@gmail.com>
2018-05-25 09:25:16 -0400

    unmanaged_jobs.rb - be more reader friendly

--- puppet/modules/jenkins_job_builder/files/theforeman.org/unmanaged_jobs.rb
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/unmanaged_jobs.rb
@@ -3,9 +3,41 @@
 require 'net/http'
 require 'json'
 
-url = File.open(ARGV.pop) { |file| file.readlines }.select { |line| line.include?('url=') }.first.gsub(/url=(.*\/\/)?/, '').chomp
+def managed?(job)
+  job['description']&.include?('<!-- Managed by Jenkins Job Builder -->')
+end
 
-payload = Net::HTTP.get(url, '/api/json?tree=jobs[name,description]')
+def get_jenkins_url_from_ini(ini_file)
+  lines = File.open(ini_file, "r") do |file|
+    file.readlines
+  end
 
-puts JSON.parse(payload)['jobs'].select { |job| !job['description'].include?('<!-- Managed by Jenkins Job Builder -->') }.map { |job| job['name'] }.join(' ')
+  url_line = lines.select { |line| line.include?('url=') }.first
+  url = url_line.gsub(/url=(.*\/\/)?/, '').chomp
+  url
+end
 
+def find_unmanaged_jobs_from_payload(json_payload)
+  payload = JSON.parse(json_payload)
+
+  um_jobs = payload['jobs'].select { |job| !managed?(job) }
+  um_jobs
+end
+
+def format_jobs_for_output(jobs)
+  jobs.map { |job| job['name'] }.join(' ')
+end
+
+def main
+  url = get_jenkins_url_from_ini(ARGV.pop)
+
+  payload = Net::HTTP.get(url, '/api/json?tree=jobs[name,description]')
+
+  unmanaged_jobs = find_unmanaged_jobs_from_payload(payload)
+
+  puts format_jobs_for_output(unmanaged_jobs)
+end
+
+if __FILE__ == $0
+  main
+end
