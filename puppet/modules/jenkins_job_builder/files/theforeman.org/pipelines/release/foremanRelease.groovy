theforeman/foreman-infra

commit 3f9a3f015bbd06b2695cb14508968bc87230ce4e

Update repoclosures

puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/release/foremanRelease.groovy
@@ -88,18 +88,14 @@ void repoclosure(repo, dist, additions = []) {
         def command = [
             "./repoclosure.sh yum_el${dist}.conf",
             "http://koji.katello.org/releases/yum/foreman-nightly/${repo}/${dist}/x86_64/",
+            "-l el${dist}-foreman-rails-nightly"
             "-l el${dist}-base",
             "-l el${dist}-updates",
             "-l el${dist}-epel",
             "-l el${dist}-extras",
             "-l el${dist}-scl",
             "-l el${dist}-scl-sclo",
-            "-l el${dist}-scl-ruby",
-            "-l el${dist}-scl-v8",
-            "-l el${dist}-puppet-4",
-            "-l el${dist}-subscription-manager",
-            "-l el${dist}-qpid",
-            "-l el${dist}-tfm-ror51"
+            "-l el${dist}-puppet-5",
         ]
 
         command = command + additions

puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/release/foremanPluginsRelease.groovy
@@ -91,16 +91,14 @@ void repoclosure(repo, dist, additions = []) {
             "./repoclosure.sh yum_${dist}.conf",
             "http://koji.katello.org/releases/yum/foreman-plugins-${repo}/${os_ver}/x86_64/",
             "-l ${dist}-foreman-${repo}",
+            "-l ${dist}-foreman-rails-${repo}",
             "-l ${dist}-base",
             "-l ${dist}-updates",
             "-l ${dist}-epel",
             "-l ${dist}-extras",
-            "-l ${dist}-tfm-ror51",
             "-l ${dist}-scl",
             "-l ${dist}-scl-sclo",
-            "-l ${dist}-scl-ruby",
-            "-l ${dist}-scl-v8",
-            "-l ${dist}-puppet-4"
+            "-l ${dist}-puppet-5"
         ]
 
         command = command + additions

puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/release/katelloRelease.groovy
@@ -112,17 +112,14 @@ void repoclosure(repo, dist, additions = []) {
             "http://koji.katello.org/releases/yum/katello-nightly/${repo}/${dist}/x86_64/",
             "-l ${dist}-foreman-nightly",
             "-l ${dist}-foreman-plugins-nightly",
+            "-l ${dist}-foreman-rails-nightly",
             "-l ${dist}-base",
             "-l ${dist}-updates",
             "-l ${dist}-epel",
             "-l ${dist}-extras",
             "-l ${dist}-scl",
             "-l ${dist}-scl-sclo",
-            "-l ${dist}-scl-ruby",
-            "-l ${dist}-scl-v8",
-            "-l ${dist}-tfm-ror51",
-            "-l ${dist}-puppet-3",
-            "-l ${dist}-puppet-4",
+            "-l ${dist}-puppet-5",
             "-l ${dist}-subscription-manager",
             "-l ${dist}-qpid",
             "-l ${dist}-katello-pulp-nightly",

puppet/modules/jenkins_job_builder/files/theforeman.org/scripts/packaging_repoclosure.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/bin/bash -e
 
-if [ ${os#fedora} != $os ]; then
+if [[ ${os#fedora} != $os ]]; then
   os=f${os#fedora}
-elif [ ${os#centos} != $os ]; then
+elif [[ ${os#centos} != $os ]]; then
   os=el${os#centos}
 fi
 
-if [ ${os#f} != $os ]; then
+if [[ ${os#f} != $os ]]; then
   osname=Fedora
   osver=${os#f}
-elif [ ${os#el} != $os ]; then
+elif [[ ${os#el} != $os ]]; then
   osname=RHEL
   osver=${os#el}
 else
@@ -18,12 +18,13 @@ else
 fi
 
 foreman_lookaside=""
+foreman_lookasides="${foreman_lookasides} rails/${repo}"
 if [ -n "${foreman_lookasides}" ]; then
   lookaside_repos=$(echo $foreman_lookasides | tr "," "\n")
 
   for lookaside_repo in $lookaside_repos
   do
-      foreman_lookaside+="--repofrompath=${lookaside_repo},http://yum.theforeman.org/${lookaside_repo}/${os}/x86_64/ -l ${lookaside_repo} "
+      foreman_lookaside+="--repofrompath=${lookaside_repo},https://yum.theforeman.org/${lookaside_repo}/${os}/x86_64/ -l ${lookaside_repo} "
   done
 fi
 
@@ -47,22 +48,9 @@ if [ -n "${copr_lookasides}" ]; then
   done
 fi
 
-puppet_lookaside=""
-if [ -n "${puppet_lookasides}" ]; then
-  lookaside_repos=$(echo $puppet_lookasides | tr "," "\n")
-
-  for lookaside_repo in $lookaside_repos
-  do
-    if [ ${osname} == 'Fedora' ]; then
-      puppet_lookaside+="--repofrompath=${lookaside_repo}-pc1,http://yum.puppetlabs.com/fedora/${os}/PC1/x86_64/ -l ${lookaside_repo}-pc1"
-    else
-      puppet_lookaside+="--repofrompath=${lookaside_repo},http://yum.puppetlabs.com/el/${osver}/products/x86_64/ -l ${lookaside_repo} --repofrompath=${lookaside_repo}-pc1,http://yum.puppetlabs.com/el/${osver}/PC1/x86_64/ -l ${lookaside_repo}-pc1"
-    fi
-  done
-fi
-
 predefined_lookaside=""
-if [ -n "${predefined_lookasides}" ]; then
+predefined_lookasides="${predefined_lookasides} base updates extras epel scl scl-sclo puppet-5"
+if [[ -n $predefined_lookasides ]]; then
   lookaside_repos=$(echo $predefined_lookasides | tr "," "\n")
 
   for lookaside_repo in $lookaside_repos
@@ -71,7 +59,7 @@ if [ -n "${predefined_lookasides}" ]; then
   done
 fi
 
-options="yum_${os}.conf http://koji.katello.org/releases/yum/${repo}/${osname}/${osver}/x86_64/ -l ${os}-base -l ${os}-updates -l ${os}-extras -l ${os}-epel -l ${os}-scl -l ${os}-scl-sclo -l ${os}-scl-ruby -l ${os}-tfm-ror51 -l ${os}-scl-v8 ${koji_lookaside} ${foreman_lookaside} ${copr_lookaside} ${puppet_lookaside} ${predefined_lookaside}"
+options="yum_${os}.conf http://koji.katello.org/releases/yum/${repo}/${osname}/${osver}/x86_64/ ${koji_lookaside} ${foreman_lookaside} ${copr_lookaside} ${puppet_lookaside} ${predefined_lookaside}"
 
 if [[ $1 == '--dry-run' ]]; then
   echo $options

puppet/modules/jenkins_job_builder/files/theforeman.org/yaml/jobs/release_test.yaml
@@ -10,7 +10,7 @@
     builders:
       - trigger-builds:
         - project: packaging_repoclosure
-          predefined-parameters: "os=${os}\nrepo=foreman-${major_version}\npuppet_lookasides=${os}-puppet\nbranch=${major_version}"
+          predefined-parameters: "os=${os}\nrepo=foreman-${major_version}\nbranch=${major_version}"
           block: true
       - systest:
           repo: '${major_version}'

commit 396c0b9ef0cc6302ed3fb16e63b81950a9964e22

Fix groovy syntax
3f9a3f015bbd06b2695cb14508968bc87230ce4e introduced a regression in the
groovy syntax.

puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/release/foremanRelease.groovy
@@ -88,14 +88,14 @@ void repoclosure(repo, dist, additions = []) {
         def command = [
             "./repoclosure.sh yum_el${dist}.conf",
             "http://koji.katello.org/releases/yum/foreman-nightly/${repo}/${dist}/x86_64/",
-            "-l el${dist}-foreman-rails-nightly"
+            "-l el${dist}-foreman-rails-nightly",
             "-l el${dist}-base",
             "-l el${dist}-updates",
             "-l el${dist}-epel",
             "-l el${dist}-extras",
             "-l el${dist}-scl",
             "-l el${dist}-scl-sclo",
-            "-l el${dist}-puppet-5",
+            "-l el${dist}-puppet-5"
         ]
 
         command = command + additions