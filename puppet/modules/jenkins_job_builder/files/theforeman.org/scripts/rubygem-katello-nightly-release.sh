c96b01eedf346aa14d217ae3d7c7c13c3cf3daa7
Adam Price <komidore64@gmail.com>
2018-06-07 16:41:33 -0400

    build gem in rubygem-katello-nightly-release

---
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/scripts/rubygem-katello-nightly-release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -ex
+
+ruby='2.4'
+
+echo "Setting up RVM environment."
+set +x
+# RVM Ruby environment
+. /etc/profile.d/rvm.sh
+# Use a gemset unique to each executor to enable parallel builds
+gemset=$(echo ${JOB_NAME} | cut -d/ -f1)-${EXECUTOR_NUMBER}
+rvm use ruby-${ruby}@${gemset} --create
+rvm gemset empty --force
+set -x
+
+mkdir pkg
+gem build katello.gemspec
+mv *.gem pkg/
