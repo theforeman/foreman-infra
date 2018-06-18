9c599bc4e94404b5b68f62d964061a3674afbee9
Adam Price <komidore64@gmail.com>
2018-06-18 09:36:41 -0400

    fix hammer-cli-katello-nightly-release job

--- puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/release/hammerCliKatelloNightlyRelease.groovy
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/release/hammerCliKatelloNightlyRelease.groovy
@@ -1,9 +1,9 @@
 pipeline {
     agent { label 'admin' }
 
-    option {
+    options {
         timestamps()
-        timeout(time: 2, unit: 'HOURS')
+        timeout(time: 60, unit: 'MINUTES')
         disableConcurrentBuilds()
         ansiColor('xterm')
     }
@@ -14,7 +14,7 @@ pipeline {
                 dir('hammer_cli_katello') {
                     git url: "https://github.com/Katello/hammer-cli-katello", branch: 'master'
                     sh "gem build hammer_cli_katello.gemspec"
-                    archiveArtifacts artifacts '*.gem'
+                    archiveArtifacts artifacts: '*.gem'
                 }
             }
         }
@@ -22,9 +22,10 @@ pipeline {
         stage('Trigger RPM Build') {
             steps {
                 build job: 'packaging_build_rpm', propagate: true, parameters: [
-                    string(name: 'project', value: 'packages/katello/hammer_cli_katello')
+                    string(name: 'project', value: 'packages/katello/rubygem-hammer_cli_katello'),
                     booleanParam(name: 'gitrelease', value: false),
                     booleanParam(name: 'scratch', value: false),
+                    string(name: 'releaser', value: 'koji-katello-jenkins'),
                     string(name: 'nightly_jenkins_job', value: env.getProperty('JOB_NAME')),
                     string(name: 'nightly_jenkins_job_id', value: env.getProperty('BUILD_ID'))
                 ]
