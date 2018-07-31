8a2ec8410776c5d50b4f2459d3d70adbded19f20
Eric D. Helms <ericdhelms@gmail.com>
2018-07-31 13:06:10 -0400

    Don't perform version and release check in parallel

--- puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/test/rpm_packaging.groovy
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/test/rpm_packaging.groovy
@@ -64,59 +64,55 @@ pipeline {
                 script {
 
                     for(int i = 0; i < packages_to_build.size(); i++) {
-                        def index = i
-                        packages[packages_to_build[index]] = {
-                            package_name = packages_to_build[index]
+                        package_name = packages_to_build[i]
 
-                            sh "git checkout origin/${env.ghprbTargetBranch}"
+                        sh "git checkout origin/${env.ghprbTargetBranch}"
 
-                            spec_path = sh(returnStdout: true, script: "find -name \"${package_name}\\.spec\"").trim()
+                        spec_path = sh(returnStdout: true, script: "find -name \"${package_name}\\.spec\"").trim()
 
-                            if (spec_path) {
-                                old_version = query_rpmspec(spec_path, '%{VERSION}')
-                                old_release = query_rpmspec(spec_path, '%{RELEASE}')
+                        if (spec_path) {
+                            old_version = query_rpmspec(spec_path, '%{VERSION}')
+                            old_release = query_rpmspec(spec_path, '%{RELEASE}')
 
-                                sh "git checkout -"
+                            sh "git checkout -"
 
-                                new_version = query_rpmspec(spec_path, '%{VERSION}')
-                                new_release = query_rpmspec(spec_path, '%{RELEASE}')
+                            new_version = query_rpmspec(spec_path, '%{VERSION}')
+                            new_release = query_rpmspec(spec_path, '%{RELEASE}')
 
-                                compare_version = sh(
-                                  script: "rpmdev-vercmp ${old_version} ${new_version}",
-                                  returnStatus: true
-                                )
+                            compare_version = sh(
+                              script: "rpmdev-vercmp ${old_version} ${new_version}",
+                              returnStatus: true
+                            )
 
-                                compare_release = sh(
-                                  script: "rpmdev-vercmp ${old_release} ${new_release}",
-                                  returnStatus: true
-                                )
+                            compare_release = sh(
+                              script: "rpmdev-vercmp ${old_release} ${new_release}",
+                              returnStatus: true
+                            )
 
-                                compare_new_to_one = sh(
-                                  script: "rpmdev-vercmp 1 ${new_release}",
-                                  returnStatus: true
-                                )
+                            compare_new_to_one = sh(
+                              script: "rpmdev-vercmp 1 ${new_release}",
+                              returnStatus: true
+                            )
 
-                                if (compare_version != VERCMP_EQUAL && (compare_new_to_one == VERCMP_OLDER || compare_new_to_one == VERCMP_EQUAL)) {
-                                    echo "New version and release is reset to 1"
-                                } else if (compare_version != VERCMP_EQUAL && compare_new_to_one == VERCMP_NEWER) {
-                                    // new version, but release was not reset
-                                    echo "Version updated but release was not reset back to 1"
-                                    sh "exit 1" // this fails the stage
-                                } else if (compare_version == VERCMP_EQUAL && compare_release == VERCMP_NEWER) {
-                                    echo "Version remained the same and release is reset to 1"
-                                } else {
-                                    echo "Version or release needs updating"
-                                    sh "exit 1"
-                                }
+                            if (compare_version != VERCMP_EQUAL && (compare_new_to_one == VERCMP_OLDER || compare_new_to_one == VERCMP_EQUAL)) {
+                                echo "New version and release is reset to 1"
+                            } else if (compare_version != VERCMP_EQUAL && compare_new_to_one == VERCMP_NEWER) {
+                                // new version, but release was not reset
+                                echo "Version updated but release was not reset back to 1"
+                                sh "exit 1" // this fails the stage
+                            } else if (compare_version == VERCMP_EQUAL && compare_release == VERCMP_NEWER) {
+                                echo "Version remained the same and release is reset to 1"
                             } else {
-
-                                sh "git checkout -"
-
+                                echo "Version or release needs updating"
+                                sh "exit 1"
                             }
+                        } else {
+
+                            sh "git checkout -"
+
                         }
                     }
 
-                    parallel packages
                 }
             }
         }
