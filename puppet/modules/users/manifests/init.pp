32d6b5b265b71f2db88bff934df4cb53edb2bcea
Tomer Brisker <tbrisker@gmail.com>
2018-08-07 17:30:52 +0300

    Clean up old users setup

--- puppet/modules/users/manifests/init.pp
+++ puppet/modules/users/manifests/init.pp
@@ -1,32 +1,9 @@
 class users (
-  $users = 'undef',
+  Hash[String, Hash] $users = {},
 ) {
   include ::sudo
 
-  if $users == 'undef' {
-    # Create basic users, delete once foreman is updated
-    users::account { 'samkottler':
-      fullname => 'Sam Kottler',
-    }
-
-    users::account { 'gregsutcliffe':
-      fullname => 'Greg Sutcliffe',
-    }
-
-    users::account { 'ohadlevy':
-      fullname => 'Ohad Levy',
-    }
-
-    users::account { 'bgupta':
-      fullname => 'Brian Gupta',
-    }
-
-    users::account { 'dcleal':
-      fullname => 'Dominic Cleal',
-    }
-  } else {
-    # Users hash is passed from Foreman
-    create_resources(users::account, $users)
-  }
+  # Users hash is passed from Foreman
+  create_resources(users::account, $users)
 
 }
