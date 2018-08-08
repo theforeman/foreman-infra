c2eceab4584f1276707c8687ce12679faf427194
Evgeni Golov <evgeni@golov.de>
2018-08-08 16:57:04 +0200

    upper case the github status (#757)

--- puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/lib/koji.groovy
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/lib/koji.groovy
@@ -2,7 +2,7 @@ def status_koji_links(repo) {
     def tasks = get_koji_tasks()
     for (String task: tasks) {
         status = sh(returnStdout: true, script: "koji taskinfo ${task} |awk '/State:/ { print \$2 }'").trim()
-        build_status = (status == 'failed') ? 'failure' : 'success'
+        build_status = (status == 'failed') ? 'FAILURE' : 'SUCCESS'
         githubNotify credentialsId: 'github-token', account: 'theforeman', repo: repo, sha: "${ghprbActualCommit}", context: "koji/${task}", description: "koji task #${task}" , status: build_status, targetUrl: "http://koji.katello.org/koji/taskinfo?taskID=${task}"
     }
 }
