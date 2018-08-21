5377ff82b31c78ba39d05e331a8c924cad81a022
Eric D. Helms <ericdhelms@gmail.com>
2018-08-21 15:05:27 -0400

    Update name of Katello 3.8 upgrade pipeline

--- puppet/modules/jenkins_job_builder/files/centos.org/pipelines/test/katello/3.8-upgrade.groovy
+++ puppet/modules/jenkins_job_builder/files/centos.org/pipelines/test/katello/3.8-upgrade.groovy
@@ -1,4 +1,4 @@
 def playBookVars() {
-    playBook = ['boxes': ['pipeline-upgrade-centos7'], 'pipeline': 'pipeline_katello_upgrade_3.8.yml']
+    playBook = ['boxes': ['pipeline-upgrade-centos7'], 'pipeline': 'pipeline_katello_upgrade_38.yml']
     return playBook
 }
