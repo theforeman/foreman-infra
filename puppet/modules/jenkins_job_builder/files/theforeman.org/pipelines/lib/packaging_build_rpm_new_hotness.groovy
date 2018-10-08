85327f4f6c545e832281a58a25dfee9ca6357a01
Adam Price <komidore64@gmail.com>
2018-10-08 09:06:25 -0400

    use pipeline git-checkout for obal

--- puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/lib/packaging_build_rpm_new_hotness.groovy
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/lib/packaging_build_rpm_new_hotness.groovy
@@ -11,7 +11,8 @@ pipeline {
     stages {
         stage("Setup Environment") {
             steps {
-                git(url: 'https://github.com/theforeman/foreman-packaging/', branch: env.branch)
+                dir('foreman-packaging') { git(url: 'https://github.com/theforeman/foreman-packaging/', branch: env.branch) }
+                dir('obal') { git(url: 'https://github.com/theforeman/obal', branch: 'master') }
             }
         }
 
@@ -23,35 +24,36 @@ pipeline {
                 }
             } // when
             steps {
-                sh("""
-                    git clone https://github.com/theforeman/obal
-                    export PYTHONPATH=`pwd`/obal
+                dir('foreman-packaging') {
+                    sh("""
+                        export PYTHONPATH=${env.WORKSPACE}/obal
 
-                    git-annex init
-                    ./setup_sources.sh \${project}
+                        git-annex init
+                        ./setup_sources.sh \${project}
 
-                    mkdir rel-eng/build
-                    args="-o "\$(pwd)"/rel-eng/build/"
-                    [ -n "\${tag}" ] && args="\${args} --tag=\${tag}"
-                    [ x"\${scratch}" != xfalse ] && args="\${args} --scratch"
-                    [ x"\${gitrelease}" != xfalse -a x\${releaser} != xkoji-foreman-nightly -a x\${releaser} != xkoji-foreman-jenkins ] && args="\${args} --test"
-                    [ -n "\${nightly_jenkins_job}" ] && args="\${args} --arg jenkins_job=\${nightly_jenkins_job}"
-                    [ -n "\${nightly_jenkins_job_id}" ] && args="\${args} --arg jenkins_job_id=\${nightly_jenkins_job_id}"
+                        mkdir -p ${env.WORKSPACE}/foreman-packaging/rel-eng/build
+                        args="-o ${env.WORKSPACE}/foreman-packaging/rel-eng/build/"
+                        [ -n "\${tag}" ] && args="\${args} --tag=\${tag}"
+                        [ x"\${scratch}" != xfalse ] && args="\${args} --scratch"
+                        [ x"\${gitrelease}" != xfalse -a x\${releaser} != xkoji-foreman-nightly -a x\${releaser} != xkoji-foreman-jenkins ] && args="\${args} --test"
+                        [ -n "\${nightly_jenkins_job}" ] && args="\${args} --arg jenkins_job=\${nightly_jenkins_job}"
+                        [ -n "\${nightly_jenkins_job_id}" ] && args="\${args} --arg jenkins_job_id=\${nightly_jenkins_job_id}"
 
-                    cd \${project}
-                    tito release \${args} \${releaser} 2>&1 | tee tito.log
+                        cd \${project}
+                        tito release \${args} \${releaser} 2>&1 | tee tito.log
 
-                    if [ \${PIPESTATUS[0]} -ne 0 ]; then
-                      echo "tito release failed, exit code \${PIPESTATUS[0]}"
-                      exit 1
-                    fi
+                        if [ \${PIPESTATUS[0]} -ne 0 ]; then
+                          echo "tito release failed, exit code \${PIPESTATUS[0]}"
+                          exit 1
+                        fi
 
-                    grep "Task info:" tito.log | grep -o "[0-9]*" > tasks || true
+                        grep "Task info:" tito.log | grep -o "[0-9]*" > tasks || true
 
-                    if [ -s tasks ]; then
-                      xargs koji watch-task < tasks
-                    fi
-                """)
+                        if [ -s tasks ]; then
+                          xargs koji watch-task < tasks
+                        fi
+                    """)
+                }
             }
         }
     }
