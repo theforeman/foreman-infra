9078fe06e4041bd03786a13d0e4abbdd76516774
Adam Price <komidore64@gmail.com>
2018-10-10 10:00:16 -0400

    doesn't process PR builds anymore

--- puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/release/packaging_build_rpm.groovy
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/release/packaging_build_rpm.groovy
@@ -15,42 +15,15 @@ pipeline {
             }
         }
 
-        stage("Apply Pull Request Changes") {
-            when {
-                expression { env.pr_git_url != null && env.pr_git_ref != null }
-            }
-            steps {
-                sh("""
-                    git remote add pr ${env.pr_git_url}
-                    git fetch pr
-                    git merge pr/${env.pr_git_ref}
-                """)
-            }
-        }
-
         stage("Build RPM") {
-            when {
-                anyOf {
-                    expression { env.pr_git_url != null && env.scratch == 'true' }
-                    expression { env.pr_git_url == null }
-                }
-            }
             steps {
                 script {
                     sh("mkdir -p ${env.WORKSPACE}/rel-eng/build")
 
                     def build_package_tito_releaser_args = "-o ${env.WORKSPACE}/rel-eng/build/"
-                    def build_package_scratch = false
-                    def build_package_test = false
 
                     if (env.tag) {
                         build_package_tito_releaser_args += " --tag=${env.tag}"
-                    }
-                    if (env.scratch) {
-                        build_package_scratch = true
-                    }
-                    if (env.gitrelease && env.releaser != 'koji-foreman-nightly' && env.releaser != 'koji-foreman-jenkins') {
-                        build_package_test = true
                     }
                     if (env.nightly_jenkins_job) {
                         build_package_tito_releaser_args += " --arg jenkins_job=${env.nightly_jenkins_job}"
@@ -63,8 +36,6 @@ pipeline {
                         action: 'release',
                         extraVars: [
                             'build_package_tito_releaser_args': [build_package_tito_releaser_args],
-                            'build_package_scratch': build_package_scratch,
-                            'build_package_test': build_package_test,
                             'releaser': env.releaser
                         ],
                         packages: env.project.tokenize('/').last()
