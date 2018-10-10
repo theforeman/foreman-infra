ba3f551e59d88ad3cd6e816b32e529f2170e84bc
Michael Moll <kvedulv@kvedulv.de>
2018-10-10 14:15:45 +0200

    use puppetlabs-java to install JDK on slaves

--- puppet/modules/slave/manifests/java.pp
+++ puppet/modules/slave/manifests/java.pp
@@ -1,9 +1,3 @@
 class slave::java {
-  package { 'java':
-    ensure => present,
-    name   => $::osfamily ? {
-      'RedHat' => 'java-1.8.0-openjdk',
-      default  => 'openjdk-8-jdk',
-    },
-  }
+  include ::java
 }
