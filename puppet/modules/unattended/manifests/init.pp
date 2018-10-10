fbba5ecedd7a5c33d48a9d73a7e792b861a8230d
Michael Moll <kvedulv@kvedulv.de>
2018-10-10 14:15:45 +0200

    use puppetlabs-java to install JDK on slaves

--- puppet/modules/unattended/manifests/init.pp
+++ puppet/modules/unattended/manifests/init.pp
@@ -4,10 +4,14 @@ class unattended {
       auto      => { 'reboot' => false },
       blacklist => [
         'docker-ce',
+        'openjdk-8-jdk',
+        'openjdk-8-jdk-headless',
         'openjdk-8-jre',
         'openjdk-8-jre-headless',
-        'oracle-java8-installer',
-        'oracle-java8-set-default',
+        'openjdk-11-jdk',
+        'openjdk-11-jdk-headless',
+        'openjdk-11-jre',
+        'openjdk-11-jre-headless',
       ],
       mail => { 'to' => 'sysadmins', },
     }
