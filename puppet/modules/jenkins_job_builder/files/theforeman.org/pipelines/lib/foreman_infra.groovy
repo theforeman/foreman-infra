7a32f4a256f07557e8dda00335688cb04ad93ec1
Eric D. Helms <ericdhelms@gmail.com>
2018-11-19 08:54:54 -0500

    Move build_job_description to foreman_infra.groovy

--- puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/lib/foreman_infra.groovy
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/lib/foreman_infra.groovy
@@ -5,3 +5,15 @@ void git_clone_foreman_infra(args = [:]) {
         git url: 'https://github.com/theforeman/foreman-infra'
     }
 }
+
+def set_job_build_description() {
+    def build_description = ""
+    def files_list = sh(script: "ls jobs/ -1", returnStdout: true).trim().split()
+
+    for (i = 0; i < files_list.size(); i++) {
+       link = readFile("jobs/${files_list[i]}")
+       build_description += "<a href=\"${link}\">${files_list[i]}</a><br/>"
+    }
+
+    currentBuild.description = build_description
+}
