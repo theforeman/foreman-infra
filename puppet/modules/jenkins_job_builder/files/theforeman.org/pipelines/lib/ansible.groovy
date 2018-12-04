b63a4e09b53c082f864cde9939e857dc73ff274a
Evgeni Golov <evgeni@golov.de>
2018-12-04 10:58:06 +0100

    implement sensitiveExtraVars

--- puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/lib/ansible.groovy
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/lib/ansible.groovy
@@ -2,6 +2,7 @@ def runPlaybook(args) {
     playbook = args.playbook
     inventory = args.inventory ?: 'localhost'
     extraVars = args.extraVars ?: [:]
+    sensitiveExtraVars = args.sensitiveExtraVars ?: [:]
     options = args.options ?: []
 
     def command = [
@@ -19,6 +20,11 @@ def runPlaybook(args) {
         command.push("-e@${extra_vars_file}")
     }
 
+    if (sensitiveExtraVars) {
+        sensitive_extra_vars_file = writeExtraVars(extraVars: sensitiveExtraVars, archiveExtraVars: false)
+        command.push("-e@${sensitive_extra_vars_file}")
+    }
+
     wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
         sh "${command.join(' ')}"
     }
@@ -28,9 +34,12 @@ def writeExtraVars(args) {
     def timestamp = new Date().getTime()
     def extra_vars_file = 'extra_vars-' + timestamp.toString() + '.yaml'
     def extra_vars = args.extraVars ?: [:]
+    def archive_extra_vars = args.archiveExtraVars ?: true
 
     writeYaml file: extra_vars_file, data: extra_vars
-    archiveArtifacts artifacts: extra_vars_file
+    if (archive_extra_vars) {
+        archiveArtifacts artifacts: extra_vars_file
+    }
 
     return extra_vars_file
 }
