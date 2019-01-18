bc77f0e929a4ab4d1e58521ddb506c1845e36a60
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-01-18 13:35:17 +0100

    Update puppet spec testing

--- puppet/spec/spec_helper.rb
+++ puppet/spec/spec_helper.rb
@@ -1,7 +1,10 @@
+RSpec.configure do |c|
+  c.mock_with :rspec
+end
+
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
 include RspecPuppetFacts
 
-add_custom_fact :concat_basedir, '/doesnotexist'
 add_custom_fact :root_home, '/root'
 add_custom_fact :rvm_installed, false
