e76ef3dbe07ba94a846f3382431b5ede83f9856c
John Mitsch <jomitsch@redhat.com>
2019-02-08 08:57:05 -0500

    Updated katello devel test according to comments

--- puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/test/katelloDevel.groovy
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/test/katelloDevel.groovy
@@ -3,16 +3,16 @@ pipeline {
 
     options {
         timestamps()
-            timeout(time: 2, unit: 'HOURS')
-            disableConcurrentBuilds()
-            ansiColor('xterm')
+        timeout(time: 2, unit: 'HOURS')
+        disableConcurrentBuilds()
+        ansiColor('xterm')
     }
 
     stages {
-        stage('forklift centos7-devel pipeline tests') {
+        stage('katello devel forklift pipeline tests') {
             agent { label 'el' }
             environment {
-                cico_job_name = "foreman-forklift-centos-devel-test"
+                cico_job_name = "foreman-katello-devel-test"
             }
 
             steps {
@@ -22,9 +22,9 @@ pipeline {
                     runPlaybook(
                         playbook: 'ci/centos.org/ansible/jenkins_job.yml',
                         extraVars: [
-                        "jenkins_job_name": "${env.cico_job_name}",
-                        "jenkins_username": "foreman",
-                        "jenkins_job_link_file": "${env.WORKSPACE}/jobs/${env.cico_job_name}"
+                            "jenkins_job_name": "${env.cico_job_name}",
+                            "jenkins_username": "foreman",
+                            "jenkins_job_link_file": "${env.WORKSPACE}/jobs/${env.cico_job_name}"
                         ],
                         sensitiveExtraVars: ["jenkins_password": "${env.PASSWORD}"]
                     )
