0908bdc86940882b1c6ac063c88178663288711e
Eric D. Helms <ericdhelms@gmail.com>
2019-02-10 17:03:00 -0500

    Re-factor hammer-cli-katello-master-release

--- puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/vars/hammer-cli-foreman-master-release.groovy
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/vars/hammer-cli-foreman-master-release.groovy
@@ -1,6 +1,6 @@
 def git_url = 'https://github.com/theforeman/hammer-cli-foreman.git'
 def git_ref = 'master'
-def package_name = 'rubygem-hammer_cli_foreman'
+def obal_package_name = 'rubygem-hammer_cli_foreman'
 def project_name = 'hammer-cli-foreman'
 def build_rpm = true
 def build_deb = false
