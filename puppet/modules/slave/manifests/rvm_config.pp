f27201393d4a31ea76ffa6fdbffc3e45f70113f5
Michael Moll <kvedulv@kvedulv.de>
2019-03-13 17:52:04 +0100

    Update rubygems to 2.7.9

--- puppet/modules/slave/manifests/rvm_config.pp
+++ puppet/modules/slave/manifests/rvm_config.pp
@@ -1,4 +1,4 @@
-define slave::rvm_config($version, $rubygems_version = '2.7.8') {
+define slave::rvm_config($version, $rubygems_version = '2.7.9') {
   $alias = $title
 
   rvm_system_ruby { $version:
