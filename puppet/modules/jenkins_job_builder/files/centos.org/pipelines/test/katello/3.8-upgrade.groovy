40897f7f7cfb367fc10256dd1ebeacb664158cba
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-03-19 15:45:06 +0100

    Remove Katello 3.8 jobs
    Now that 3.11 is out this is EOL.

--- puppet/modules/jenkins_job_builder/files/centos.org/pipelines/test/katello/3.8-upgrade.groovy
+++
@@ -1,4 +0,0 @@
-def playBookVars() {
-    playBook = ['boxes': ['pipeline-upgrade-katello-3.8-centos7'], 'pipeline': 'upgrade_pipeline.yml', 'extraVars': ['pipeline_version': '3.8', 'pipeline_os': 'centos7', 'pipeline_type': 'katello']]
-    return playBook
-}
