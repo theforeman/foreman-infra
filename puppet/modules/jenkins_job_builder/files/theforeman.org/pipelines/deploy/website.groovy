a119ea0568045fe7c997124b3d3c18b8bda8be61
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-03-21 13:40:15 +0100

    Refactor website deployment into a pipeline

---
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/deploy/website.groovy
@@ -0,0 +1,42 @@
+pipeline {
+    agent { label 'admin && sshkey' }
+
+    options {
+        ansiColor('xterm')
+        buildDiscarder(logRotator(numToKeepStr: '3'))
+        disableConcurrentBuilds()
+        timestamps()
+    }
+
+    environment {
+        ruby_version = '2.5'
+        // Sync to the pivot-point on the web node
+        target_path = 'website@theforeman.org:rsync_cache/'
+    }
+
+    stages {
+        stage('Deploy website') {
+            steps {
+                git url: 'https://github.com/theforeman/theforeman.org'
+
+                script {
+                    try {
+                        configureRVM(ruby_version)
+                        withRVM(['bundle install --jobs=5 --retry=5'], ruby_version)
+                        withRVM(['bundle exec jekyll build'], ruby_version)
+                    } finally {
+                        cleanupRVM(ruby_version)
+                    }
+                }
+
+                sshagent(['deploy-website']) {
+                    // Copy the site to the web node
+                    // Dependencies
+                    // * the slave must have the web::uploader class
+                    // * the web node must have the web class
+                    sh "/usr/bin/rsync --archive --checksum --verbose --one-file-system --compress --stats --delete-after ./_site/ ${target_path}"
+                }
+            }
+        }
+    }
+}
