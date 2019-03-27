5c1ba8ac124b120492c4d91ba2455f6da195ac90
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-03-27 12:39:45 +0100

    Introduce a foreman-selinux-develop-test job
    This follows the pattern of having a -test job to verify functionality
    and a -release job to build packages.
    
    It avoids the rebuild for every foreman-packaging change issue.
    
    This change drops the timed builds which aren't needed for
    foreman-selinux since it doesn't bundle anything.

---
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/test/foreman-selinux-develop-test.groovy
@@ -0,0 +1,48 @@
+def commit_hash = ''
+
+pipeline {
+    agent { label 'el' }
+
+    options {
+        timestamps()
+        timeout(time: 2, unit: 'HOURS')
+        ansiColor('xterm')
+    }
+
+    stages {
+        stage("Collect Git Hash") {
+            steps {
+                git(url: 'https://github.com/theforeman/foreman-selinux', branch: 'develop')
+                script {
+                    commit_hash = archive_git_hash()
+                }
+            }
+        }
+        stage("Build for rhel7") {
+            steps {
+                script {
+                    distro = 'rhel7'
+                    instprefix = pwd(tmp: true)
+                }
+                sh "make INSTPREFIX=${instprefix}/${distro} DISTRO=${distro}"
+            }
+        }
+        stage("Release Nightly Package") {
+            steps {
+                build(
+                    job: 'foreman-selinux-develop-release',
+                    propagate: false,
+                    wait: false,
+                    parameters: [
+                        string(name: 'git_ref', value: commit_hash)
+                    ]
+                )
+            }
+        }
+    }
+    post {
+        always {
+            deleteDir()
+        }
+    }
+}
