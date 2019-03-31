9142dbb8e352922248a69cbb4392c9048cc7b0c5
Michael Moll <kvedulv@kvedulv.de>
2019-03-31 23:22:44 +0200

    set BUILD_TIMESTAMP in pbuilder scripts

--- puppet/modules/jenkins_job_builder/files/theforeman.org/scripts/debian/execute_pbuilder_plugin.sh
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/scripts/debian/execute_pbuilder_plugin.sh
@@ -16,6 +16,7 @@ cd ${project}
 if [ x$gitrelease = xtrue ] || [ x$pr_number != x ]; then
   PACKAGE_NAME=$(head -n1 debian/changelog|awk '{print $1}')
   DATE=$(date -R)
+  BUILD_TIMESTAMP=$(date +%Y%m%d%H%M%S)
   RELEASE="9999-plugin+scratchbuild+${BUILD_TIMESTAMP}"
   MAINTAINER="${repoowner} <no-reply@theforeman.org>"
   mv debian/changelog debian/changelog.tmp
