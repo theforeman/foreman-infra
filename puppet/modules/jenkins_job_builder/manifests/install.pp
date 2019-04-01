2b92e5939ac3b21850326e8b24e10f4ab202f349
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-04-01 18:50:01 +0200

    Ensure python-pip is installed before using it

--- puppet/modules/jenkins_job_builder/manifests/install.pp
+++ puppet/modules/jenkins_job_builder/manifests/install.pp
@@ -4,6 +4,10 @@
 # https://git.openstack.org/cgit/openstack-infra/puppet-jenkins/tree/manifests/job_builder.pp
 # and should probably be kept up to date from there
 class jenkins_job_builder::install {
+  ensure_packages(['python-pip'])
+
+  Package['python-pip'] -> Package <| provider == 'pip' |>
+
   # A lot of things need yaml, be conservative requiring this package to avoid
   # conflicts with other modules.
   if ! defined(Package['PyYAML']) {
