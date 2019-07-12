ec5f182ae9221280bc3983faae5701db78993200
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-07-12 19:00:44 +0200

    Use new pipeline box names
    In forklift the convention changed to explicitly use -server for the
    server box. This reflects that.

--- puppet/modules/jenkins_job_builder/files/centos.org/pipelines/test/foreman/nightly-centos7.groovy
+++ puppet/modules/jenkins_job_builder/files/centos.org/pipelines/test/foreman/nightly-centos7.groovy
@@ -1,4 +1,4 @@
 def playBookVars() {
-    playBook = ['boxes': ['pipeline-foreman-nightly-centos7'], 'pipeline': 'install_pipeline.yml', 'extraVars': ['pipeline_version': 'nightly', 'pipeline_os': 'centos7', 'pipeline_type': 'foreman']]
+    playBook = ['boxes': ['pipeline-foreman-server-nightly-centos7'], 'pipeline': 'install_pipeline.yml', 'extraVars': ['pipeline_version': 'nightly', 'pipeline_os': 'centos7', 'pipeline_type': 'foreman']]
     return playBook
 }
--- puppet/modules/jenkins_job_builder/files/centos.org/pipelines/test/foreman/nightly-debian9.groovy
+++ puppet/modules/jenkins_job_builder/files/centos.org/pipelines/test/foreman/nightly-debian9.groovy
@@ -1,4 +1,4 @@
 def playBookVars() {
-    playBook = ['boxes': ['pipeline-foreman-nightly-debian9'], 'pipeline': 'install_pipeline.yml', 'extraVars': ['pipeline_version': 'nightly', 'pipeline_os': 'debian9', 'pipeline_type': 'foreman']]
+    playBook = ['boxes': ['pipeline-foreman-server-nightly-debian9'], 'pipeline': 'install_pipeline.yml', 'extraVars': ['pipeline_version': 'nightly', 'pipeline_os': 'debian9', 'pipeline_type': 'foreman']]
     return playBook
 }
