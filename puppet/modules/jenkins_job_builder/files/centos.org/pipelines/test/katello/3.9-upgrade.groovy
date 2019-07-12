5bfcd5c1c2b9408a45606b0effb883a35e5f4496
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-07-12 19:00:12 +0200

    Remove 3.9 pipelines on CentOS CI
    cc14569beba2648804dca72a90195271316cf4f9 removed them on the Foreman
    side.

--- puppet/modules/jenkins_job_builder/files/centos.org/pipelines/test/katello/3.9-upgrade.groovy
+++
@@ -1,4 +0,0 @@
-def playBookVars() {
-    playBook = ['boxes': ['pipeline-upgrade-katello-3.9-centos7'], 'pipeline': 'upgrade_pipeline.yml', 'extraVars': ['pipeline_version': '3.9', 'pipeline_os': 'centos7', 'pipeline_type': 'katello']]
-    return playBook
-}
