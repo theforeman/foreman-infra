53f7447610371a89f01c4779d91eb34aeb107336
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-08-02 15:09:36 +0200

    Rewrite Jenkins job validation
    The new script can read multiple files at once and uses a session. This
    also improves its performance.

--- .travis/jjb.sh
+++ .travis/jjb.sh
@@ -2,11 +2,13 @@
 
 # Test JJB configs
 for d in puppet/modules/jenkins_job_builder/files/theforeman.org; do
-  ( cd $d && jenkins-jobs -l debug test -r . )
-  ( cd $d && jenkins-jobs test -r . --config-xml -o output && git grep -l "project-type:.*pipeline" |xargs -n1 grep -m1 "name:" |cut -d: -f2 | xargs -n1 -I@@ python ../jenkins-lint.py --xml output/@@/config.xml && rm -rf output )
+  DIR="."
+  ( cd $d && jenkins-jobs -l debug test -r $DIR )
+  ( cd $d && jenkins-jobs test -r $DIR --config-xml -o output && find output -name config.xml -exec python ../jenkins-lint.py --xml {} + && rm -rf output )
 done
 
 for d in puppet/modules/jenkins_job_builder/files/centos.org; do
-  ( cd $d && jenkins-jobs -l debug test -r jobs )
-  ( cd $d && jenkins-jobs test -r jobs --config-xml -o output && git grep -l "project-type:.*pipeline" |xargs -n1 grep -m1 "name:" |cut -d: -f2 | xargs -n1 -I@@ python ../jenkins-lint.py --xml output/@@/config.xml && rm -rf output )
+  DIR="jobs"
+  ( cd $d && jenkins-jobs -l debug test -r $DIR )
+  ( cd $d && jenkins-jobs test -r $DIR --config-xml -o output && find output -name config.xml -exec python ../jenkins-lint.py --xml {} + && rm -rf output )
 done
