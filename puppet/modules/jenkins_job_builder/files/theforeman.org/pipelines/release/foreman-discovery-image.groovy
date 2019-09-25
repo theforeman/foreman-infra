02e81c94ca03d890550e8253665a61b2abd923fe
Evgeni Golov <evgeni@golov.de>
2019-09-25 10:14:32 +0200

    cleaner prepare/publish split

--- puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/release/foreman-discovery-image.groovy
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/release/foreman-discovery-image.groovy
@@ -25,21 +25,27 @@ pipeline {
         stage('Publish FDI build') {
             steps {
                 script {
+                    base_dir = "/var/www/vhosts/downloads/htdocs/discovery"
                     destination_user = 'root'
                     destination_server = 'web02.theforeman.org'
-                    base_dir = "/var/www/vhosts/downloads/htdocs/discovery"
+                    destination_dir = "${base_dir}/${output_dir}"
+
+                    // prepare upload
+                    sh """
+                    mkdir -p ${output_dir}
+                    mv fdi*tar fdi*iso ${output_dir}/
+                    pushd ${output_dir}
+                    ln -snf fdi*tar fdi-image-latest.tar
+                    md5sum fdi*tar fdi*iso > MD5SUMS
+                    popd
+                    """
 
                     sshagent(['deploy-downloads']) {
-                        // delete old files in the target folder
-                        sh "ssh ${destination_user}@${destination_server} 'mkdir -p ${base_dir}/${output_dir}/ ; rm -f ${base_dir}/${output_dir}/*' || true"
-
                         // publish on web
-                        sh "md5sum fdi*tar fdi*iso > MD5SUMS"
-                        sh "scp fdi*tar fdi*iso MD5SUMS ${destination_user}@${destination_server}:${base_dir}/${output_dir}/"
+                        sh "rsync -av --delete ${output_dir}/ ${destination_user}@${destination_server}:${destination_dir}/"
 
                         // create symlinks
                         sh "ssh ${destination_user}@${destination_server} 'pushd ${base_dir}/releases/ && rm -f latest; ln -snf \$(ls -t | head -n 1) latest; popd' || true"
-                        sh "ssh ${destination_user}@${destination_server} 'pushd ${base_dir}/${output_dir}/ && ln -snf fdi*tar fdi-image-latest.tar && popd' || true"
                     }
                 }
             }
