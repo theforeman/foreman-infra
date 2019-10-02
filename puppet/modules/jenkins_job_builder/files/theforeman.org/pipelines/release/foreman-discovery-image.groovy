48a21b881d077c68e35a0c3af46a8b0bce7c1b07
Evgeni Golov <evgeni@golov.de>
2019-10-02 10:04:48 +0200

    foreman-discovery-image release pipeline (#1128)
    * fdi release job
    
    * rename job to -release
    
    * use ln -snf
    
    * create md5 sums on ci machine
    
    * run rsync with dry-run for now

---
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/release/foreman-discovery-image.groovy
@@ -0,0 +1,68 @@
+pipeline {
+    agent { label 'admin && sshkey' }
+
+    options {
+        timestamps()
+        timeout(time: 3, unit: 'HOURS')
+        disableConcurrentBuilds()
+        ansiColor('xterm')
+    }
+
+    stages {
+        stage('Run FDI build') {
+            steps {
+                deleteDir()
+                dir('build') {
+                    script {
+                        job_parameters = [
+                            proxy_repository: env.proxy_repository,
+                            branch: env.branch,
+                        ]
+                        job_extra_vars = [
+                            jenkins_download_artifacts: 'true',
+                            jenkins_artifacts_directory: '../artifacts/',
+                        ]
+                        runCicoJob("foreman-discovery-image-build", job_parameters, job_extra_vars)
+                    }
+                }
+            }
+        }
+
+        stage('Prepare FDI upload') {
+            steps {
+                dir('result') {
+                    sh """
+                    mv ../artifacts/fdi*tar ../artifacts/fdi*iso .
+                    ln -snf fdi*tar fdi-image-latest.tar
+                    md5sum fdi*tar fdi*iso > MD5SUMS
+                    """
+                }
+            }
+        }
+
+        stage('Publish FDI build') {
+            steps {
+                script {
+                    base_dir = "/var/www/vhosts/downloads/htdocs/discovery"
+                    destination_user = 'root'
+                    destination_server = 'web02.theforeman.org'
+                    destination_dir = "${base_dir}/${output_dir}"
+
+                    sshagent(['deploy-downloads']) {
+                        // publish on web
+                        sh "rsync -av --dry-run --delete result/ ${destination_user}@${destination_server}:${destination_dir}/"
+
+                        // create symlinks
+                        sh "ssh ${destination_user}@${destination_server} 'pushd ${base_dir}/releases/ && rm -f latest; ln -snf \$(ls -t | head -n 1) latest; popd' || true"
+                    }
+                }
+            }
+        }
+    }
+
+    post {
+        cleanup {
+            deleteDir()
+        }
+    }
+}
