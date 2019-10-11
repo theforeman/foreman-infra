58240da34a685aa77e92615e14ec13f26ffc3d86
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-10-11 10:51:30 +0200

    Allow turning off the receiver setup in web
    This makes testing a lot easier, even if it's less realistic.

--- puppet/spec/classes/web_spec.rb
+++ puppet/spec/classes/web_spec.rb
@@ -4,6 +4,7 @@ describe 'web' do
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) { facts }
+      let(:params) { {setup_receiver: false} }
 
       it { is_expected.to compile.with_all_deps }
     end
