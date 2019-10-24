c507f53134ca2af1eadba9b9e4c91f01af72aea9
Evgeni Golov <evgeni@golov.de>
2019-10-24 11:22:27 +0200

    add 1.24 vars

---
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/vars/foreman/1.24.groovy
@@ -0,0 +1,18 @@
+def foreman_version = '1.24'
+def foreman_client_distros = [
+    'el7',
+    'el6',
+    'el5',
+    'fc29',
+    'fc28',
+    'sles12',
+    'sles11'
+]
+def foreman_server_distros = [
+    'centos7',
+    'debian9',
+    'debian10',
+    'ubuntu1604',
+    'ubuntu1804'
+]
+
