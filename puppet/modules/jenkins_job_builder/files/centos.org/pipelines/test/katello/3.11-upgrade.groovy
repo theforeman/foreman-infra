568658228f34fbe665368726fb1e6cebdaf2ea1e
Evgeni Golov <evgeni@golov.de>
2019-11-06 15:12:48 +0100

    drop 3.10/3.11 vars

--- puppet/modules/jenkins_job_builder/files/centos.org/pipelines/test/katello/3.11-upgrade.groovy
+++
@@ -1,4 +0,0 @@
-def playBookVars() {
-    playBook = ['boxes': ['pipeline-upgrade-katello-3.11-centos7'], 'pipeline': 'upgrade_pipeline.yml', 'extraVars': ['pipeline_version': '3.11', 'pipeline_os': 'centos7', 'pipeline_type': 'katello']]
-    return playBook
-}
