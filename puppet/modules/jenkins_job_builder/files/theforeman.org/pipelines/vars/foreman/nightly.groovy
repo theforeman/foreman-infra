0afe90f97817a2fd97318303bec7ff5cd5cb70bd
Tomer Brisker <tbrisker@gmail.com>
2019-11-17 13:26:55 +0200

    Drop xenial from nightly pipeline

--- puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/vars/foreman/nightly.groovy
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/vars/foreman/nightly.groovy
@@ -11,8 +11,7 @@ def foreman_server_distros = [
     'centos7',
     'debian9',
     'debian10',
-    'ubuntu1604',
     'ubuntu1804'
 ]
 
-def foreman_debian_releases = ['stretch', 'buster', 'xenial', 'bionic']
+def foreman_debian_releases = ['stretch', 'buster', 'bionic']
