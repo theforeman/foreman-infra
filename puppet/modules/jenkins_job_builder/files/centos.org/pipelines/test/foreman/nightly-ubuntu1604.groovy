089a9f51a6b4613ba92546f96382aa7997b2dc6b
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-11-21 18:16:06 +0100

    Drop Xenial and Stretch from nightly

--- puppet/modules/jenkins_job_builder/files/centos.org/pipelines/test/foreman/nightly-ubuntu1604.groovy
+++
@@ -1,4 +0,0 @@
-def playBookVars() {
-    playBook = ['boxes': ['pipeline-foreman-server-nightly-ubuntu1604'], 'pipeline': 'install_pipeline.yml', 'extraVars': ['pipeline_version': 'nightly', 'pipeline_os': 'ubuntu1604', 'pipeline_type': 'foreman']]
-    return playBook
-}
