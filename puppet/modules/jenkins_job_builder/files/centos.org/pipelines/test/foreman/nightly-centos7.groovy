58543d855afd593ed7e7ac8ad09ae2d37efdd2cd
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-12-13 18:09:11 +0100

    Collect smoker logs in foreman nightly

--- puppet/modules/jenkins_job_builder/files/centos.org/pipelines/test/foreman/nightly-centos7.groovy
+++ puppet/modules/jenkins_job_builder/files/centos.org/pipelines/test/foreman/nightly-centos7.groovy
@@ -1,4 +1,12 @@
 def playBookVars() {
-    playBook = ['boxes': ['pipeline-foreman-server-nightly-centos7'], 'pipeline': 'install_pipeline.yml', 'extraVars': ['pipeline_version': 'nightly', 'pipeline_os': 'centos7', 'pipeline_type': 'foreman']]
+    playBook = [
+      'boxes': ['pipeline-foreman-server-nightly-centos7', 'pipeline-foreman-smoker-nightly-centos7'],
+      'pipeline': 'install_pipeline.yml',
+      'extraVars': [
+        'pipeline_version': 'nightly',
+        'pipeline_os': 'centos7',
+        'pipeline_type': 'foreman'
+      ]
+    ]
     return playBook
 }
