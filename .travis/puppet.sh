8797a7a1095124f3964e30750a1aaef1860684f4
Michael Moll <kvedulv@kvedulv.de>
2019-12-23 23:52:19 +0100

    move external modules to a separate directory

--- .travis/puppet.sh
+++ .travis/puppet.sh
@@ -2,5 +2,6 @@
 
 cd puppet
 mv forge_modules/* modules/
+mv git_modules/* modules/
 rake syntax
 rake spec
--- puppet/modules/deploy/files/script.sh
+++ puppet/modules/deploy/files/script.sh
@@ -5,5 +5,6 @@ dir=`mktemp -d`
 trap "rm -rf ${dir}" EXIT
 git clone --recurse-submodules https://github.com/theforeman/foreman-infra ${dir}/
 mv ${dir}/puppet/forge_modules/* ${dir}/puppet/modules/
+mv ${dir}/puppet/git_modules/* ${dir}/puppet/modules/
 rsync -aqx --delete-after --exclude=.git ${dir}/puppet/modules/ /etc/puppetlabs/code/environments/production/modules/
 echo "Deploy complete at `date`"
