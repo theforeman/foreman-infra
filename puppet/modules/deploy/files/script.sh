4fbd3207a95fd9e25a271877beb298d591bfe1ef
Michael Moll <kvedulv@kvedulv.de>
2019-12-23 23:43:40 +0100

    move puppet forge modules to a separate directory

--- puppet/modules/deploy/files/script.sh
+++ puppet/modules/deploy/files/script.sh
@@ -2,7 +2,8 @@
 set -e # dont rsync if clone fails
 echo "Deploy started at `date`"
 dir=`mktemp -d`
-trap "rm -rf $dir" EXIT
-git clone --recurse-submodules https://github.com/theforeman/foreman-infra $dir/
-rsync -aqx --delete-after --exclude=.git $dir/puppet/modules/ /etc/puppetlabs/code/environments/production/modules/
+trap "rm -rf ${dir}" EXIT
+git clone --recurse-submodules https://github.com/theforeman/foreman-infra ${dir}/
+mv ${dir}/puppet/forge_modules/* ${dir}/puppet/modules/
+rsync -aqx --delete-after --exclude=.git ${dir}/puppet/modules/ /etc/puppetlabs/code/environments/production/modules/
 echo "Deploy complete at `date`"
