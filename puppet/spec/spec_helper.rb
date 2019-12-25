20e81f98d4aa8299f85c00958a1ed2ee898db33b
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-12-25 18:37:27 +0100

    Refactor testing to a control module

--- puppet/spec/spec_helper.rb
+++ puppet/spec/spec_helper.rb
@@ -1,11 +1,35 @@
-RSpec.configure do |c|
-  c.mock_with :rspec
-end
-
-require 'puppetlabs_spec_helper/module_spec_helper'
+require 'rspec-puppet'
 require 'rspec-puppet-facts'
 include RspecPuppetFacts
 
 add_custom_fact :root_home, '/root'
 add_custom_fact :rvm_installed, false
 add_custom_fact :sudoversion, '1.8.23'
+
+def on_supported_os(opts = {})
+  opts[:supported_os] ||= [
+    {
+      'operatingsystem'        => 'CentOS',
+      'operatingsystemrelease' => ['7'],
+    },
+    {
+      'operatingsystem'        => 'Debian',
+      'operatingsystemrelease' => ['10', '9'],
+    },
+    {
+      'operatingsystem'        => 'Ubuntu',
+      'operatingsystemrelease' => ['16.04'],
+    },
+  ]
+  super(opts)
+end
+
+base_dir = File.dirname(File.expand_path(__dir__))
+
+RSpec.configure do |c|
+  c.module_path     = [File.join(base_dir, 'modules'), File.join(base_dir, 'git_modules'), File.join(base_dir, 'forge_modules'), File.join(base_dir, 'test_modules')].join(File::PATH_SEPARATOR)
+  c.manifest_dir    = File.join(base_dir, 'manifests')
+  c.manifest        = File.join(base_dir, 'manifests', 'site.pp')
+  c.environmentpath = base_dir
+  c.strict_variables = true
+end
