31d8f2a6e87df191d17b93a556617f050cd67b9c
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-12-31 14:55:26 +0100

    Install jenkins-job-builder using Python 3

--- puppet/modules/jenkins_job_builder/manifests/install.pp
+++ puppet/modules/jenkins_job_builder/manifests/install.pp
@@ -4,28 +4,18 @@
 # https://git.openstack.org/cgit/openstack-infra/puppet-jenkins/tree/manifests/job_builder.pp
 # and should probably be kept up to date from there
 class jenkins_job_builder::install {
-  ensure_packages(['python-pip'])
+  ensure_packages(['python3-pip'])
 
-  Package['python-pip'] -> Package <| provider == 'pip' |>
+  Package['python3-pip'] -> Package <| provider == 'pip3' |>
 
-  # A lot of things need yaml, be conservative requiring this package to avoid
-  # conflicts with other modules.
-  if ! defined(Package['PyYAML']) {
-    package { 'PyYAML':
-      ensure => present,
-    }
-  }
-
-  if ! defined(Package['python-jenkins']) {
-    package { 'python-jenkins':
-      ensure   => present,
-      provider => 'pip',
-    }
+  package { 'python36-PyYAML':
+    ensure => present,
   }
 
   package { 'jenkins-job-builder':
     ensure   => present,
-    provider => 'pip',
+    provider => 'pip3',
+    require  => Package['python36-PyYAML'],
   }
 
   file { '/etc/jenkins_jobs':
