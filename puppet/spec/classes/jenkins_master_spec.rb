1a2a47adf16a41eac70bcc700a5d5479088678e1
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-12-31 14:58:45 +0100

    Drop code to remove Java 1.8 from the server

--- puppet/spec/classes/jenkins_master_spec.rb
+++ puppet/spec/classes/jenkins_master_spec.rb
@@ -6,7 +6,6 @@ describe 'jenkins_master' do
       let(:facts) { facts }
 
       it { is_expected.to compile.with_all_deps }
-      it { is_expected.to contain_package('java-1.8.0-openjdk').with_ensure('absent').that_requires('Package[java-11-openjdk-headless]') }
     end
   end
 end
