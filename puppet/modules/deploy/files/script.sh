1b0250c1fa0bddedb54d14d5e3e4d204b6448c45
Michael Moll <kvedulv@kvedulv.de>
2019-12-31 19:20:11 +0100

    deploy puppet modules into their native dirs

--- puppet/modules/deploy/files/script.sh
+++ puppet/modules/deploy/files/script.sh
@@ -4,7 +4,12 @@ echo "Deploy started at `date`"
 dir=`mktemp -d`
 trap "rm -rf ${dir}" EXIT
 git clone --recurse-submodules https://github.com/theforeman/foreman-infra ${dir}/
-mv ${dir}/puppet/forge_modules/* ${dir}/puppet/modules/
-mv ${dir}/puppet/git_modules/* ${dir}/puppet/modules/
-rsync -aqx --delete-after --exclude=.git ${dir}/puppet/modules/ /etc/puppetlabs/code/environments/production/modules/
+prod_dir="/etc/puppetlabs/code/environments/production"
+module_dirs=(forge_modules git_modules modules)
+for module_dir in "${module_dirs[@]}"
+do
+  rsync -aqx --delete-after --exclude=.git ${dir}/puppet/${module_dir}/ ${prod_dir}/${module_dir}
+done
+module_dirs_string=$(printf "%s:" "${module_dirs[@]}")
+echo "modulepath = ${module_dirs_string}\$basemodulepath" >${prod_dir}/environment.conf
 echo "Deploy complete at `date`"
