2a176b3de62d4d8c74e62d215d3476bbef07c570
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-01-13 23:50:37 +0100

    Drop nightly debian9 vars
    a1750fa620044620a610ae346ffb3768ea4ad5f3 dropped Debian 9 nightly builds
    but left this file.

--- puppet/modules/jenkins_job_builder/files/centos.org/pipelines/test/foreman/nightly-debian9.groovy
+++
@@ -1,4 +0,0 @@
-def playBookVars() {
-    playBook = ['boxes': ['pipeline-foreman-server-nightly-debian9'], 'pipeline': 'install_pipeline.yml', 'extraVars': ['pipeline_version': 'nightly', 'pipeline_os': 'debian9', 'pipeline_type': 'foreman']]
-    return playBook
-}
