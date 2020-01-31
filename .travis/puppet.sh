50b21bc1315de8449ef06242b0970ef46c8cebc4
Michael Moll <kvedulv@kvedulv.de>
2020-01-31 20:12:46 +0100

    Use g10k to fetch modules from Puppet forge

--- .travis/puppet.sh
+++ .travis/puppet.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -xe
 
+wget https://github.com/xorpaul/g10k/releases/download/v0.8.9/g10k-linux-amd64.zip
+unzip g10k-linux-amd64.zip
+
+./g10k -quiet -puppetfile -puppetfilelocation puppet/Puppetfile_forge -moduledir puppet/forge_modules
+
 cd puppet
 rake syntax
 rake spec
