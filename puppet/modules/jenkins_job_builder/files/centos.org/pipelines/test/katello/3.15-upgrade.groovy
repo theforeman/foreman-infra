2a6dfcdfd69e541aac22c2ed460aa41245861cc8
Evgeni Golov <evgeni@golov.de>
2020-02-27 14:12:41 +0100

    katello jobs should have type=katello

--- puppet/modules/jenkins_job_builder/files/centos.org/pipelines/test/katello/3.15-upgrade.groovy
+++ puppet/modules/jenkins_job_builder/files/centos.org/pipelines/test/katello/3.15-upgrade.groovy
@@ -1,4 +1,4 @@
 def playBookVars() {
-    playBook = pipelineVars(action: 'upgrade', type: 'foreman', version: '3.15', os: 'centos7')
+    playBook = pipelineVars(action: 'upgrade', type: 'katello', version: '3.15', os: 'centos7')
     return playBook
 }
