c45d67d288b93ea446f9a98d394252ecf339e23a
Michael Moll <kvedulv@kvedulv.de>
2020-02-28 23:06:58 +0100

    use correct directory for g10k

--- puppet/modules/deploy/files/script.sh
+++ puppet/modules/deploy/files/script.sh
@@ -4,6 +4,6 @@ echo "Deploy started at `date`"
 dir=`mktemp -d`
 trap "rm -rf ${dir}" EXIT
 git clone --recurse-submodules https://github.com/theforeman/foreman-infra ${dir}/
-g10k -quiet -puppetfile -puppetfilelocation ${dir}/Puppetfile_forge -moduledir ${dir}/forge_modules
+g10k -quiet -puppetfile -puppetfilelocation ${dir}/puppet/Puppetfile_forge -moduledir ${dir}/puppet/forge_modules
 rsync -aqx --delete-after --exclude=.git ${dir}/puppet/* /etc/puppetlabs/code/environments/production/
 echo "Deploy complete at `date`"
