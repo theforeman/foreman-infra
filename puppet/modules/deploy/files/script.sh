b91415d0a799387c817c9b0f4ef33b78f632f6fb
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-04-04 22:50:24 +0200

    Only clone git_modules in Puppet deployment
    This saves cloning the test_modules and the Ansible submodule.

--- puppet/modules/deploy/files/script.sh
+++ puppet/modules/deploy/files/script.sh
@@ -3,7 +3,8 @@ set -e # dont rsync if clone fails
 echo "Deploy started at `date`"
 dir=`mktemp -d`
 trap "rm -rf ${dir}" EXIT
-git clone --recurse-submodules https://github.com/theforeman/foreman-infra ${dir}/
+git clone https://github.com/theforeman/foreman-infra ${dir}/
+git --git-dir ${dir}/.git submodule update --init -- puppet/git_modules
 g10k -quiet -puppetfile -puppetfilelocation ${dir}/puppet/Puppetfile_forge -moduledir ${dir}/puppet/forge_modules
 rsync -aqx --delete-after --exclude=.git ${dir}/puppet/* /etc/puppetlabs/code/environments/production/
 echo "Deploy complete at `date`"
