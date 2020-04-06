766fda19c7d7030ab32f0f3d0b3f3dda6b399016
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-04-06 11:53:04 +0200

    Revert "Only clone git_modules in Puppet deployment"
    This reverts commit 0152014962f7077a316b142bc731cf586fd382a5.

--- puppet/modules/deploy/files/script.sh
+++ puppet/modules/deploy/files/script.sh
@@ -3,8 +3,7 @@ set -e # dont rsync if clone fails
 echo "Deploy started at `date`"
 dir=`mktemp -d`
 trap "rm -rf ${dir}" EXIT
-git clone https://github.com/theforeman/foreman-infra ${dir}/
-git --git-dir ${dir}/.git submodule update --init -- puppet/git_modules
+git clone --recurse-submodules https://github.com/theforeman/foreman-infra ${dir}/
 g10k -quiet -puppetfile -puppetfilelocation ${dir}/puppet/Puppetfile_forge -moduledir ${dir}/puppet/forge_modules
 rsync -aqx --delete-after --exclude=.git ${dir}/puppet/* /etc/puppetlabs/code/environments/production/
 echo "Deploy complete at `date`"
