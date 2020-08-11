a9a012d07b9a610247d456e8e7360d44da4eb17c
Eric D. Helms <ericdhelms@gmail.com>
2020-08-11 09:10:29 -0400

    Skip EL8 for Katello 3.15, 3.16, 3.17

--- koji/collection-mash-split.py
+++ koji/collection-mash-split.py
@@ -458,7 +458,7 @@ def main():
             pulpcore.name = 'katello-pulpcore-{}-el7'.format(version)
             mashes.append(pulpcore)
 
-            if version != '3.15':
+            if version not in ['3.15', '3.16', '3.17']:
                 el8_katello = MashConfig(collection, version, "el8", "el8", "katello/el8")
                 mashes.append(el8_katello)
 
