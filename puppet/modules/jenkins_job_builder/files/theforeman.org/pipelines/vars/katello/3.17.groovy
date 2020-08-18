dbb5277541fbc90a0cd157d3ef7fcb8578833fc1
Eric D. Helms <ericdhelms@gmail.com>
2020-08-18 15:02:56 -0400

    Add Katello 3.17

---
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/vars/katello/3.17.groovy
@@ -0,0 +1,13 @@
+def foreman_version = '2.2'
+def katello_version = '3.17'
+def foreman_el_releases = [
+    'el7'
+]
+def pipelines = [
+    'install': [
+        'centos7'
+    ],
+    'upgrade': [
+        'centos7'
+    ]
+]
