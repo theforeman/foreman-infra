557b8c5cd8a544b21c57b9688d964cd34bc7fe73
Tomer Brisker <tbrisker@gmail.com>
2020-08-21 17:46:36 +0300

    Update web profile for 2.2 manual

--- puppet/modules/profiles/manifests/web.pp
+++ puppet/modules/profiles/manifests/web.pp
@@ -22,7 +22,7 @@
 #   Set up the SSH receiver setup. Mostly turned off for testing.
 class profiles::web (
   String[1] $stable = '2.1',
-  String[1] $next = '2.2',
+  String[1] $next = '2.3',
   Hash[String, Hash] $debugs_htpasswds = {},
   Boolean $https = true,
   Integer[0] $rsync_max_connections = 5,
