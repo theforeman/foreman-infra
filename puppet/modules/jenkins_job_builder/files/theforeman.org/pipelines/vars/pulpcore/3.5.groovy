ad2da58e0058434d07d627e0847420ba8881937e
Evgeni Golov <evgeni@golov.de>
2020-08-27 15:45:17 +0200

    add pulpcore release jobs

--- puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/vars/pulpcore/3.5.groovy
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/vars/pulpcore/3.5.groovy
@@ -1,1 +1,3 @@
+def pulpcore_version = '3.5'
+def pulpcore_distros = ['el7', 'el8']
 def packaging_branch = 'rpm/3.5'
--- puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/vars/pulpcore/3.6.groovy
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/vars/pulpcore/3.6.groovy
@@ -1,1 +1,3 @@
+def pulpcore_version = '3.6'
+def pulpcore_distros = ['el7', 'el8']
 def packaging_branch = 'rpm/3.6'
