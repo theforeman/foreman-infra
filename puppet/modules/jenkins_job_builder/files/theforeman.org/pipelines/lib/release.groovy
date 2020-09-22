dc77cafd0abdce91fa50de3000d4167fbe94b1ac
Evgeni Golov <evgeni@golov.de>
2020-09-22 10:07:57 +0200

    also deploy to web01

--- puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/lib/release.groovy
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/lib/release.groovy
@@ -9,7 +9,8 @@ void push_rpms(repo_src, repo_dest, version, distro) {
 
 void push_rpms_direct(repo_source, repo_dest, overwrite = true, merge = false) {
     sshagent(['repo-sync']) {
-        sh "ssh yumrepo@web02.theforeman.org ${repo_source} ${repo_dest} ${overwrite} ${merge}"
+        sh "ssh yumrepo@web02.rackspace.theforeman.org ${repo_source} ${repo_dest} ${overwrite} ${merge}"
+        sh "ssh yumrepo@web01.osuosl.theforeman.org ${repo_source} ${repo_dest} ${overwrite} ${merge} || true"
     }
 }
 
