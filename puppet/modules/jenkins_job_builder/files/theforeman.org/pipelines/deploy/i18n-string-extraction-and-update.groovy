63e8101ec412a1d73f17d31ef077a6f48770a4bf
Amit Upadhye <upadhyeammit@gmail.com>
2020-10-06 16:15:14 +0530

    Add job definition, dont delete git branch

--- puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/deploy/i18n-string-extraction-and-update.groovy
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/deploy/i18n-string-extraction-and-update.groovy
@@ -1,18 +1,18 @@
 pipeline {
   agent { label 'admin && sshkey' }
   environment {
-  ruby_version = '2.5'
-  triggers {
-  	 TZ='Asia/Kolkata'
-  	 cron('0 10 */7 * *')
+    ruby_version = '2.5'
   }
+  triggers {
+    cron('30 4 */7 * *')
   }
   stages {
     stage('Setup Environment') {
       steps {
         git url: 'https://github.com/theforeman/foreman', branch: 'develop'
         script {
-          sh 'git checkout -b i18n-updates'
+          sh '(git show-branch i18n-updates &>/dev/null) && (git checkout i18n-updates) || (git checkout -b i18n-updates)'
+          sh 'git rebase origin/develop'
           configureRVM(ruby_version)
           withRVM(['bundle install --jobs=5 --retry=5'], ruby_version)
           sh 'cp config/database.yml.example config/database.yml'
@@ -22,66 +22,27 @@ pipeline {
     stage('Extract strings and update translations'){
       steps {
         script {
-          withCredentials([string(credentialsId: 'transifex-api-token', variable: 'TX_TOKEN')]) {
-            sh 'make -C locale/ tx-update'
+          configureRVM(ruby_version)
+          withRVM([withCredentials([string(credentialsId: 'transifex-api-token', variable: 'TX_TOKEN')]) {
+            'make -C locale/ tx-update'}], ruby_version)
           }
         }
       }
-    }
     stage('Test new strings'){
       steps {
         script {
-          sh 'make -C locale/ all-mo'
+          configureRVM(ruby_version)
+          withRVM(['make -C locale/ all-mo'], ruby_version)
         }
       }
     }
     stage('Create PR with updated strings') {
       steps {
         script {
-          sh 'git commit --amend i18n - extracting new, pulling from tx(jenkins-auto-update)'
-          sh 'git branch -D i18n-updates'
+          sh "git commit --amend -m 'i18n - extracting new, pulling from tx(jenkins-auto-update)'"
+          sh 'git push origin i18n-updates -f'
         }
       }
     }
   }
-}
-
-def gemset(name = null) {
-
-    def base_name = env.BUILD_TAG
-
-    if (EXECUTOR_NUMBER != '0') {
-        base_name += '-' + EXECUTOR_NUMBER
-    }
-
-    if (name) {
-        base_name += '-' + name.replace(".", "-")
-    }
-
-    base_name
-}
-
-def configureRVM(ruby = '2.0', name = '') {
-    emptyGemset(ruby, name)
-    withRVM(["gem install bundler -v '< 2.0'"], ruby, name)
-}
-
-def emptyGemset(ruby = '2.0', name = '') {
-    withRVM(["rvm gemset empty ${gemset(name)} --force"], ruby, name)
-}
-
-def cleanupRVM(ruby = '2.0', name = '') {
-    withRVM(["rvm gemset delete ${gemset(name)} --force"], ruby, name)
-}
-
-def withRVM(commands, ruby = '2.0', name = '') {
-
-    commands = commands.join("\n")
-    echo commands.toString()
-
-    sh """#!/bin/bash -l
-        set +e
-        rvm use ruby-${ruby}@${gemset(name)} --create
-        ${commands}
-    """
 }
