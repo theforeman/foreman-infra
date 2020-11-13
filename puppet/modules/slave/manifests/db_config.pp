2146dfe4d8c5205d8f9fcb5816eed6b44c392e8a
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-11-13 15:51:30 +0100

    Add data types to db_config

--- puppet/modules/slave/manifests/db_config.pp
+++ puppet/modules/slave/manifests/db_config.pp
@@ -1,13 +1,19 @@
 # @api private
-define slave::db_config(
-  $ensure = 'present',
-  $jenkins_home = '/home/jenkins',
-  $jenkins_user = 'jenkins',
-  $jenkins_group = 'jenkins'
+define slave::db_config (
+  Enum['file', 'absent'] $ensure = 'file',
+  Stdlib::Absolutepath $jenkins_home = '/home/jenkins',
+  String[1] $jenkins_user = 'jenkins',
+  String[1] $jenkins_group = 'jenkins',
 ) {
+  if $ensure == 'file' {
+    $content = file("${module_name}/db/${title}")
+  } else {
+    $content = undef
+  }
+
   file { "${jenkins_home}/${title}.db.yaml":
-    ensure  => file,
-    content => file("${module_name}/db/${title}"),
+    ensure  => $ensure,
+    content => $content,
     owner   => $jenkins_user,
     group   => $jenkins_group,
   }
