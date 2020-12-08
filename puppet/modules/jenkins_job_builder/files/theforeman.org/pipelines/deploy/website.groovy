7d9822acb77c2edbef609ef884b33036ac7576b0
Evgeni Golov <evgeni@golov.de>
2020-12-08 10:54:14 +0100

    add structured changeg-logging to website deploy
    this will be later used for cache invalidation

--- puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/deploy/website.groovy
+++ puppet/modules/jenkins_job_builder/files/theforeman.org/pipelines/deploy/website.groovy
@@ -12,6 +12,7 @@ pipeline {
         ruby_version = '2.5'
         // Sync to the pivot-point on the web node
         target_path = 'website@web01.osuosl.theforeman.org:rsync_cache/'
+        rsync_log = 'deploy-website.log'
     }
 
     stages {
@@ -33,8 +34,13 @@ pipeline {
                     // Copy the site to the web node
                     // Dependencies
                     // * the web node must have the web class
-                    sh "/usr/bin/rsync --archive --checksum --verbose --one-file-system --compress --stats --delete-after ./_site/ ${target_path}"
+                    sh "/usr/bin/rsync --log-file '${rsync_log}' --log-file-format 'CHANGED %f' --archive --checksum --verbose --one-file-system --compress --stats --delete-after ./_site/ ${target_path}"
                 }
+
+                sh "cat '${rsync_log}'"
+                // this should become something like this later:
+                // sh "awk '/ CHANGED /{print $5}' '${rsync_log}' | xargs --no-run-if-empty fastly-purge 'https://theforeman.org/'"
+                sh "rm '${rsync_log}'"
             }
         }
     }
