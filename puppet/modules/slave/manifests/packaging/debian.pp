14c107676c81dff386cffb6a891c32c3f8857888
Evgeni Golov <evgeni@golov.de>
2021-01-27 15:16:21 +0100

    drop case around arch, we only have one

--- puppet/modules/slave/manifests/packaging/debian.pp
+++ puppet/modules/slave/manifests/packaging/debian.pp
@@ -20,30 +20,25 @@ class slave::packaging::debian(
   } ->
   Class['Pbuilder::Common']
 
-  case $facts['os']['architecture'] {
-    'amd64': {
-      slave::pbuilder_setup {
-        'bionic64':
-          ensure     => present,
-          arch       => 'amd64',
-          release    => 'bionic',
-          apturl     => $ubuntu_mirror,
-          aptcontent => "deb $ubuntu_mirror bionic main restricted universe\ndeb-src $ubuntu_mirror bionic main restricted universe\n";
-        'buster64':
-          ensure     => present,
-          arch       => 'amd64',
-          release    => 'buster',
-          apturl     => $debian_mirror,
-          aptcontent => "deb $debian_mirror buster main non-free contrib\ndeb-src $debian_mirror buster main non-free contrib\n";
-        'focal64':
-          ensure     => present,
-          arch       => 'amd64',
-          release    => 'focal',
-          apturl     => $ubuntu_mirror,
-          aptcontent => "deb $ubuntu_mirror focal main restricted universe\ndeb-src $ubuntu_mirror focal main restricted universe\n";
-      }
-    }
-    default: {}
+  slave::pbuilder_setup {
+    'bionic64':
+      ensure     => present,
+      arch       => 'amd64',
+      release    => 'bionic',
+      apturl     => $ubuntu_mirror,
+      aptcontent => "deb $ubuntu_mirror bionic main restricted universe\ndeb-src $ubuntu_mirror bionic main restricted universe\n";
+    'buster64':
+      ensure     => present,
+      arch       => 'amd64',
+      release    => 'buster',
+      apturl     => $debian_mirror,
+      aptcontent => "deb $debian_mirror buster main non-free contrib\ndeb-src $debian_mirror buster main non-free contrib\n";
+    'focal64':
+      ensure     => present,
+      arch       => 'amd64',
+      release    => 'focal',
+      apturl     => $ubuntu_mirror,
+      aptcontent => "deb $ubuntu_mirror focal main restricted universe\ndeb-src $ubuntu_mirror focal main restricted universe\n";
   }
 
   shellvar { 'extend_pbuilder_path':
