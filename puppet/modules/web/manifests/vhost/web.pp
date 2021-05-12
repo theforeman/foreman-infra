theforeman/foreman-infra

commit c7c69ae4ec77d794f536ffe13bb8ea728dd0dba5

Modify rewrite rules for apidocs subdomain

puppet/modules/web/manifests/vhost/web.pp
@@ -26,12 +26,14 @@ class web::vhost::web(
     { 'rewrite_rule' => ['^/wiki(.*)  https://projects.theforeman.org/wiki$1  [R,L]'] },
     { 'rewrite_rule' => ['^/events/all.ics https://community.theforeman.org/c/events/l/calendar.ics [R,L]'] },
     { 'rewrite_rule' => ['^/events/? https://community.theforeman.org/c/events/l/calendar [R,L]'] },
+    { 'rewrite_rule' => ["^/api/latest(.*) https://apidocs.theforeman.org/foreman/${stable}\$1 [R,L]"] },
+    { 'rewrite_rule' => ['^/api/(.*) https://apidocs.theforeman.org/foreman/\$1 [R,L]'] },
+    { 'rewrite_rule' => ['^/api$ https://apidocs.theforeman.org/foreman/ [R,L]'] },
   ]
 
   $docs_rewrites = [
     { 'rewrite_rule' => ["^/manuals/latest(.*) /manuals/${stable}\$1 [R,L]"] },
     { 'rewrite_rule' => ["^/manuals/${next}(.*) /manuals/nightly\$1 [R,L]"] },
-    { 'rewrite_rule' => ["^/api/latest(.*) /api/${stable}\$1 [R,L]"] },
   ]
 
   $directory_config = [

commit dbf479234a3bf4e75a4bb56ab42e98df3bde47a8

Correct API docs redirect
In c7c69ae4ec77d794f536ffe13bb8ea728dd0dba5 the apidoc redirect was
made. However, it copied a line with variables and then changed to
single quotes. That meant escaping was no longer needed. The result:

  $ curl https://theforeman.org/api/2.4/apidoc/v2/compute_profiles/create.html -si | grep Location
  Location: https://apidocs.theforeman.org/foreman/$1

Fixes: c7c69ae4ec77d794f536ffe13bb8ea728dd0dba5

puppet/modules/web/manifests/vhost/web.pp
@@ -27,7 +27,7 @@ class web::vhost::web(
     { 'rewrite_rule' => ['^/events/all.ics https://community.theforeman.org/c/events/l/calendar.ics [R,L]'] },
     { 'rewrite_rule' => ['^/events/? https://community.theforeman.org/c/events/l/calendar [R,L]'] },
     { 'rewrite_rule' => ["^/api/latest(.*) https://apidocs.theforeman.org/foreman/${stable}\$1 [R,L]"] },
-    { 'rewrite_rule' => ['^/api/(.*) https://apidocs.theforeman.org/foreman/\$1 [R,L]'] },
+    { 'rewrite_rule' => ['^/api/(.*) https://apidocs.theforeman.org/foreman/$1 [R,L]'] },
     { 'rewrite_rule' => ['^/api$ https://apidocs.theforeman.org/foreman/ [R,L]'] },
   ]
 