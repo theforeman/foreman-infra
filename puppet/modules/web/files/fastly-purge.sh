theforeman/foreman-infra

commit 875bb689ce2d52697de7c9a077719f33844143c0

purge the contents of "latest" from the CDN when it changes

puppet/modules/web/files/fastly-purge.sh
@@ -1,0 +1,11 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+BASE=${1}
+shift
+
+for purge in $@; do
+  curl --silent -X PURGE -H 'Fastly-Saft-Purge:1' ${BASE}/${purge}
+done

puppet/modules/web/files/deploy-yumrepo.sh
@@ -64,11 +64,11 @@ replace() {
 }
 
 purgecdn() {
-	awk '/ CHANGED /{print "https://yum.theforeman.org/'${REPO_DEST}'/"$5}' "${REPO_RSYNC_LOG}" | xargs --no-run-if-empty curl --silent -X PURGE -H 'Fastly-Soft-Purge:1'
+	awk '/ CHANGED /{print $5}' "${REPO_RSYNC_LOG}" | xargs --no-run-if-empty fastly-purge "https://yum.theforeman.org/${REPO_DEST}"
 	set +f
 	for d in "${REPO_PATH}"/*; do
 		purge_base="https://yum.theforeman.org/${REPO_DEST}/$(basename $d)"
-		curl --silent -X PURGE -H 'Fastly-Soft-Purge:1' ${purge_base}/foreman-release.rpm ${purge_base}/foreman-client-release.rpm
+		fastly-purge ${purge_base} foreman-release.rpm foreman-client-release.rpm
 	done
 	set -f
 }

puppet/modules/web/files/fastly-purge-find.sh
@@ -1,0 +1,12 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+URLBASE=${1}
+BASEDIR=${2}
+PURGE=${3}
+
+pushd $BASEDIR
+  find $PURGE -type f | xargs --no-run-if-empty fastly-purge "${URLBASE}"
+popd

puppet/modules/web/manifests/init.pp
@@ -62,6 +62,22 @@ class web(
     source => 'puppet:///modules/web/filter_apache_stats.sh',
   }
 
+  file { '/usr/local/bin/fastly-purge':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    content => file('web/fastly-purge.sh'),
+  }
+
+  file { '/usr/local/bin/fastly-purge-find':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    content => file('web/fastly-purge-find.sh'),
+  }
+
   # daily at 4am, should be fairly quiet on the server
   cron { 'filter_apache_stats':
     command => '/usr/bin/nice -19 /usr/local/bin/filter_apache_stats',

puppet/modules/web/manifests/vhost/yum.pp
@@ -84,7 +84,21 @@ class web::vhost::yum (
     file { "${yum_directory}/${directory}/latest":
       ensure => link,
       target => $stable,
+      notify => Exec["fastly-purge-${directory}-latest"],
     }
+
+    exec { "fastly-purge-${directory}-latest":
+      command     => "fastly-purge-find 'https://${servername}' ${yum_directory} ${directory}/latest/",
+      path        => '/bin:/usr/bin:/usr/local/bin',
+      refreshonly => true,
+    }
+  }
+
+  exec { "fastly-purge-root-latest":
+    command     => "fastly-purge-find 'https://${servername}' ${yum_directory} latest/",
+    path        => '/bin:/usr/bin:/usr/local/bin',
+    subscribe   => File["${yum_directory}/releases/latest"],
+    refreshonly => true,
   }
 
   file { "${yum_directory}/releases/nightly":

commit 619f22eb1acafbdaa8e18e4479dfaf337e6b4fa7

Correct typo in Fastly-Soft-Purge header
https://docs.fastly.com/en/guides/soft-purges

Fixes: 875bb689ce2d52697de7c9a077719f33844143c0

puppet/modules/web/files/fastly-purge.sh
@@ -7,5 +7,5 @@ BASE=${1}
 shift
 
 for purge in $@; do
-  curl --silent -X PURGE -H 'Fastly-Saft-Purge:1' ${BASE}/${purge}
+  curl --silent -X PURGE -H 'Fastly-Soft-Purge:1' ${BASE}/${purge}
 done