8a3dee751286d1cb1e6e6b5b9e0b2a53de2a73bb
Evgeni Golov <evgeni@golov.de>
2021-08-30 16:30:31 +0200

    don't env-reset jenkins when doing sudo
    the way pbuilder works under the hood, it ends up calling sudo inside
    the pbuilder run, and drops the env

--- puppet/spec/classes/slave_spec.rb
+++ puppet/spec/classes/slave_spec.rb
@@ -13,7 +13,7 @@ describe 'slave' do
         end
         it { is_expected.to compile.with_all_deps }
         if facts[:osfamily] == 'Debian'
-          it { is_expected.to contain_users__account('jenkins').with_sudo('ALL=NOPASSWD: ALL') }
+          it { is_expected.to contain_users__account('jenkins').with_sudo("ALL=NOPASSWD: ALL\nDefaults:jenkins env_keep += FOREMAN_VERSION") }
         else
           it { is_expected.to contain_users__account('jenkins').with_sudo('') }
         end
