b252b3af6b78a095f5193c9125e2ba114431cdf6
Evgeni Golov <evgeni@golov.de>
2021-09-01 11:31:53 +0200

    Revert "don't env-reset jenkins when doing sudo"
    This reverts commit 61352837a54d7d53780845f90c72ba2fef3a6cc4.

--- puppet/spec/classes/slave_spec.rb
+++ puppet/spec/classes/slave_spec.rb
@@ -13,7 +13,7 @@ describe 'slave' do
         end
         it { is_expected.to compile.with_all_deps }
         if facts[:osfamily] == 'Debian'
-          it { is_expected.to contain_users__account('jenkins').with_sudo("ALL=NOPASSWD: ALL\nDefaults:jenkins env_keep += FOREMAN_VERSION") }
+          it { is_expected.to contain_users__account('jenkins').with_sudo('ALL=NOPASSWD: ALL') }
         else
           it { is_expected.to contain_users__account('jenkins').with_sudo('') }
         end
