a5ac88fb677574e6758666688532e92d14854e8d
Evgeni Golov <evgeni@golov.de>
2021-09-21 13:34:46 +0200

    don't install additional packages on EL8

--- puppet/modules/utility/manifests/init.pp
+++ puppet/modules/utility/manifests/init.pp
@@ -8,7 +8,12 @@ class utility($sysadmins = ['/dev/null']) {
     default  => 'vim',
   }
 
-  ensure_packages([$vim, 'htop', 'iftop', 'screen'])
+  if $facts['os']['family'] == 'RedHat' and $facts['os']['release']['major'] == '8' {
+    $additional_packages = []
+  } else {
+    $additional_packages = ['htop', 'iftop', 'screen']
+  }
+  ensure_packages([$vim] + $additional_packages)
 
   # TODO: rsync package is managed by puppetlabs-rsync
 
