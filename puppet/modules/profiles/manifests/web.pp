dd742a53111c3b0cf56542a9a21032eb130dc9ff
Amit Upadhye <upadhyeammit@gmail.com>
2021-09-23 18:38:12 +0530

    Update web class stable param to 3.0

--- puppet/modules/profiles/manifests/web.pp
+++ puppet/modules/profiles/manifests/web.pp
@@ -21,7 +21,7 @@
 # @param setup_receiver
 #   Set up the SSH receiver setup. Mostly turned off for testing.
 class profiles::web (
-  String[1] $stable = '2.5',
+  String[1] $stable = '3.0',
   String[1] $next = '3.1',
   Hash[String, Hash] $debugs_htpasswds = {},
   Boolean $https = true,
