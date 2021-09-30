theforeman/foreman-infra

commit 70c725348f7ca01c7704db7d0adedf45bd7c1381

Install Puppet git modules via g10k
This allows g10k to cache and unifies the deployment.

puppet/Puppetfile
@@ -1,14 +1,16 @@
-moduledir 'forge_modules'
+moduledir 'external_modules'
 
 mod 'camptocamp/pbuilder', '0.1.11'
 mod 'camptocamp/systemd', '2.9.0'
 mod 'citrin/htpasswd', '1.0.1'
 mod 'claranet/timezone', '2.0.1'
 mod 'golja/gnupg', '1.2.3'
+mod 'gwmngilfen/dirvish' :git => 'https://github.com/GregSutcliffe/puppet-dirvish', :ref => 'cfad9c4bed8911ab7674ce86866890e60a62ae31'
 mod 'herculesteam/augeasproviders_core', '2.6.0'
 mod 'herculesteam/augeasproviders_mounttab', '2.0.3'
 mod 'herculesteam/augeasproviders_shellvar', '4.0.0'
 mod 'herculesteam/augeasproviders_ssh', '3.3.0'
+mod 'nightfly19/external_ip', :git => 'https://github.com/nightfly19/puppet-external_ip', :ref => '883ca56888be75418f854f77e24665c1363c8357'
 mod 'puppet/archive', '4.4.0'
 mod 'puppet/chrony', '2.0.0'
 mod 'puppet/epel', '3.0.1'
@@ -16,6 +18,7 @@ mod 'puppet/extlib', '5.0.0'
 mod 'puppet/jenkins', '2.0.0'
 mod 'puppet/letsencrypt', '5.0.0'
 mod 'puppet/nodejs', '8.0.0'
+mod 'puppet/rvm', :git => 'https://github.com/voxpupuli/puppet-rvm.git', :ref => '2dae1b78d7dd8272a6a4f9106234d8969d7d5a72'
 mod 'puppet/selinux', '3.1.0'
 mod 'puppet/unattended_upgrades', '6.0.0'
 mod 'puppetlabs/apache', '5.4.0'

.github/workflows/main.yml
@@ -26,7 +26,7 @@ jobs:
           wget https://github.com/xorpaul/g10k/releases/download/v0.8.9/g10k-linux-amd64.zip
           unzip g10k-linux-amd64.zip
       - name: Install modules using g10k
-        run: ./g10k -cachedir .g10k/cache -puppetfile -puppetfilelocation Puppetfile_forge -moduledir forge_modules
+        run: ./g10k -cachedir .g10k/cache -puppetfile
       - name: Run syntax
         run: bundle exec rake syntax
       - name: Run lint

.gitignore
@@ -1,7 +1,7 @@
 ssl/*
 yaml/*
 puppet/puppet.conf
-puppet/forge_modules/
+puppet/external_modules/
 g10k
 g10k-linux-amd64.zip
 .g10k/cache

.gitmodules
@@ -1,12 +1,3 @@
-[submodule "puppet/git_modules/rvm"]
-	path = puppet/git_modules/rvm
-	url = https://github.com/maestrodev/puppet-rvm.git
-[submodule "puppet/git_modules/dirvish"]
-	path = puppet/git_modules/dirvish
-	url = https://github.com/GregSutcliffe/puppet-dirvish
-[submodule "puppet/git_modules/external_ip"]
-	path = puppet/git_modules/external_ip
-	url = https://github.com/nightfly19/puppet-external_ip
 [submodule "ansible/roles/Jimdo.fastly"]
 	path = ansible/roles/Jimdo.fastly
 	url = https://github.com/evgeni/ansible-fastly

Vagrantfile
@@ -13,7 +13,7 @@ Vagrant.configure("2") do |config|
   SHELL
 
   config.vm.provision "run puppet", type: 'puppet' do |puppet|
-    puppet.module_path = ["puppet/modules", "puppet/git_modules", "puppet/forge_modules"]
+    puppet.module_path = ["puppet/modules", "puppet/external_modules"]
     puppet.manifests_path = "vagrant/manifests"
     puppet.synced_folder_type = "rsync"
   end

puppet/environment.conf
@@ -1,1 +1,1 @@
-modulepath = forge_modules:git_modules:modules:$basemodulepath
+modulepath = external_modules:modules:$basemodulepath

puppet/modules/deploy/files/script.sh
@@ -4,8 +4,8 @@ echo "Deploy started at `date`"
 dir=`mktemp -d`
 environment=production
 trap "rm -rf ${dir}" EXIT
-git clone --recurse-submodules https://github.com/theforeman/foreman-infra ${dir}/
-g10k -quiet -puppetfile -puppetfilelocation ${dir}/puppet/Puppetfile_forge -moduledir ${dir}/puppet/forge_modules
+git clone https://github.com/theforeman/foreman-infra ${dir}/
+g10k -quiet -puppetfile -puppetfilelocation ${dir}/puppet/Puppetfile -moduledir ${dir}/puppet/external_modules
 rsync -aqx --delete-after --exclude=.git ${dir}/puppet/* /etc/puppetlabs/code/environments/$environment/
 puppet generate types --environment $environment
 echo "Deploy complete at `date`"

puppet/spec/spec_helper.rb
@@ -23,7 +23,7 @@ end
 base_dir = File.dirname(File.expand_path(__dir__))
 
 RSpec.configure do |c|
-  c.module_path     = [File.join(base_dir, 'modules'), File.join(base_dir, 'git_modules'), File.join(base_dir, 'forge_modules'), File.join(base_dir, 'test_modules')].join(File::PATH_SEPARATOR)
+  c.module_path     = [File.join(base_dir, 'modules'), File.join(base_dir, 'external_modules'), File.join(base_dir, 'test_modules')].join(File::PATH_SEPARATOR)
   c.manifest_dir    = File.join(base_dir, 'manifests')
   c.manifest        = File.join(base_dir, 'manifests', 'site.pp')
   c.environmentpath = base_dir

commit c9761ab060653e359c32464c80d854a5b72eced4

Import Greg's puppet-dirvish
The previous module didn't have a metadata.json so it wasn't properly
recognized.

Fixes: 70c725348f7ca01c7704db7d0adedf45bd7c1381

puppet/Puppetfile
@@ -5,7 +5,6 @@ mod 'camptocamp/systemd', '2.9.0'
 mod 'citrin/htpasswd', '1.0.1'
 mod 'claranet/timezone', '2.0.1'
 mod 'golja/gnupg', '1.2.3'
-mod 'gwmngilfen/dirvish' :git => 'https://github.com/GregSutcliffe/puppet-dirvish', :ref => 'cfad9c4bed8911ab7674ce86866890e60a62ae31'
 mod 'herculesteam/augeasproviders_core', '2.6.0'
 mod 'herculesteam/augeasproviders_mounttab', '2.0.3'
 mod 'herculesteam/augeasproviders_shellvar', '4.0.0'

puppet/modules/dirvish/files/dirvish-cronjob
@@ -1,0 +1,43 @@
+#! /bin/sh
+#
+# daily cron job for the dirvish package
+#
+if [ ! `which dirvish-expire`  ]; then exit 1; fi
+if [ ! `which dirvish-runall`  ]; then exit 1; fi
+if [ ! -s /etc/dirvish/master.conf ]; then exit 1; fi
+
+mount_check() {
+	mntout=`tempfile -p mount`
+	mount $1 >$mntout 2>&1
+	if [ ! -d $1/lost+found ]; then # only works for "real" filesystems :-)
+					# (Yes, I know about reiserfs.)
+		echo "'mount $1' failed?! Stopping."
+		echo "mount output:"
+		cat $mntout
+		rm -f $mntout
+		exit 2
+	fi
+
+	if stat $1 | grep 'Inode: 2[^0-9]' >/dev/null; then # ditto
+		rm -f $mntout
+		return 0 # ok
+	fi
+	echo "$1 isn't inode 2 ?! Mount must have failed; stopping."
+	echo ''
+	stat $1
+	echo "mount output:"
+	cat $mntout
+	rm -f $mntout
+	umount $1
+	exit 2
+}
+
+## Example of how to mount and umount a backup partition...
+# mount_check /backup
+
+`which dirvish-expire` --quiet && `which dirvish-runall` --quiet
+rc=$?
+
+# umount /backup || rc=$?
+
+exit $rc

puppet/modules/dirvish/manifests/client.pp
@@ -1,0 +1,49 @@
+# Trivial class to include the dirvish keys on the clients
+#
+# Requires that /root/.ssh/authorized_keys is present
+#
+class dirvish::client (
+  $pre_script    = 'undef',
+  $pre_template  = 'undef',
+  $declare_rsync = true
+) {
+
+  # Read the dirvish key from the puppetmaster
+  $pub_key  = ssh_keygen({name => 'dirvish_key', public => 'true'})
+
+  file_line { 'dirvish_ssh_pubkey':
+    ensure => present,
+    path   => '/root/.ssh/authorized_keys',
+    line   => "ssh-rsa ${pub_key} dirvish_key",
+  }
+
+  $template_content = $pre_template ? {
+    'undef' => template('dirvish/pre_client.sh.erb'),
+    default => template($pre_template),
+  }
+
+  $content = $pre_script ? {
+    'undef' => $template_content,
+    default => $pre_script,
+  }
+
+  # Basic pre-run script
+  file { '/etc/dirvish':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+  file { '/etc/dirvish/pre_client':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    content => $content,
+  }
+
+  # Dirvish depends on rsync
+  if $declare_rsync {
+    ensure_packages(['rsync'])
+  }
+
+}

puppet/modules/dirvish/manifests/config.pp
@@ -1,0 +1,22 @@
+class dirvish::config {
+
+  # The main config file
+  file { '/etc/dirvish/master.conf':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('dirvish/master.conf.erb')
+  }
+
+  if $::dirvish::symlink_latest {
+    file { '/etc/dirvish/post-server':
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0755',
+      content => template('dirvish/post-server.erb')
+    }
+  }
+
+  create_resources(dirvish::vault,$::dirvish::vaults)
+
+}

puppet/modules/dirvish/manifests/init.pp
@@ -1,0 +1,15 @@
+class dirvish(
+  $backup_location   = $dirvish::params::backup_location,
+  $symlink_latest    = $dirvish::params::symlink_latest,
+  $vaults            = $dirvish::params::vaults,
+  $use_systemd       = $dirvish::params::use_systemd,
+  $overwrite_cronjob = $dirvish::params::overwrite_cronjob,
+) inherits dirvish::params {
+
+  anchor { 'dirvish::start':   } ->
+  class  { 'dirvish::install': } ~>
+  class  { 'dirvish::config':  } ~>
+  class  { 'dirvish::service': } ~>
+  anchor { 'dirvish::end':     }
+
+}

puppet/modules/dirvish/manifests/install.pp
@@ -1,0 +1,42 @@
+class dirvish::install {
+
+  package { 'dirvish': ensure => installed }
+
+  file { $::dirvish::backup_location:
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  file { "${::dirvish::backup_location}/ssh":
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0700',
+  }
+
+  # This clever function creates SSH keys on the puppetmaster and allows them to
+  # be read back and passed to the puppet clients
+
+  # Read the dirvish SSH key (and create it if necessary)
+
+  # Generate RSA keys reliably
+  $pub_key  = ssh_keygen({name => 'dirvish_key', public => 'true'})
+  $priv_key = ssh_keygen({name => 'dirvish_key'})
+
+  file { "${::dirvish::backup_location}/ssh/dirvish_key":
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0400',
+    content => "${priv_key}",
+  }
+
+  file { "${::dirvish::backup_location}/ssh/dirvish_key.pub":
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => "ssh-rsa ${pub_key} dirvish_key\n",
+  }
+
+}

puppet/modules/dirvish/manifests/params.pp
@@ -1,0 +1,28 @@
+class dirvish::params {
+
+  # The place to store the backups
+  $backup_location  = "/srv/backups"
+
+  # Whether to create a symlink called "latest" when a
+  # backup completes
+  $symlink_latest   = true
+
+  # Use systemd timers instead of cron for nightly execution?
+  $use_systemd = false
+
+  # The backups to perform. This is an example.
+  $vaults = {
+    test       => {
+      client   => 'myclient',
+      tree     => '/etc',
+      excludes => [
+        '*hosts*',
+        '/etc/puppet'
+      ]
+    }
+  }
+
+  # Some packages don't provide a default cronjob
+  $overwrite_cronjob = true
+
+}

puppet/modules/dirvish/manifests/service.pp
@@ -1,0 +1,53 @@
+class dirvish::service {
+
+  if $::dirvish::use_systemd {
+    file { '/etc/systemd/system/dirvish.timer':
+      content => template('dirvish/systemd.dirvish.timer.erb'),
+      notify  => Exec['dirvish-systemctl-daemon-reload'],
+    }
+
+    file { '/etc/systemd/system/dirvish.service':
+      content => template('dirvish/systemd.dirvish.service.erb'),
+      notify  => Exec['dirvish-systemctl-daemon-reload'],
+    }
+
+    exec { 'dirvish-systemctl-daemon-reload':
+      refreshonly => true,
+      path        => $::path,
+      command     => 'systemctl daemon-reload',
+      subscribe   => [
+        File['/etc/systemd/system/dirvish.service'],
+        File['/etc/systemd/system/dirvish.timer'],
+      ],
+    }
+
+    service { 'dirvish.timer':
+      provider  => 'systemd',
+      ensure    => running,
+      enable    => true,
+      subscribe => [
+        File['/etc/systemd/system/dirvish.timer'],
+        File['/etc/systemd/system/dirvish.service'],
+        Exec['dirvish-systemctl-daemon-reload'],
+      ],
+    }
+  } else {
+    # Dirvish runs from a cronjob
+    cron { 'dirvish':
+      command => '/etc/dirvish/dirvish-cronjob',
+      user    => root,
+      hour    => '2',
+      minute  => '45'
+    }
+
+    if $::dirvish::overwrite_cronjob {
+      file { '/etc/dirvish/dirvish-cronjob':
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+        source => 'puppet:///modules/dirvish/dirvish-cronjob',
+      }
+    }
+  }
+}

puppet/modules/dirvish/manifests/vault.pp
@@ -1,0 +1,44 @@
+define dirvish::vault(
+  $client,
+  $tree,
+  $preclient      = false,
+  $excludes       = [],
+  $expire_default = "+30 days",
+  $expire_rules   = ['mday { 1 } +6 months']
+) {
+
+  file { "${::dirvish::backup_location}/${name}":
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+  ->
+  file { "${::dirvish::backup_location}/${name}/dirvish":
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+  ->
+  file { "${::dirvish::backup_location}/${name}/dirvish/default.conf":
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('dirvish/vault.erb')
+  }
+  ->
+  # Initialize the vault. This could take a while, so we disable the timeout.
+  # Sadly 'creates' and 'refreshonly' work as an OR pair, so instead we
+  # look for the history file - this will only exist if there has been at least
+  # one successful backup
+  exec { "Initialize Dirvish Vault: ${name}":
+    timeout     => 0,
+    command     => "/usr/bin/dirvish --init --vault ${name} --image initial",
+    creates     => "${::dirvish::backup_location}/${name}/dirvish/default.hist",
+    require     => File['/etc/dirvish/master.conf'],
+  }
+
+
+}

puppet/modules/dirvish/templates/master.conf.erb
@@ -1,0 +1,17 @@
+bank:
+  <%= scope.lookupvar('::dirvish::backup_location') %>
+
+exclude:
+  lost+found/
+  *~
+  .nfs*
+
+expire-default: +15 days
+<% if scope.lookupvar('::dirvish::symlink_latest') -%>
+post-server: /etc/dirvish/post-server
+<% end -%>
+
+Runall:
+<% scope.lookupvar('::dirvish::vaults').keys.sort.each do |vault| -%>
+  <%= vault %>
+<% end -%>

puppet/modules/dirvish/templates/post-server.erb
@@ -1,0 +1,15 @@
+#!/bin/bash
+
+echo "Dirvish Post-Server Script: starting"
+
+if [[ $DIRVISH_STATUS == "success" ]]; then
+  vault=`echo $DIRVISH_IMAGE | cut -f1 -d:`
+  image=`echo $DIRVISH_IMAGE | cut -f3 -d:`
+
+  echo "Dirvish Post-Server Script: linking $image->latest in $vault"
+  cd <%= scope.lookupvar('::dirvish::backup_location') %>/$vault/ && ln -snf $image latest
+else
+  echo "Dirvish Post-Server Script: backup not successful, not updating link"
+fi
+
+echo "Dirvish Post-Server Script: complete"

puppet/modules/dirvish/templates/pre_client.sh.erb
@@ -1,0 +1,6 @@
+#!/bin/bash
+
+# Puppet-created script run by the dirvish server before the backup is run
+# This is the default script which does nothing
+
+true

puppet/modules/dirvish/templates/systemd.dirvish.service.erb
@@ -1,0 +1,8 @@
+[Unit]
+Description=Run Dirvish backup jobs
+
+[Service]
+Type=oneshot
+ExecStart=/etc/dirvish/dirvish-cronjob
+User=root
+Group=root

puppet/modules/dirvish/templates/systemd.dirvish.timer.erb
@@ -1,0 +1,9 @@
+[Unit]
+Description=Systemd Timer for Dirvish
+
+[Timer]
+OnCalendar=02:45
+Persistent=true
+
+[Install]
+WantedBy=timers.target

puppet/modules/dirvish/templates/vault.erb
@@ -1,0 +1,24 @@
+client: <%= @client %>
+tree: <%= @tree %>
+
+xdev: true
+index: gzip
+image-default: %Y%m%d
+
+rsh: ssh -oStrictHostKeyChecking=no -i<%= scope.lookupvar('::dirvish::backup_location') %>/ssh/dirvish_key
+
+<% if @preclient -%>
+pre-client: /etc/dirvish/pre_client
+
+<% end -%>
+<% unless @expire_rules.empty? -%>
+expire-rule:
+  <%= @expire_rules.join("\n  ") %>
+<% end -%>
+
+expire-default: <%= @expire_default %>
+
+<% unless @excludes.empty? -%>
+exclude:
+  <%= @excludes.join("\n  ") %>
+<% end -%>