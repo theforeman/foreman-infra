47717a8f7c85569c1a27473c940e723d07bfd53b
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-09-30 13:10:28 +0200

    Fix typo in variable name
    Fixes: fa1f88953681dd9033c90fd2c514c1fc73ebe3d7

--- puppet/modules/deploy/files/script.sh
+++ puppet/modules/deploy/files/script.sh
@@ -6,6 +6,6 @@ environment=production
 trap "rm -rf ${dir}" EXIT
 git clone --recurse-submodules https://github.com/theforeman/foreman-infra ${dir}/
 g10k -quiet -puppetfile -puppetfilelocation ${dir}/puppet/Puppetfile_forge -moduledir ${dir}/puppet/forge_modules
-rsync -aqx --delete-after --exclude=.git ${dir}/puppet/* /etc/puppetlabs/code/environments/$enviroment/
-puppet generate types --environment $enviroment
+rsync -aqx --delete-after --exclude=.git ${dir}/puppet/* /etc/puppetlabs/code/environments/$environment/
+puppet generate types --environment $environment
 echo "Deploy complete at `date`"
