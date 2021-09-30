b08657a3ed0d70f3e8e139bcb914ef8f504af782
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-09-30 12:16:59 +0200

    Run puppet generate deploy in deploy-puppet
    This creates a .resource_types directory which is needed by Puppetserver
    to have proper environment isolation. It also helps with changing type
    signatures like the addition of a parameter.

--- puppet/modules/deploy/files/script.sh
+++ puppet/modules/deploy/files/script.sh
@@ -2,8 +2,10 @@
 set -e # dont rsync if clone fails
 echo "Deploy started at `date`"
 dir=`mktemp -d`
+environment=production
 trap "rm -rf ${dir}" EXIT
 git clone --recurse-submodules https://github.com/theforeman/foreman-infra ${dir}/
 g10k -quiet -puppetfile -puppetfilelocation ${dir}/puppet/Puppetfile_forge -moduledir ${dir}/puppet/forge_modules
-rsync -aqx --delete-after --exclude=.git ${dir}/puppet/* /etc/puppetlabs/code/environments/production/
+rsync -aqx --delete-after --exclude=.git ${dir}/puppet/* /etc/puppetlabs/code/environments/$enviroment/
+puppet generate types --environment $enviroment
 echo "Deploy complete at `date`"
