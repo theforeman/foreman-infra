theforeman/foreman-infra

commit 70c725348f7ca01c7704db7d0adedf45bd7c1381

Install Puppet git modules via g10k
This allows g10k to cache and unifies the deployment.

puppet/Puppetfile
@@ -1,14 +1,16 @@
-moduledir 'forge_modules'
+moduledir 'external_modules'
 
 mod 'camptocamp/pbuilder', '0.1.11'
 mod 'camptocamp/systemd', '2.9.0'
 mod 'citrin/htpasswd', '1.0.1'
 mod 'claranet/timezone', '2.0.1'
 mod 'golja/gnupg', '1.2.3'
+mod 'gwmngilfen/dirvish' :git => 'https://github.com/GregSutcliffe/puppet-dirvish', :ref => 'cfad9c4bed8911ab7674ce86866890e60a62ae31'
 mod 'herculesteam/augeasproviders_core', '2.6.0'
 mod 'herculesteam/augeasproviders_mounttab', '2.0.3'
 mod 'herculesteam/augeasproviders_shellvar', '4.0.0'
 mod 'herculesteam/augeasproviders_ssh', '3.3.0'
+mod 'nightfly19/external_ip', :git => 'https://github.com/nightfly19/puppet-external_ip', :ref => '883ca56888be75418f854f77e24665c1363c8357'
 mod 'puppet/archive', '4.4.0'
 mod 'puppet/chrony', '2.0.0'
 mod 'puppet/epel', '3.0.1'
@@ -16,6 +18,7 @@ mod 'puppet/extlib', '5.0.0'
 mod 'puppet/jenkins', '2.0.0'
 mod 'puppet/letsencrypt', '5.0.0'
 mod 'puppet/nodejs', '8.0.0'
+mod 'puppet/rvm', :git => 'https://github.com/voxpupuli/puppet-rvm.git', :ref => '2dae1b78d7dd8272a6a4f9106234d8969d7d5a72'
 mod 'puppet/selinux', '3.1.0'
 mod 'puppet/unattended_upgrades', '6.0.0'
 mod 'puppetlabs/apache', '5.4.0'

.github/workflows/main.yml
@@ -26,7 +26,7 @@ jobs:
           wget https://github.com/xorpaul/g10k/releases/download/v0.8.9/g10k-linux-amd64.zip
           unzip g10k-linux-amd64.zip
       - name: Install modules using g10k
-        run: ./g10k -cachedir .g10k/cache -puppetfile -puppetfilelocation Puppetfile_forge -moduledir forge_modules
+        run: ./g10k -cachedir .g10k/cache -puppetfile
       - name: Run syntax
         run: bundle exec rake syntax
       - name: Run lint

.gitignore
@@ -1,7 +1,7 @@
 ssl/*
 yaml/*
 puppet/puppet.conf
-puppet/forge_modules/
+puppet/external_modules/
 g10k
 g10k-linux-amd64.zip
 .g10k/cache

.gitmodules
@@ -1,12 +1,3 @@
-[submodule "puppet/git_modules/rvm"]
-	path = puppet/git_modules/rvm
-	url = https://github.com/maestrodev/puppet-rvm.git
-[submodule "puppet/git_modules/dirvish"]
-	path = puppet/git_modules/dirvish
-	url = https://github.com/GregSutcliffe/puppet-dirvish
-[submodule "puppet/git_modules/external_ip"]
-	path = puppet/git_modules/external_ip
-	url = https://github.com/nightfly19/puppet-external_ip
 [submodule "ansible/roles/Jimdo.fastly"]
 	path = ansible/roles/Jimdo.fastly
 	url = https://github.com/evgeni/ansible-fastly

Vagrantfile
@@ -13,7 +13,7 @@ Vagrant.configure("2") do |config|
   SHELL
 
   config.vm.provision "run puppet", type: 'puppet' do |puppet|
-    puppet.module_path = ["puppet/modules", "puppet/git_modules", "puppet/forge_modules"]
+    puppet.module_path = ["puppet/modules", "puppet/external_modules"]
     puppet.manifests_path = "vagrant/manifests"
     puppet.synced_folder_type = "rsync"
   end

puppet/environment.conf
@@ -1,1 +1,1 @@
-modulepath = forge_modules:git_modules:modules:$basemodulepath
+modulepath = external_modules:modules:$basemodulepath

puppet/modules/deploy/files/script.sh
@@ -4,8 +4,8 @@ echo "Deploy started at `date`"
 dir=`mktemp -d`
 environment=production
 trap "rm -rf ${dir}" EXIT
-git clone --recurse-submodules https://github.com/theforeman/foreman-infra ${dir}/
-g10k -quiet -puppetfile -puppetfilelocation ${dir}/puppet/Puppetfile_forge -moduledir ${dir}/puppet/forge_modules
+git clone https://github.com/theforeman/foreman-infra ${dir}/
+g10k -quiet -puppetfile -puppetfilelocation ${dir}/puppet/Puppetfile -moduledir ${dir}/puppet/external_modules
 rsync -aqx --delete-after --exclude=.git ${dir}/puppet/* /etc/puppetlabs/code/environments/$environment/
 puppet generate types --environment $environment
 echo "Deploy complete at `date`"

puppet/spec/spec_helper.rb
@@ -23,7 +23,7 @@ end
 base_dir = File.dirname(File.expand_path(__dir__))
 
 RSpec.configure do |c|
-  c.module_path     = [File.join(base_dir, 'modules'), File.join(base_dir, 'git_modules'), File.join(base_dir, 'forge_modules'), File.join(base_dir, 'test_modules')].join(File::PATH_SEPARATOR)
+  c.module_path     = [File.join(base_dir, 'modules'), File.join(base_dir, 'external_modules'), File.join(base_dir, 'test_modules')].join(File::PATH_SEPARATOR)
   c.manifest_dir    = File.join(base_dir, 'manifests')
   c.manifest        = File.join(base_dir, 'manifests', 'site.pp')
   c.environmentpath = base_dir

commit eee23869e132b5e4ce6246ed49bb4f2d52089d71

Replace external_ip module with local facts
This module only has custom facts but no metadata.json which means g10k
ignores the module during deployment. This includes the same facts (with
a different implementation) in the profiles module.

Fixes: 70c725348f7ca01c7704db7d0adedf45bd7c1381

puppet/Puppetfile
@@ -10,7 +10,6 @@ mod 'herculesteam/augeasproviders_core', '2.6.0'
 mod 'herculesteam/augeasproviders_mounttab', '2.0.3'
 mod 'herculesteam/augeasproviders_shellvar', '4.0.0'
 mod 'herculesteam/augeasproviders_ssh', '3.3.0'
-mod 'nightfly19/external_ip', :git => 'https://github.com/nightfly19/puppet-external_ip', :ref => '883ca56888be75418f854f77e24665c1363c8357'
 mod 'puppet/archive', '4.4.0'
 mod 'puppet/chrony', '2.0.0'
 mod 'puppet/epel', '3.0.1'

puppet/modules/profiles/lib/facter/external_ips.rb
@@ -1,0 +1,21 @@
+require 'open-uri'
+
+Facter.add(:external_ip4) do
+  setcode do
+    begin
+      URI.parse('http://ipv4.icanhazip.com/').read.chomp
+    rescue
+      nil
+    end
+  end
+end
+
+Facter.add(:external_ip6) do
+  setcode do
+    begin
+      URI.parse('http://ipv6.icanhazip.com/').read.chomp
+    rescue
+      nil
+    end
+  end
+end