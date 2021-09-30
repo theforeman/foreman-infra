theforeman/foreman-infra

commit fa1f88953681dd9033c90fd2c514c1fc73ebe3d7

Run puppet generate deploy in deploy-puppet
This creates a .resource_types directory which is needed by Puppetserver
to have proper environment isolation. It also helps with changing type
signatures like the addition of a parameter.

puppet/modules/deploy/files/script.sh
@@ -2,8 +2,10 @@
 set -e # dont rsync if clone fails
 echo "Deploy started at `date`"
 dir=`mktemp -d`
+environment=production
 trap "rm -rf ${dir}" EXIT
 git clone --recurse-submodules https://github.com/theforeman/foreman-infra ${dir}/
 g10k -quiet -puppetfile -puppetfilelocation ${dir}/puppet/Puppetfile_forge -moduledir ${dir}/puppet/forge_modules
-rsync -aqx --delete-after --exclude=.git ${dir}/puppet/* /etc/puppetlabs/code/environments/production/
+rsync -aqx --delete-after --exclude=.git ${dir}/puppet/* /etc/puppetlabs/code/environments/$enviroment/
+puppet generate types --environment $enviroment
 echo "Deploy complete at `date`"

commit 73eb982bf7fdfc57b3c5b393504a05a3fd7322c2

Fix typo in variable name
Fixes: fa1f88953681dd9033c90fd2c514c1fc73ebe3d7

puppet/modules/deploy/files/script.sh
@@ -6,6 +6,6 @@ environment=production
 trap "rm -rf ${dir}" EXIT
 git clone --recurse-submodules https://github.com/theforeman/foreman-infra ${dir}/
 g10k -quiet -puppetfile -puppetfilelocation ${dir}/puppet/Puppetfile_forge -moduledir ${dir}/puppet/forge_modules
-rsync -aqx --delete-after --exclude=.git ${dir}/puppet/* /etc/puppetlabs/code/environments/$enviroment/
-puppet generate types --environment $enviroment
+rsync -aqx --delete-after --exclude=.git ${dir}/puppet/* /etc/puppetlabs/code/environments/$environment/
+puppet generate types --environment $environment
 echo "Deploy complete at `date`"