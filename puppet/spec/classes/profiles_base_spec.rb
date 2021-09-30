c0d4bab424cbda5586baf46b5dc207063044ffa8
Evgeni Golov <evgeni@golov.de>
2021-09-30 08:18:54 +0200

    add profiles::base tests

---
+++ puppet/spec/classes/profiles_base_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'profiles::base' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile.with_all_deps }
+    end
+  end
+end
