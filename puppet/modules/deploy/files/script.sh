ddb2b0cdc178f1d89d19a229bb552ba04ccec5b0
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-09-30 14:38:07 +0200

    [deploy] Clone quietly and only a single depth
    This speeds up cloning. It's also generally not very interesting output.

--- puppet/modules/deploy/files/script.sh
+++ puppet/modules/deploy/files/script.sh
@@ -4,7 +4,7 @@ echo "Deploy started at `date`"
 dir=`mktemp -d`
 environment=production
 trap "rm -rf ${dir}" EXIT
-git clone https://github.com/theforeman/foreman-infra ${dir}/
+git clone --quiet --depth 1 https://github.com/theforeman/foreman-infra ${dir}/
 g10k -quiet -puppetfile -puppetfilelocation ${dir}/puppet/Puppetfile -moduledir ${dir}/puppet/external_modules
 rsync -aqx --delete-after --exclude=.git ${dir}/puppet/* /etc/puppetlabs/code/environments/$environment/
 puppet generate types --environment $environment --config /etc/puppetlabs/puppet/puppet.conf
