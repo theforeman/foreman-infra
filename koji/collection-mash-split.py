theforeman/foreman-infra

commit 3f4ee09d908dc7f947f451cad8c3ba68d18157d6

simplify get_from_git

koji/collection-mash-split.py
@@ -11,6 +11,7 @@ import difflib
 import time
 import re
 import sys
+import StringIO
 from distutils.version import LooseVersion
 
 import koji
@@ -78,9 +79,7 @@ class MashSplit(object):
         @type baseloc: str
         @param filename: file name
         @type filename: str
-        @param finalloc: local destination directory
-        @type finalloc: str
-        @return: output file name
+        @return: file contents
         @rtype: str
         """
         if not os.path.exists(finalloc):
@@ -91,16 +90,14 @@ class MashSplit(object):
             clone_cmd = "git clone %s %s" % (gitloc, workdir)
             kobo.shortcuts.run(clone_cmd, workdir="/mnt/tmp/gitrepo/", can_fail=True)
         final_fn = os.path.join(os.path.realpath(finalloc), filename)
-        cmd = "git fetch && git archive remotes/origin/%s %s | tar -C %s -x -f -" % (baseloc, filename, finalloc)
+        cmd = "git fetch && git show remotes/origin/%s/%s" % (baseloc, filename)
         self.logger.debug("running %s" % cmd)
         status, output = kobo.shortcuts.run(cmd, workdir=workdir, can_fail=True)
         if status != 0:
             err_msg = "Could not download %s/%s/%s." % (gitloc, baseloc, filename)
             self.logger.error(err_msg)
             raise SystemExit(err_msg)
-        if not os.path.exists(final_fn):
-            self.logger.error("File %s does not exist." % final_fn)
-        return final_fn
+        return output
 
     def run_mash(self, path, config):
         """Run mash with given config.
@@ -185,13 +182,13 @@ class MashSplit(object):
         cmd += " --queryformat='%{name}-%{epochnum}:%{version}-%{release}.%{arch}\n'"
         self.logger.debug("running %s" % cmd)
         status, output = kobo.shortcuts.run(cmd)
-        with open(modulemd_yaml) as modulemd_file:
-            modules = yaml.safe_load(modulemd_file)
+        modules = yaml.safe_load(modulemd_yaml)
         modules['data']['artifacts'] = {'rpms': output.splitlines()}
         modules['data']['version'] = modulemd_version
-        with open(modulemd_yaml, 'w') as modulemd_file:
-            yaml.dump(modules, modulemd_file, default_flow_style=False)
-        cmd = "modifyrepo_c --mdtype=modules {} {}/repodata".format(modulemd_yaml, path)
+        modules_yaml = os.path.join(path, 'repodata', 'modules.yaml')
+        with open(modules_yaml, 'w') as modules_file:
+            yaml.dump(modules, modules_file, default_flow_style=False)
+        cmd = "modifyrepo_c --mdtype=modules {} {}/repodata".format(modules_yaml, path)
         self.logger.debug("running %s" % cmd)
         kobo.shortcuts.run(cmd)
 
@@ -213,13 +210,13 @@ class MashSplit(object):
 
     def list_comps(self, comps):
         """Get list of all package names from comps file
-        @param comps: comps file path
+        @param comps: comps file content
         @type comps: str
         @return: package name
         @rtype: set
         """
         yum_comps = yum.comps.Comps()
-        comps_file = open(comps, "r")
+        comps_file = StringIO.StringIO(comps)
         yum_comps.add(comps_file)
         comps_file.close()
         pkgs = set()
@@ -312,9 +309,8 @@ class MashSplit(object):
         for arch in arches:
             extras_cache_path = os.path.join(extras_path, mash_config, arch)
             extras_repo_path = os.path.join(whole_path, mash_config, arch, "os", "Packages")
-            extras_file = self.get_from_git(gitloc, extras_baseloc, extras + '-' + arch, extras_git_path)
-            with open(extras_file) as f:
-                self.download_extras(extras_cache_path, extras_repo_path, [line.rstrip() for line in f.readlines()])
+            extras_file = self.get_from_git(gitloc, extras_baseloc, extras + '-' + arch)
+            self.download_extras(extras_cache_path, extras_repo_path, extras_file.splitlines())
 
     def handle_comps(self, whole_path, tmp_path, split_path, mash_config, arches, compses, git_tag,
                      checksum=None, modulemd_yaml=None, modulemd_version=0):
@@ -346,7 +342,7 @@ class MashSplit(object):
 
         all_from_comps = set()
         for comps, output_path in compses.items():
-            comps = self.get_from_git(gitloc, comps_baseloc, comps, comps_path)
+            comps = self.get_from_git(gitloc, comps_baseloc, comps)
             comps_pkg_names = self.list_comps(comps)
             all_from_comps.update(comps_pkg_names)
 
@@ -362,7 +358,7 @@ class MashSplit(object):
                 copied.update(self.copyout(comps_pkg_names, source, tmp_target))
                 self.createrepo(tmp_target, comps, checksum=checksum)
                 if modulemd_yaml:
-                    modulemd = self.get_from_git(gitloc, modulemd_baseloc, modulemd_yaml, tmp_target)
+                    modulemd = self.get_from_git(gitloc, modulemd_baseloc, modulemd_yaml)
                     self.inject_modulemd_yml(tmp_target, modulemd, modulemd_version)
                 rpm_target = os.path.join(split_path, "yum", output_path, arch)
                 if not os.path.exists(rpm_target):

commit 8446194d6530ce13f55f9c6fe4a5f62a0dcac9d3

make get_from_git also return a path
Fixes: 3f4ee09

koji/collection-mash-split.py
@@ -69,7 +69,7 @@ class MashSplit(object):
         kobo.log.add_file_logger(self.mash_logger, mash_log, log_level=logging.DEBUG, format="%(message)s")
         self.koji_session = koji.ClientSession(CONFIG["koji_url"])
 
-    def get_from_git(self, gitloc, baseloc, filename, finalloc):
+    def get_from_git(self, gitloc, baseloc, filename, finalloc=None):
         """Download a file from remote git repo.
 
         @param gitloc: git repository url (for ex.: git://git.fedorahosted.org)
@@ -78,17 +78,16 @@ class MashSplit(object):
         @type baseloc: str
         @param filename: file name
         @type filename: str
+        @param finalloc: optional local destination directory
+        @type finalloc: str
         @return: file contents
         @rtype: str
         """
-        if not os.path.exists(finalloc):
-            os.makedirs(finalloc)
         repo_name = os.path.basename(gitloc)
         workdir = "/mnt/tmp/gitrepo/%s" % repo_name
         if not os.path.exists(workdir):
             clone_cmd = "git clone %s %s" % (gitloc, workdir)
             kobo.shortcuts.run(clone_cmd, workdir="/mnt/tmp/gitrepo/", can_fail=True)
-        final_fn = os.path.join(os.path.realpath(finalloc), filename)
         cmd = "git fetch && git show remotes/origin/%s/%s" % (baseloc, filename)
         self.logger.debug("running %s" % cmd)
         status, output = kobo.shortcuts.run(cmd, workdir=workdir, can_fail=True)
@@ -96,7 +95,15 @@ class MashSplit(object):
             err_msg = "Could not download %s/%s/%s." % (gitloc, baseloc, filename)
             self.logger.error(err_msg)
             raise SystemExit(err_msg)
-        return output
+        if finalloc is not None:
+            if not os.path.exists(finalloc):
+                os.makedirs(finalloc)
+            final_fn = os.path.join(os.path.realpath(finalloc), filename)
+            with open(final_fn, 'w') as resultfile:
+                resultfile.write(output)
+        else:
+            final_fn = None
+        return output, final_fn
 
     def run_mash(self, path, config):
         """Run mash with given config.
@@ -286,8 +293,8 @@ class MashSplit(object):
 
         all_from_comps = set()
         for comps, output_path in compses.items():
-            comps = self.get_from_git(gitloc, comps_baseloc, comps)
-            comps_pkg_names = self.list_comps(comps)
+            comps_content, comps_file = self.get_from_git(gitloc, comps_baseloc, comps, comps_path)
+            comps_pkg_names = self.list_comps(comps_content)
             all_from_comps.update(comps_pkg_names)
 
             rpm_target = None
@@ -300,9 +307,9 @@ class MashSplit(object):
                     os.makedirs(tmp_target)
                 source = os.path.join(whole_path, mash_config, arch, "os", "Packages")
                 copied.update(self.copyout(comps_pkg_names, source, tmp_target))
-                self.createrepo(tmp_target, comps, checksum=checksum)
+                self.createrepo(tmp_target, comps_file, checksum=checksum)
                 if modulemd_yaml:
-                    modulemd = self.get_from_git(gitloc, modulemd_baseloc, modulemd_yaml)
+                    modulemd, _ = self.get_from_git(gitloc, modulemd_baseloc, modulemd_yaml)
                     self.inject_modulemd_yml(tmp_target, modulemd, modulemd_version)
                 rpm_target = os.path.join(split_path, "yum", output_path, arch)
                 if not os.path.exists(rpm_target):