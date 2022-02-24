theforeman/foreman-infra

commit 5d8a9fe524abf3e5ebb96c951ba7f7fe8aa93705

better puppet install on debian

Vagrantfile
@@ -7,16 +7,9 @@ Vagrant.configure("2") do |config|
 
   config.vm.provision "install puppet", type: "shell", inline: <<-SHELL
     . /etc/os-release
-    if [ "${ID}" = "debian" ]; then
-       wget https://apt.puppet.com/puppet6-release-${VERSION_CODENAME}.deb
-       apt-get install -y ./puppet6-release-${VERSION_CODENAME}.deb
-       apt-get update
-       apt-get install -y puppet-agent
-    else
-       yum -y install epel-release
-       yum -y install puppet6-release || yum -y install https://yum.puppetlabs.com/puppet6/puppet6-release-el-${VERSION_ID}.noarch.rpm
-       yum -y install puppet-agent
-    fi
+    yum -y install epel-release
+    yum -y install puppet6-release || yum -y install https://yum.puppetlabs.com/puppet6/puppet6-release-el-${VERSION_ID}.noarch.rpm
+    yum -y install puppet-agent
   SHELL
 
   config.vm.provision "run puppet", type: 'puppet' do |puppet|
@@ -64,6 +57,13 @@ Vagrant.configure("2") do |config|
     override.vm.provider "libvirt" do |libvirt|
       libvirt.memory = "4096"
     end
+    config.vm.provision "install puppet", type: "shell", inline: <<-SHELL
+      . /etc/os-release
+      wget https://apt.puppet.com/puppet6-release-${VERSION_CODENAME}.deb
+      apt-get install -y ./puppet6-release-${VERSION_CODENAME}.deb
+      apt-get update
+      apt-get install -y puppet-agent
+    SHELL
   end
 
   config.vm.define "web" do |override|

commit a814c04ee8179303d9d1c98ab5f7bd50fe559c20

Correct Vagrant provisioning override
Without this it tries to run the apt code on all boxes instead of only
those particular builders.

Fixes: 5d8a9fe524abf3e5ebb96c951ba7f7fe8aa93705
Fixes: 387719b19ea050b3b7925741ca06dd46b780bb61

Vagrantfile
@@ -57,7 +57,7 @@ Vagrant.configure("2") do |config|
     override.vm.provider "libvirt" do |libvirt|
       libvirt.memory = "4096"
     end
-    config.vm.provision "install puppet", type: "shell", inline: <<-SHELL
+    override.vm.provision "install puppet", type: "shell", inline: <<-SHELL
       . /etc/os-release
       wget https://apt.puppet.com/puppet6-release-${VERSION_CODENAME}.deb
       apt-get install -y ./puppet6-release-${VERSION_CODENAME}.deb
@@ -73,7 +73,7 @@ Vagrant.configure("2") do |config|
     override.vm.provider "libvirt" do |libvirt|
       libvirt.memory = "4096"
     end
-    config.vm.provision "install puppet", type: "shell", inline: <<-SHELL
+    override.vm.provision "install puppet", type: "shell", inline: <<-SHELL
       . /etc/os-release
       wget https://apt.puppet.com/puppet6-release-${VERSION_CODENAME}.deb
       apt-get install -y ./puppet6-release-${VERSION_CODENAME}.deb