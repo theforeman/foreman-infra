010096f959ad9d163c786aea41976f3012a9cbfd
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-07-06 12:26:58 +0200

    Make rvm configs ensurable to allow removal

--- puppet/modules/slave/manifests/rvm_config.pp
+++ puppet/modules/slave/manifests/rvm_config.pp
@@ -1,21 +1,29 @@
 # @api private
 define slave::rvm_config (
   String[1] $version,
+  Enum['present', 'absent'] $ensure = 'present',
   String[1] $rubygems_version = '3.0.6',
 ) {
   $alias = $title
 
   rvm_system_ruby { $version:
-    ensure => present,
-  } ->
+    ensure => $ensure,
+  }
+
   rvm_alias { $alias:
-    ensure      => present,
+    ensure      => $ensure,
     target_ruby => $version,
-  } ->
-  exec { "${version}/update_rubygems":
-    command  => "rvm ${version} rubygems ${rubygems_version} --force",
-    unless   => "test `rvm ${version} do gem -v` = ${rubygems_version}",
-    path     => '/usr/local/rvm/bin:/usr/bin:/bin',
-    provider => 'shell',
+  }
+
+  if $ensure == 'present' {
+    Rvm_system_ruby[$version] -> Rvm_alias[$alias]
+    -> exec { "${version}/update_rubygems":
+      command  => "rvm ${version} rubygems ${rubygems_version} --force",
+      unless   => "test `rvm ${version} do gem -v` = ${rubygems_version}",
+      path     => '/usr/local/rvm/bin:/usr/bin:/bin',
+      provider => 'shell',
+    }
+  } else {
+    Rvm_alias[$alias] -> Rvm_system_ruby[$version]
   }
 }
