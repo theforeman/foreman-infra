b8278e2af34ab5600a944366f7b26f65c65f6014
Patrick Creech <pcreech@redhat.com>
2022-08-09 14:34:22 -0400

    Update collection-mash-split for foreman 3.4/katello 4.6

--- koji/collection-mash-split.py
+++ koji/collection-mash-split.py
@@ -430,6 +430,7 @@ def main():
     elif collection == 'katello':
         branch_map = {
             'nightly': 'develop',
+            '4.6': '3.4',
             '4.5': '3.3',
             '4.4': '3.2',
             '4.3': '3.1',
