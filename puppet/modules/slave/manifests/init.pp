theforeman/foreman-infra

commit 7e3425cfb7d6193d32ab0f32ed311c150155ed7a

Remove Java 1.8 on Jenkins nodes
Jenkins should use Java 11, so 1.8 is no longer needed. Having it
installed may mean it's used accidentally.

Fixes: 2cdc68e4c86cac88cb350d776ff52df4547e4c7c

puppet/modules/slave/manifests/init.pp
@@ -6,7 +6,17 @@ class slave (
   Boolean $unittests = $facts['os']['family'] == 'RedHat',
   Boolean $packaging = true,
 ) {
-  $java_package = if $facts['os']['family'] == 'RedHat' { 'java-11-openjdk-headless' } else { undef }
+
+  if $facts['os']['family'] == 'RedHat' {
+    $java_package = 'java-11-openjdk-headless'
+
+    package { 'java-1.8.0-openjdk-headless':
+      ensure => absent,
+    }
+  } else {
+    $java_package = undef
+  }
+
   class { 'java':
     package => $java_package,
   }

commit c5fd1cc54854dbaa4119e8b752588a6f60c584bf

Remove all Java 1.8 packages from Jenkins nodes
Puppet uses rpm -e to remove packages rather than yum, so removing the
leaf package doesn't work since it's still needed by others. This lists
all java packages that need to be removed.

Fixes: 7e3425cfb7d6193d32ab0f32ed311c150155ed7a

puppet/modules/slave/manifests/init.pp
@@ -10,9 +10,10 @@ class slave (
   if $facts['os']['family'] == 'RedHat' {
     $java_package = 'java-11-openjdk-headless'
 
-    package { 'java-1.8.0-openjdk-headless':
+    package { ['java-1.8.0-openjdk', 'java-1.8.0-openjdk-headless', 'java-1.8.0-openjdk-devel']:
       ensure => absent,
     }
+    Package['java-1.8.0-openjdk-devel'] -> Package['java-1.8.0-openjdk'] -> Package['java-1.8.0-openjdk-headless']
   } else {
     $java_package = undef
   }