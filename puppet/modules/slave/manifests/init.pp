theforeman/foreman-infra

commit 2cdc68e4c86cac88cb350d776ff52df4547e4c7c

Update Java to 11 on Jenkins nodes
Jenkins 2.361.1 has started to require Java 11. On Debian this is
already the default, but the puppetlabs-java module defaults to
OpenJDK 1.8.0 on EL.

puppet/modules/slave/manifests/init.pp
@@ -6,7 +6,10 @@ class slave (
   Boolean $unittests = $facts['os']['family'] == 'RedHat',
   Boolean $packaging = true,
 ) {
-  include java
+  $java_package = if $facts['os']['family'] == 'RedHat' { 'java-11-openjdk-headless' } else { undef }
+  class { 'java':
+    package => $java_package,
+  }
 
   include git
 

commit 7e3425cfb7d6193d32ab0f32ed311c150155ed7a

Remove Java 1.8 on Jenkins nodes
Jenkins should use Java 11, so 1.8 is no longer needed. Having it
installed may mean it's used accidentally.

Fixes: 2cdc68e4c86cac88cb350d776ff52df4547e4c7c

puppet/modules/slave/manifests/init.pp
@@ -6,7 +6,17 @@ class slave (
   Boolean $unittests = $facts['os']['family'] == 'RedHat',
   Boolean $packaging = true,
 ) {
-  $java_package = if $facts['os']['family'] == 'RedHat' { 'java-11-openjdk-headless' } else { undef }
+
+  if $facts['os']['family'] == 'RedHat' {
+    $java_package = 'java-11-openjdk-headless'
+
+    package { 'java-1.8.0-openjdk-headless':
+      ensure => absent,
+    }
+  } else {
+    $java_package = undef
+  }
+
   class { 'java':
     package => $java_package,
   }