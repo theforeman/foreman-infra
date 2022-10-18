8232b3c29e0231b36803a3128f1da81d38b2ff8d
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-10-18 14:30:23 +0200

    Generate SSH keys in Puppet's vardir
    Rather than hardcoding it to /etc/puppet (which doesn't exist on a
    Puppet AIO install) this uses the vardir setting. This works well for
    extlib::cache_data. As an additional benefit it can now run this in
    tests on a non-privileged user.

--- puppet/spec/classes/profiles_web_spec.rb
+++ puppet/spec/classes/profiles_web_spec.rb
@@ -4,12 +4,11 @@ describe 'profiles::web' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
-      let(:params) { {setup_receiver: false} }
 
       it { is_expected.to compile.with_all_deps }
 
       context 'without https' do
-        let(:params) { super().merge(https: false) }
+        let(:params) { { https: false } }
 
         it { is_expected.to compile.with_all_deps }
       end
