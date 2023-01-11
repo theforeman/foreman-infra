2295df799f148c7231859a1a4fd18ad69f25ad8f
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-01-11 15:25:14 +0100

    Declare Foreman 3.5 as the latest version

--- puppet/modules/profiles/manifests/web.pp
+++ puppet/modules/profiles/manifests/web.pp
@@ -18,7 +18,7 @@
 #   Maximum connection per rsync target. Using a small value to try and reduce
 #   server load
 class profiles::web (
-  String[1] $stable = '3.4',
+  String[1] $stable = '3.5',
   String[1] $next = '3.6',
   Hash[String, Hash] $debugs_htpasswds = {},
   Boolean $https = true,
