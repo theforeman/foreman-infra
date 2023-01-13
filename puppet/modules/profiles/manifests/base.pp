theforeman/foreman-infra

commit e519403be6959247ab8632599dfc1edaa51c2e90

introduce a base profile
and install ntp *or* chrony, depending on the OS

puppet/modules/profiles/manifests/base.pp
@@ -1,0 +1,14 @@
+# @summary A base profile for all machines
+class profiles::base (
+) {
+  include puppet
+  include ssh
+  include timezone
+  include users
+  include utility
+  if $facts['os']['family'] == 'RedHat' and versioncmp($facts['os']['release']['major'], '8') >= 0 {
+    include chrony
+  } else {
+    include ntp
+  }
+}

puppet/Puppetfile_forge
@@ -9,6 +9,7 @@ mod 'herculesteam/augeasproviders_mounttab', '2.0.3'
 mod 'herculesteam/augeasproviders_shellvar', '4.0.0'
 mod 'herculesteam/augeasproviders_ssh', '3.3.0'
 mod 'puppet/archive', '4.4.0'
+mod 'puppet/chrony', '2.0.0'
 mod 'puppet/epel', '3.0.1'
 mod 'puppet/extlib', '5.0.0'
 mod 'puppet/jenkins', '2.0.0'

commit 95f242b9230cc269b13e1fa079187794174cdc13

Use chrony on EL7 too
Previously we ran chrony on EL7, but the bulk of the hosts didn't use
this base profile yet so it wasn't noticed. After the migration this
started to show up.

Because it ran on some hosts, this also includes code to remove ntp from
the system.

Fixes: e519403be6959247ab8632599dfc1edaa51c2e90

puppet/modules/profiles/manifests/base.pp
@@ -6,7 +6,11 @@ class profiles::base (
   include ssh
   include timezone
   include unattended
-  if $facts['os']['family'] == 'RedHat' and versioncmp($facts['os']['release']['major'], '8') >= 0 {
+  if $facts['os']['family'] == 'RedHat' {
+    package { 'ntp':
+      ensure => absent,
+      before => Class['chrony'],
+    }
     include chrony
   } else {
     include ntp