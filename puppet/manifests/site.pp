theforeman/foreman-infra

commit 6cbe2656dd605d22475cc4dcdf5fc3f84d0d3072

Add a Hiera structure
This allows deployment without the Foreman ENC. It does not manage users
nor include utility. Those accept parameters with data that we do not
want in Hiera due to privacy (user email addresses) and password hashes.
Those should still be set via Foreman's ENC.

puppet/manifests/site.pp
@@ -1,0 +1,40 @@
+node default {
+  include profiles::base
+}
+
+node /^controller\d+\.[a-z]+\.theforeman\.org$/ {
+  include profiles::base
+  include profiles::jenkins::controller
+}
+
+node /^discourse\d+\.[a-z]+\.theforeman\.org$/ {
+  include profiles::base
+  # TODO profiles::discourse
+}
+
+node /^foreman\d+\.[a-z]+\.theforeman\.org$/ {
+  include profiles::base
+  include profiles::foreman
+}
+
+node /^node\d+\.jenkins\.[a-z]+\.theforeman\.org$/ {
+  include profiles::base
+  include profiles::jenkins::node
+}
+
+node /^puppet\d+\.[a-z]+\.theforeman\.org$/ {
+  include profiles::base
+  include profiles::puppetserver
+}
+
+node /^redmine\d+\.[a-z]+\.theforeman\.org$/ {
+  include profiles::base
+  include dirvish::client
+  include exim # TODO
+  include redmine
+}
+
+node /^web\d+\.[a-z]+\.theforeman\.org$/ {
+  include profiles::base
+  include profiles::web
+}

puppet/data/common.yaml
@@ -1,0 +1,26 @@
+foreman_servicename: 'foreman.theforeman.org'
+foreman_url: 'https://%{lookup("foreman_servicename")}'
+puppet_servicename: 'puppet.theforeman.org'
+
+foreman::repo::repo: '3.1'
+# TODO: theforeman/foreman 21.1.0 includes /server-status
+foreman::config::apache::proxy_no_proxy_uris:
+  - /icons
+  - /server-status
+foreman::serveraliases:
+  - '%{alias("foreman_servicename")}'
+
+foreman_proxy::trusted_hosts:
+  - "foreman01.conova.theforeman.org"
+foreman_proxy::foreman_base_url: '%{alias("foreman_url")}'
+
+puppet::runmode: cron
+puppet::puppetmaster: '%{alias("puppet_servicename")}'
+puppet::server_additional_settings:
+  dns_alt_names:
+    - '%{alias("puppet_servicename")}'
+puppet::server_foreman_url: '%{alias("foreman_url")}'
+puppet::server_environments_owner: 'deploypuppet'
+puppet::server_environments_group: 'deploypuppet'
+puppet::server_puppetserver_telemetry: false
+puppet::show_diff: true

puppet/data/osfamily/RedHat-8.yaml
@@ -1,0 +1,9 @@
+apache::default_mods:
+  # https://github.com/puppetlabs/puppetlabs-apache/pull/2337
+  - http2
+  - status
+apache::protocols:
+  - 'h2'
+  - 'h2c'
+  - 'http/1.1'
+apache::mpm_module: event

puppet/hiera.yaml
@@ -1,0 +1,29 @@
+---
+version: 5
+defaults:  # Used for any hierarchy level that omits these keys.
+  # The default value for "datadir" is "data" under the same directory as the hiera.yaml
+  # file (this file)
+  # When specifying a datadir, make sure the directory exists.
+  # See https://puppet.com/docs/puppet/latest/environments_about.html for further details on environments.
+  datadir: data
+  data_hash: yaml_data
+
+hierarchy:
+  - name: "Per-node data"
+    path: "nodes/%{trusted.certname}.yaml"
+
+  - name: "Per-OS major defaults"
+    path: "os/%{facts.os.name}-%{facts.os.release.major}.yaml"
+
+  - name: "Per-OS defaults"
+    path: "os/%{facts.os.name}.yaml"
+
+  - name: "Per-OS family major defaults"
+    path: "osfamily/%{facts.os.family}-%{facts.os.release.major}.yaml"
+
+  - name: "Per-OS family defaults"
+    path: "osfamily/%{facts.os.family}.yaml"
+
+  - name: "Other YAML hierarchy levels"
+    paths:
+      - "common.yaml"

puppet/modules/deploy/manifests/init.pp
@@ -1,6 +1,14 @@
-class deploy {
+# @summary Set up a deployment of Puppet environments
+#
+# @param user
+#   The username for the user
+class deploy (
+  String[1] $user = 'deploypuppet',
+) {
+  # TODO: install g10k in $PATH
+
   secure_ssh::receiver_setup { 'deploy':
-    user           => 'deploypuppet',
+    user           => $user,
     foreman_search => 'host ~ node*.jenkins.osuosl.theforeman.org and (name = external_ip4 or name = external_ip6)',
     script_content => file('deploy/script.sh'),
   }

puppet/modules/profiles/manifests/base.pp
@@ -1,11 +1,11 @@
 # @summary A base profile for all machines
 class profiles::base (
 ) {
+  include motd
   include puppet
   include ssh
   include timezone
-  include users
-  include utility
+  include unattended
   if $facts['os']['family'] == 'RedHat' and versioncmp($facts['os']['release']['major'], '8') >= 0 {
     include chrony
   } else {

puppet/modules/profiles/manifests/foreman.pp
@@ -1,0 +1,14 @@
+# @summary A Foreman application server
+class profiles::foreman {
+  include foreman
+  include foreman::repo
+  include foreman::compute::libvirt
+  include foreman::compute::openstack
+  include foreman::plugin::ansible
+  include foreman::plugin::puppet
+  include foreman::plugin::remote_execution
+
+  puppet::config::main { 'dns_alt_names':
+    value => $foreman::serveraliases,
+  }
+}

puppet/modules/profiles/manifests/puppetserver.pp
@@ -1,0 +1,17 @@
+# @summary A Puppetserver with Foreman integration
+class profiles::puppetserver {
+  include puppet
+  include puppet::server
+
+  include foreman::repo
+  class { 'foreman_proxy':
+    puppet   => true,
+    puppetca => true,
+  }
+  include foreman_proxy::plugin::ansible
+  include foreman_proxy::plugin::remote_execution::ssh
+
+  class { 'deploy':
+    user => $puppet::server_environments_owner,
+  }
+}

commit 235aa5b1c96975e4c8a14f74afd9a4b2a77d00cb

Match deb-node01 in site.pp
Previously it was assumed all nodes have nodeXX but we also have
deb-nodeXX.

Fixes: 6cbe2656dd605d22475cc4dcdf5fc3f84d0d3072

puppet/manifests/site.pp
@@ -17,7 +17,7 @@ node /^foreman\d+\.[a-z]+\.theforeman\.org$/ {
   include profiles::foreman
 }
 
-node /^node\d+\.jenkins\.[a-z]+\.theforeman\.org$/ {
+node /^(deb-)?node\d+\.jenkins\.[a-z]+\.theforeman\.org$/ {
   include profiles::base
   include profiles::jenkins::node
 }