eeef67716eb02b16a6c8e5216da293ddfbc4f0a8
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-01-17 14:56:59 +0100

    Make secretsgit pass puppet-lint and document

--- puppet/modules/secretsgit/manifests/init.pp
+++ puppet/modules/secretsgit/manifests/init.pp
@@ -1,9 +1,21 @@
-class secretsgit(
+# @summary A shared secret storage
+#
+# The shared secret storage creates a group where all users have access
+# to a shared directory for use with gopass. It is then expected that
+# there are several git repositories within that directory. Each
+# repository is created using:
+# `git init --bare --shared=group therepo.git`
+#
+# @param group The group every user should be part of
+# @param path The location to store the git repositories
+# @param users The users who should be part of the group
+#
+# @see https://www.gopass.pw/
+class secretsgit (
   String $group = 'secretsgit',
   Stdlib::Absolutepath $path = '/srv/secretsgit',
   Array[String] $users = [],
 ) {
-
   group { $group:
     ensure => present,
   }
@@ -18,5 +30,4 @@ class secretsgit(
   $users.each |String $user| {
     User<| title == $user |> { groups +> [$group] }
   }
-
 }
