119c09f3f841f7149ac6307af2da9c79b1bb581d
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-01-18 12:54:42 +0100

    Make foreman_debug_rsync puppet-lint clean

--- puppet/modules/foreman_debug_rsync/manifests/config.pp
+++ puppet/modules/foreman_debug_rsync/manifests/config.pp
@@ -1,9 +1,10 @@
+# @summary The configuration of the rsync server
+# @api private
 class foreman_debug_rsync::config {
-
   include 'rsync'
   include 'rsync::server'
 
-  rsync::server::module{ 'debug-incoming':
+  rsync::server::module { 'debug-incoming':
     path            => $foreman_debug_rsync::base,
     require         => File[$foreman_debug_rsync::base],
     comment         => 'Write-only place for foreman-debug',
--- puppet/modules/foreman_debug_rsync/manifests/cron.pp
+++ puppet/modules/foreman_debug_rsync/manifests/cron.pp
@@ -1,4 +1,5 @@
-# Clean out old tarballs
+# @summary Clean out old tarballs
+# @api private
 class foreman_debug_rsync::cron {
   cron { 'remove-old-tarballs':
     command => "/usr/bin/find ${foreman_debug_rsync::base} -type f -mtime +90 -exec rm {} \\;",
