65b2f7cdfc64d94c3b66c5904d1e7e7a295d3c26
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-02-17 12:27:54 +0100

    Use user's own cache directory for g10k
    g10k defaults to /tmp/g10k, which should be considered insecure. This
    uses one that isn't shared with other users.

--- puppet/modules/deploy/files/script.sh
+++ puppet/modules/deploy/files/script.sh
@@ -5,7 +5,7 @@ dir=$(mktemp -d)
 environment=production
 trap 'rm -rf ${dir}' EXIT
 git clone --quiet --depth 1 https://github.com/theforeman/foreman-infra "${dir}/"
-g10k -quiet -puppetfile -puppetfilelocation "${dir}"/puppet/Puppetfile -moduledir "${dir}/puppet/external_modules"
+g10k -quiet -cachedir "$HOME/.cache/g10k" -puppetfile -puppetfilelocation "${dir}/puppet/Puppetfile" -moduledir "${dir}/puppet/external_modules"
 rsync -aqx --delete-after --exclude=.git "${dir}"/puppet/* "/etc/puppetlabs/code/environments/$environment/"
 puppet generate types --environment "$environment" --config /etc/puppetlabs/puppet/puppet.conf
 echo "Deploy complete at $(date)"
