theforeman/foreman-infra

commit ad305563f6b361fee11d43558b65ae920f61e27f

Get koji_certificate from global variables
In the past Foreman's ENC was used, but since
6cbe2656dd605d22475cc4dcdf5fc3f84d0d3072 the koji certificate was no
longer set. Since we don't have a secure way to store secret data in
Hiera, this uses Foreman's ENC again.

puppet/modules/profiles/manifests/jenkins/node.pp
@@ -13,7 +13,7 @@
 # @param packaging
 #   Should the node be able to run packaging jobs
 class profiles::jenkins::node (
-  Optional[String[1]] $koji_certificate = undef,
+  Optional[String[1]] $koji_certificate = getvar('koji_certificate'),
   Integer[0] $swap_size_mb = 8192,
   Boolean $unittests = $facts['os']['family'] == 'RedHat',
   Boolean $packaging = true,

commit 432bcc99757dd0ca3d81cceafd9ea06c028fc2ba

Avoid name collision issue with koji_certificate
This avoids the following error:

    default expression for $koji_certificate tries to illegally access not yet evaluated $koji_certificate

Fixes: ad305563f6b361fee11d43558b65ae920f61e27f

puppet/modules/profiles/manifests/jenkins/node.pp
@@ -13,7 +13,7 @@
 # @param packaging
 #   Should the node be able to run packaging jobs
 class profiles::jenkins::node (
-  Optional[String[1]] $koji_certificate = getvar('koji_certificate'),
+  Optional[String[1]] $koji_certificate = getvar('koji_cert'),
   Integer[0] $swap_size_mb = 8192,
   Boolean $unittests = $facts['os']['family'] == 'RedHat',
   Boolean $packaging = true,