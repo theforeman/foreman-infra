56b6668967ca1fb123df515d66c19d50beacdae0
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-03-31 22:15:43 +0200

    Declare the Puppetserver as the backup receiver

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -25,6 +25,7 @@ node /^(deb-)?node\d+\.jenkins\.[a-z]+\.theforeman\.org$/ {
 node /^puppet\d+\.[a-z]+\.theforeman\.org$/ {
   include profiles::base
   include profiles::puppetserver
+  include profiles::backup::receiver
 }
 
 node /^redmine\d+\.[a-z]+\.theforeman\.org$/ {
