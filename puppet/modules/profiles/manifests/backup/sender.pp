ac09e97cca3daef2c911db75f0db2afd4578baf1
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-03-31 22:13:20 +0200

    Add a backup sender profile

---
+++ puppet/modules/profiles/manifests/backup/sender.pp
@@ -0,0 +1,46 @@
+# @summary The backup sender
+#
+# @param host
+#   The target backup host
+# @param username
+#   The remote username
+class profiles::backup::sender (
+  Sdlib::Host $host = 'backups.theforeman.org',
+  String[1] $username = "backup-${facts['networking']['hostname']}",
+) {
+  # There are no packages in EL7 - EL8+ has it in EPEL
+  if $facts['os']['family'] == 'RedHat' and versioncmp($facts['os']['release']['major'], '7') <= 0 {
+    $params = {
+      install_method  => 'url',
+      package_version => '0.15.1',
+      binary          => '/usr/local/bin/restic',
+    }
+  } else {
+    $params = {}
+  }
+
+  class { 'restic':
+    backup_timer => 'daily',
+    type         => 'sftp',
+    host         => $host,
+    id           => $username,
+    *            => $params,
+  }
+
+  $ssh_dir = "${restic::user_homedir}/.ssh"
+
+  file { $ssh_dir:
+    ensure => directory,
+    owner  => $restic::user,
+    group  => $restic::group,
+    mode   => '0700',
+  }
+
+  file { "${ssh_dir}/id_rsa":
+    ensure  => file,
+    owner   => $restic::user,
+    group   => $restic::group,
+    mode    => '0600',
+    content => ssh::keygen($username),
+  }
+}
