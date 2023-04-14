theforeman/foreman-infra

commit ac09e97cca3daef2c911db75f0db2afd4578baf1

Add a backup sender profile

puppet/modules/profiles/manifests/backup/sender.pp
@@ -1,0 +1,46 @@
+# @summary The backup sender
+#
+# @param host
+#   The target backup host
+# @param username
+#   The remote username
+class profiles::backup::sender (
+  Sdlib::Host $host = 'backups.theforeman.org',
+  String[1] $username = "backup-${facts['networking']['hostname']}",
+) {
+  # There are no packages in EL7 - EL8+ has it in EPEL
+  if $facts['os']['family'] == 'RedHat' and versioncmp($facts['os']['release']['major'], '7') <= 0 {
+    $params = {
+      install_method  => 'url',
+      package_version => '0.15.1',
+      binary          => '/usr/local/bin/restic',
+    }
+  } else {
+    $params = {}
+  }
+
+  class { 'restic':
+    backup_timer => 'daily',
+    type         => 'sftp',
+    host         => $host,
+    id           => $username,
+    *            => $params,
+  }
+
+  $ssh_dir = "${restic::user_homedir}/.ssh"
+
+  file { $ssh_dir:
+    ensure => directory,
+    owner  => $restic::user,
+    group  => $restic::group,
+    mode   => '0700',
+  }
+
+  file { "${ssh_dir}/id_rsa":
+    ensure  => file,
+    owner   => $restic::user,
+    group   => $restic::group,
+    mode    => '0600',
+    content => ssh::keygen($username),
+  }
+}

puppet/Puppetfile
@@ -45,3 +45,7 @@ mod 'theforeman/puppet', '16.5.0'
 mod 'theforeman/puppetserver_foreman', '2.1.0'
 mod 'theforeman/tftp', '8.0.0'
 mod 'treydock/yum_cron', '6.2.0'
+# Maintain a stable branch until https://github.com/syseleven/puppet-restic/pull/24 is merged
+# The PR is on a stable branch which can be force pushed
+# foreman-infra-production is aimed to only roll forward
+mod 'syseleven/restic', :git => 'https://github.com/theforeman/puppet-restic', :branch => 'foreman-infra-production'

commit b14b7cf29c228a3a50fda349737e3221dcd601d6

Fix type name and add test for backup sender class
Fixes: ac09e97cca3daef2c911db75f0db2afd4578baf1

puppet/modules/profiles/manifests/backup/sender.pp
@@ -5,7 +5,7 @@
 # @param username
 #   The remote username
 class profiles::backup::sender (
-  Sdlib::Host $host = 'backups.theforeman.org',
+  Stdlib::Host $host = 'backups.theforeman.org',
   String[1] $username = "backup-${facts['networking']['hostname']}",
 ) {
   # There are no packages in EL7 - EL8+ has it in EPEL

puppet/spec/classes/profiles_backup_sender_spec.rb
@@ -1,0 +1,11 @@
+require 'spec_helper'
+
+describe 'profiles::backup::sender' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile.with_all_deps }
+    end
+  end
+end