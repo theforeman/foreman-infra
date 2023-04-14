theforeman/foreman-infra

commit 357837b9365e4dc1a3da874cbf269a96f92620af

Correct redmine hostname
It currently doesn't have the provider as part of the domain.

puppet/manifests/site.pp
@@ -28,7 +28,7 @@ node /^puppet\d+\.[a-z]+\.theforeman\.org$/ {
   include profiles::backup::receiver
 }
 
-node /^redmine\d+\.[a-z]+\.theforeman\.org$/ {
+node /^redmine\d+\.([a-z]+)?\.theforeman\.org$/ {
   include profiles::base
   include profiles::redmine
   include exim # TODO

commit e67914570f58df7bce93e36ebfa73d43382f23fd

Correct domain regex
Fixes: 357837b9365e4dc1a3da874cbf269a96f92620af

puppet/manifests/site.pp
@@ -28,7 +28,7 @@ node /^puppet\d+\.[a-z]+\.theforeman\.org$/ {
   include profiles::backup::receiver
 }
 
-node /^redmine\d+\.([a-z]+)?\.theforeman\.org$/ {
+node /^redmine\d+\.([a-z]+\.)?theforeman\.org$/ {
   include profiles::base
   include profiles::redmine
   include exim # TODO