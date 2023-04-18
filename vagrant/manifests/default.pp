297c08c49d4effc59714fa67788faa3981413d91
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-04-18 16:24:26 +0200

    Move Vagrant node configuration to Hiera
    This aligns it closer to the real nodes.

--- vagrant/manifests/default.pp
+++ vagrant/manifests/default.pp
@@ -1,44 +1,21 @@
 node /^jenkins-master.*/ {
-  class { 'profiles::jenkins::controller':
-    hostname                     => $facts['networking']['fqdn'],
-    https                        => false,
-    jenkins_job_builder          => true,
-    jenkins_job_builder_username => 'admin',
-    jenkins_job_builder_password => 'changeme',
-  }
+  include profiles::jenkins::controller
 }
 
-node /^jenkins-node.*/ {
+node /^jenkins-(deb-)?node.*/ {
   sudo::conf { 'vagrant':
     content => 'vagrant ALL=(ALL) NOPASSWD: ALL',
   }
 
-  class { 'profiles::jenkins::node':
-    swap_size_mb => 0,
-  }
-}
-
-node /^jenkins-deb-node.*/ {
-  sudo::conf { 'vagrant':
-    content => 'vagrant ALL=(ALL) NOPASSWD: ALL',
-  }
-
-  class { 'profiles::jenkins::node':
-    swap_size_mb => 0,
-    unittests    => false,
-  }
+  include profiles::jenkins::node
 }
 
 node /^web.*/ {
-  class { 'profiles::web':
-    https => false,
-  }
+  include profiles::web
 }
 
 node /^backup.*/ {
-  class { 'profiles::backup::receiver':
-    targets => ['redmine', 'jenkins-master'],
-  }
+  include profiles::backup::receiver
 }
 
 node /^redmine.*/ {
