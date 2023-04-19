7eeedf45c433e3f74ed5154942637bcf38ece41c
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-04-19 19:46:58 +0200

    Move Jenkins controller definition into the profile
    This inlines the jenkins_master class into the controller profile. The
    list of plugins is stored in Hiera.

--- puppet/modules/profiles/manifests/jenkins/controller.pp
+++ puppet/modules/profiles/manifests/jenkins/controller.pp
@@ -10,14 +10,36 @@
 #   The username Jenkins Job Builder should use if enabled
 # @param jenkins_job_builder_password
 #   The password Jenkins Job Builder should use if enabled
+# @param packages
+#   The (java) packages to install. OpenJDK Devel is needed for jar unpacking support.
+# @param plugins
+#   The list of plugins. Get the list by going to /script on jenkins and run:
+#     Jenkins.instance.pluginManager.plugins.toArray().sort { plugin -> plugin.getShortName()}.each {
+#       plugin -> println ("    '${plugin.getShortName()}' => {},")
+#     }
 class profiles::jenkins::controller (
   Stdlib::Fqdn $hostname = 'ci.theforeman.org',
   Boolean $https = true,
   Boolean $jenkins_job_builder = true,
   Optional[String] $jenkins_job_builder_username = undef,
   Optional[String] $jenkins_job_builder_password = undef,
+  Array[String[1]] $packages = ['java-11-openjdk-headless', 'java-11-openjdk-devel', 'fontconfig'],
+  Array[String[1]] $plugins = [],
 ) {
-  include jenkins_master
+  ensure_packages($packages)
+
+  class { 'jenkins':
+    install_java    => false,
+    lts             => true,
+    default_plugins => [],
+    plugin_hash     => $plugins.reduce({}) |Hash $memo, String $plugin| { $memo + { $plugin => {} } },
+    config_hash     => {
+      'JENKINS_JAVA_OPTIONS' => {
+        'value' => '-Djava.awt.headless=true -Djenkins.install.runSetupWizard=false -Xms2048m -Xmx2048m',
+      },
+    },
+    require         => Package[$packages],
+  }
 
   class { 'web::base':
     letsencrypt => $https,
@@ -37,7 +59,7 @@ class profiles::jenkins::controller (
           password => $jenkins_job_builder_password,
         },
       },
-      require => [Class['jenkins_master', 'web::jenkins']],
+      require => [Class['jenkins', 'web::jenkins']],
     }
   }
 }
