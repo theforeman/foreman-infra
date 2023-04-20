3a751e91ed960ccc5efd8e4ab6ff8d0934008754
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-04-20 13:22:02 +0200

    Load Hiera during tests

--- puppet/spec/spec_helper.rb
+++ puppet/spec/spec_helper.rb
@@ -27,6 +27,7 @@ RSpec.configure do |c|
   c.module_path     = [File.join(base_dir, 'modules'), File.join(base_dir, 'external_modules'), File.join(base_dir, 'test_modules')].join(File::PATH_SEPARATOR)
   c.manifest_dir    = File.join(base_dir, 'manifests')
   c.manifest        = File.join(base_dir, 'manifests', 'site.pp')
+  c.hiera_config    = File.join(base_dir, 'hiera.yaml')
   c.environmentpath = base_dir
   c.strict_variables = true
 
