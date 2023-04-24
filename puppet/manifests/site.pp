theforeman/foreman-infra

commit 2bbdb33de81571af18e3024fba036aa835ba8b57

Add a profile for discourse
This currently doesn't manage Docker and the app checkout itself, but
at least sets up the backup.

puppet/manifests/site.pp
@@ -9,7 +9,7 @@ node /^controller\d+\.[a-z]+\.theforeman\.org$/ {
 
 node /^discourse\d+\.[a-z]+\.theforeman\.org$/ {
   include profiles::base
-  # TODO profiles::discourse
+  include profiles::discourse
 }
 
 node /^foreman\d+\.[a-z]+\.theforeman\.org$/ {

puppet/modules/profiles/manifests/discourse.pp
@@ -1,0 +1,14 @@
+# @summary Manage a Discourse server
+# @see https://github.com/discourse/discourse/blob/main/docs/INSTALL-cloud.md
+class profiles::discourse {
+  $root = '/var/discourse'
+  # TODO: manage docker
+  # TODO: vcsrepo https://github.com/discourse/discourse_docker.git on $root
+
+  include profiles::backup::sender
+
+  restic::repository { 'discourse':
+    backup_cap_dac_read_search => true,
+    backup_path                => ["${root}/containers", "${root}/shared/*/backups"],
+  }
+}

commit 0e68fdb9fdd8afe31677d6d4c59d518e61e8ad48

Match discourse on domain without location
The discourse server (like Redmine) doesn't have a location in its
domain name, so it wasn't matching. Prior to
2bbdb33de81571af18e3024fba036aa835ba8b57 it was equal to base, so it
wasn't noticed.

Fixes: 2bbdb33de81571af18e3024fba036aa835ba8b57

puppet/manifests/site.pp
@@ -7,7 +7,7 @@ node /^controller\d+\.[a-z]+\.theforeman\.org$/ {
   include profiles::jenkins::controller
 }
 
-node /^discourse\d+\.[a-z]+\.theforeman\.org$/ {
+node /^discourse\d+\.([a-z]+\.)?theforeman\.org$/ {
   include profiles::base
   include profiles::discourse
 }