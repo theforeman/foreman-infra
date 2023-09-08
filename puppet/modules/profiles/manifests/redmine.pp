b3943722c1aaeeb5d3c140d7f58d9e450007afcd
Evgeni Golov <evgeni@golov.de>
2023-09-08 13:36:09 +0200

    run the db backup via restic, not as a separate cron job

--- puppet/modules/profiles/manifests/redmine.pp
+++ puppet/modules/profiles/manifests/redmine.pp
@@ -6,10 +6,17 @@ class profiles::redmine (
   $backup_path = '/var/lib/redmine'
 
   include profiles::backup::sender
+  include redmine
+
+  include sudo
+  sudo::conf { "sudo-puppet-backup-redmine":
+    content => "restic ALL=($redmine::username) NOPASSWD: /usr/bin/pg_dump",
+  }
 
   restic::repository { 'redmine':
     backup_cap_dac_read_search => true,
     backup_path                => $backup_path,
     backup_flags               => ['--exclude', "${backup_path}/git"],
+    backup_pre_cmd             => ["/usr/bin/sudo -u $redmine::username pg_dump --file=${backup_path}/redmine.backup.sql $redmine::db_name"],
   }
 }
