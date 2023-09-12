ebf8a3ce8c4ed92c30e1590bca7fd6fb37fde961
Evgeni Golov <evgeni@golov.de>
2023-09-12 19:35:38 +0200

    backup foreman
    using `+/usr/bin/foreman-maintain` makes systemd execute the binary as
    root, not as the user the main execution happens as
    
    See https://www.freedesktop.org/software/systemd/man/systemd.service.html#Command%20lines

--- puppet/modules/profiles/manifests/foreman.pp
+++ puppet/modules/profiles/manifests/foreman.pp
@@ -11,4 +11,32 @@ class profiles::foreman {
   puppet::config::main { 'dns_alt_names':
     value => $foreman::serveraliases,
   }
+
+  package {'rubygem-foreman_maintain':
+    ensure => present,
+  }
+
+  $backup_base_path = '/var/backups'
+  $backup_path = "${backup_base_path}/foreman"
+
+  file { $backup_base_path:
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+  file { $backup_path:
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0750',
+  }
+
+  include profiles::backup::sender
+
+  restic::repository { 'foreman':
+    backup_cap_dac_read_search => true,
+    backup_path                => $backup_path,
+    backup_pre_cmd             => ["+/usr/bin/foreman-maintain backup online --assumeyes --preserve-directory ${backup_path}"],
+  }
 }
