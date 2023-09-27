6b4697784891e586e79154522d0d7a157ec0e030
Evgeni Golov <evgeni@golov.de>
2023-09-27 09:02:25 +0200

    allow setting ensure=absent in backup::receiver

--- puppet/modules/profiles/manifests/backup/receiver/target.pp
+++ puppet/modules/profiles/manifests/backup/receiver/target.pp
@@ -5,15 +5,18 @@
 #
 # @param username
 #   The remote username
+# @param ensure
+#   The backup receiver target state to ensure.
 define profiles::backup::receiver::target (
   String[1] $username = "backup-${title}",
+  Enum['present', 'absent'] $ensure = present,
 ) {
   include profiles::backup::receiver
 
   $homedir = "${profiles::backup::receiver::directory}/${title}"
 
   user { $username:
-    ensure         => present,
+    ensure         => $ensure,
     gid            => $profiles::backup::receiver::group,
     home           => $homedir,
     managehome     => true,
@@ -23,22 +26,24 @@ define profiles::backup::receiver::target (
 
   # Ensure authorized keys file for proper SELinux labels
   file { "${homedir}/.ssh":
-    ensure  => directory,
+    ensure  => bool2str($ensure == present, 'directory', 'absent'),
     owner   => $username,
     mode    => '0700',
+    force   => $ensure == absent,
     seltype => 'ssh_home_t',
   }
 
   file { "${homedir}/.ssh/authorized_keys":
-    ensure  => file,
+    ensure  => bool2str($ensure == present, 'file', 'absent'),
     owner   => $username,
     mode    => '0600',
     seltype => 'ssh_home_t',
   }
 
   ssh_authorized_key { "${username} managed by Puppet":
-    user => $username,
-    key  => ssh::keygen("backup-${title}", true),
-    type => 'ssh-rsa',
+    ensure => $ensure,
+    user   => $username,
+    key    => ssh::keygen("backup-${title}", true),
+    type   => 'ssh-rsa',
   }
 }
