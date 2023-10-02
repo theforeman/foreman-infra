dca22b2046ac5e36dfa7237f5c9d6ca6ee0bdcbf
Evgeni Golov <evgeni@golov.de>
2023-10-02 11:22:47 +0200

    stop testing Debian 9

--- puppet/spec/spec_helper.rb
+++ puppet/spec/spec_helper.rb
@@ -15,7 +15,7 @@ def on_supported_os(opts = {})
     },
     {
       'operatingsystem'        => 'Debian',
-      'operatingsystemrelease' => ['9', '11'],
+      'operatingsystemrelease' => ['11'],
     },
   ]
   super(opts)
