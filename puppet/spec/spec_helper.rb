95f30247a1413f1d56a104cda972eb7d9788ae99
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-10-12 12:21:35 +0200

    Compatibility with rspec-puppet 4
    rspec-puppet 4 has dropped these settings because they only affected
    Puppet < 6.

--- puppet/spec/spec_helper.rb
+++ puppet/spec/spec_helper.rb
@@ -25,13 +25,8 @@ base_dir = File.dirname(File.expand_path(__dir__))
 
 RSpec.configure do |c|
   c.module_path     = [File.join(base_dir, 'modules'), File.join(base_dir, 'external_modules'), File.join(base_dir, 'test_modules')].join(File::PATH_SEPARATOR)
-  c.manifest_dir    = File.join(base_dir, 'manifests')
   c.manifest        = File.join(base_dir, 'manifests', 'site.pp')
   c.hiera_config    = File.join(base_dir, 'hiera.yaml')
   c.environmentpath = base_dir
   c.strict_variables = true
-
-  # Needed for theforeman-puppet
-  c.trusted_node_data = true
-  c.trusted_server_facts = true
 end
