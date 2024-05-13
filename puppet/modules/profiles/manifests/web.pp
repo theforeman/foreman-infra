e90a6b9f76c3b7207467e9bb421a2f35770387f6
Griffin-Sullivan <gsulliva@redhat.com>
2024-05-13 10:32:16 -0400

    Update  to 3.12

--- puppet/modules/profiles/manifests/web.pp
+++ puppet/modules/profiles/manifests/web.pp
@@ -19,7 +19,7 @@
 #   server load
 class profiles::web (
   String[1] $stable = '3.10',
-  String[1] $next = '3.11',
+  String[1] $next = '3.12',
   Hash[String, Hash] $debugs_htpasswds = {},
   Boolean $https = true,
   Integer[0] $rsync_max_connections = 10,
