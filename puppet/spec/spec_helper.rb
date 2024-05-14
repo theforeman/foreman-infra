e0fea67defd0302e142f2ea6404561568f7d802f
Evgeni Golov <evgeni@golov.de>
2024-05-14 09:56:46 +0200

    drop rvm module

--- puppet/spec/spec_helper.rb
+++ puppet/spec/spec_helper.rb
@@ -3,7 +3,6 @@ require 'rspec-puppet-facts'
 include RspecPuppetFacts
 
 add_custom_fact :root_home, '/root'
-add_custom_fact :rvm_installed, false
 add_custom_fact :service_provider, 'systemd'
 add_custom_fact :sudoversion, '1.8.23'
 
