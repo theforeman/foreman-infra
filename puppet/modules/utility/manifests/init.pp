c46aa9c135f6793316fcd386e358cf7af7fc9a80
Evgeni Golov <evgeni@golov.de>
2024-07-09 10:05:01 +0200

    drop EL8 support from our codebase

--- puppet/modules/utility/manifests/init.pp
+++ puppet/modules/utility/manifests/init.pp
@@ -7,9 +7,7 @@ class utility($sysadmins = ['/dev/null']) {
 
   stdlib::ensure_packages([$vim])
 
-  unless $facts['os']['family'] == 'RedHat' and $facts['os']['release']['major'] == '8' {
-    stdlib::ensure_packages(['htop', 'iftop', 'screen'])
-  }
+  stdlib::ensure_packages(['htop', 'iftop', 'screen'])
 
   stdlib::ensure_packages(['rsync'])
 
