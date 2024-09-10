theforeman/foreman-infra

commit e2a182031f9f448f01f7fa262915936815d45108

use mod_expires everywhere instead of setting headers ourself

puppet/modules/freight/manifests/user.pp
@@ -64,8 +64,9 @@ define freight::user (
       options => ['Indexes', 'FollowSymLinks'],
     },
     {
-      path    => "${webdir}/dists",
-      headers => 'Set Cache-Control "public, max-age=120"',
+      path            => "${webdir}/dists",
+      expires_active  => 'on',
+      expires_default => '"access plus 2 minutes"',
     },
     {
       path    => "${webdir}/dists/*/.refs/",
@@ -73,7 +74,8 @@ define freight::user (
     },
     {
       path    => "${webdir}/pool",
-      headers => 'Set Cache-Control "public, max-age=2592000"',
+      expires_active  => 'on',
+      expires_default => '"access plus 30 days"',
     },
   ]
 

puppet/modules/web/manifests/init.pp
@@ -21,7 +21,6 @@ class web(
 ) {
   include web::base
   include rsync::server
-  include apache::mod::headers
 
   letsencrypt::certonly { 'theforeman.org':
     plugin        => 'webroot',
@@ -100,15 +99,16 @@ class web(
       expires_default => '"access plus 2 minutes"',
     },
     {
-      path     => '.+\.(bz2|gz|rpm|xz)$',
-      provider => 'filesmatch',
-      headers  => 'Set Cache-Control "public, max-age=2592000"',
+      path            => '.+\.(bz2|gz|rpm|xz)$',
+      provider        => 'filesmatch',
+      expires_active  => 'on',
+      expires_default => '"access plus 30 days"',
     },
     {
       path            => 'repomd.xml',
       provider        => 'files',
       expires_active  => 'on',
-      expires_default => '"access plus 0 seconds"',
+      expires_default => '"access plus 2 minutes"',
     },
   ]
 
@@ -188,9 +188,10 @@ class web(
       options => ['Indexes', 'FollowSymLinks', 'MultiViews'],
     },
     {
-      path     => '.+\.(bz2|csv|gem|gz|img|iso|iso-img|iso-vmlinuz|pdf|tar|webm|rpm|deb)$',
-      provider => 'filesmatch',
-      headers  => 'Set Cache-Control "public, max-age=2592000"',
+      path            => '.+\.(bz2|csv|gem|gz|img|iso|iso-img|iso-vmlinuz|pdf|tar|webm|rpm|deb)$',
+      provider        => 'filesmatch',
+      expires_active  => 'on',
+      expires_default => '"access plus 30 days"',
     },
   ]
 

commit 4ff549a664cc5826da7d02eb05fce9eda1593e16

explicitly load expires module in freight vhosts
Fixes: e2a182031f9f448f01f7fa262915936815d45108

puppet/modules/freight/manifests/user.pp
@@ -58,8 +58,9 @@ define freight::user (
     },
   ]
 
-  # locations doesn't autorequire the headers module
-  include apache::mod::headers
+  # vhosts don't autorequire the expires module
+  # https://github.com/puppetlabs/puppetlabs-apache/pull/2559
+  include apache::mod::expires
 
   web::vhost { $vhost:
     docroot       => $webdir,