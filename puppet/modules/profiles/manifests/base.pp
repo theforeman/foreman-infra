4bc7e43a52a8f5a4f96478c555b6d18fe520238b
Evgeni Golov <evgeni@golov.de>
2025-01-06 09:53:59 +0100

    fix /etc/localtime to be a symlink

--- puppet/modules/profiles/manifests/base.pp
+++ puppet/modules/profiles/manifests/base.pp
@@ -20,4 +20,11 @@ class profiles::base (
   class { 'foreman_proxy::plugin::remote_execution::ssh_user':
     manage_user => true,
   }
+
+  # Fix /etc/localtime to be a symlink
+  # claranet/timezone broke this
+  file { '/etc/localtime':
+    ensure => 'link',
+    target => '/usr/share/zoneinfo/UTC',
+  }
 }
