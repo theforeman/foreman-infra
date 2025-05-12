theforeman/foreman-infra

commit a4cda8f34f5b9a8e00de5a6f32f96fe6981f86fd

add restic exporter

puppet/modules/profiles/manifests/base/monitoring.pp
@@ -29,6 +29,18 @@ class profiles::base::monitoring (
     stdlib::ensure_packages(['prometheus-node-exporter-collectors'])
   }
 
+  file { '/usr/local/bin/restic-prometheus-exporter':
+    mode    => '0755',
+    owner   => 'root',
+    group   => 'root',
+    content => file("${module_name}/base/restic-prometheus-exporter.py"),
+  }
+
+  file { '/var/lib/prometheus/node-exporter/':
+    ensure => directory,
+    mode   => '0770',
+  }
+
   $prom_context = {
     'prometheus_username' => $prometheus_username,
     'prometheus_password' => $prometheus_password,

puppet/modules/profiles/files/base/restic-prometheus-exporter.py
@@ -1,0 +1,47 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import subprocess
+import sys
+
+import dateutil.parser
+
+from prometheus_client import CollectorRegistry, Gauge, generate_latest
+
+def main():
+    restic_bin = os.environ.get('RESTIC_BIN', 'restic')
+    restic_repository = os.environ.get('RESTIC_REPOSITORY', 'unknown')
+
+    cmd = [restic_bin, 'snapshots', '--latest', '1', '--json']
+
+    try:
+        restic_result = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        print(f'Failed to run {" ".join(cmd)}: {e.output.decode()}', file=sys.stderr)
+        sys.exit(1)
+    except IOError as e:
+        print(f'Failed to run {" ".join(cmd)}: {e}', file=sys.stderr)
+        sys.exit(1)
+
+    snapshots = json.loads(restic_result)
+
+    registry = CollectorRegistry()
+    label_names = ['repository', 'hostname', 'paths']
+
+    timestamp = Gauge('restic_snapshot_timestamp_seconds', "Restic snapshot timestamp", label_names, registry=registry)
+    size = Gauge('restic_snapshot_size_bytes', "Restic snapshot size", label_names, registry=registry)
+
+    for snapshot in snapshots:
+        labels = [restic_repository, snapshot['hostname'], ','.join(snapshot['paths'])]
+
+        timestamp.labels(*labels).set(dateutil.parser.parse(snapshot['time']).timestamp())
+
+        # summary is a restic 0.17+ feature
+        if summary := snapshot.get('summary') is not None:
+            size.labels(*labels).set(summary.get('total_bytes_processed'))
+
+    print(generate_latest(registry).decode(), end='')
+
+if __name__ == "__main__":
+    main()

puppet/modules/profiles/manifests/backup/sender.pp
@@ -38,4 +38,6 @@ class profiles::backup::sender (
     type   => $ssh_key_type,
     key    => $ssh_key,
   }
+
+  User<| title == $restic::user |> { groups +> ['prometheus'] }
 }

puppet/modules/profiles/manifests/discourse.pp
@@ -24,5 +24,8 @@ class profiles::discourse {
     backup_cap_dac_read_search => true,
     backup_path                => $backup_path,
     backup_pre_cmd             => ['+/usr/bin/docker exec app discourse backup'],
+    backup_post_cmd            => [
+      '-/bin/bash -c "/usr/local/bin/restic-prometheus-exporter | sponge /var/lib/prometheus/node-exporter/restic.prom"',
+    ],
   }
 }

puppet/modules/profiles/manifests/foreman.pp
@@ -40,5 +40,8 @@ class profiles::foreman {
     backup_cap_dac_read_search => true,
     backup_path                => $backup_path,
     backup_pre_cmd             => ["+/usr/bin/foreman-maintain backup online --assumeyes --preserve-directory ${backup_path}"],
+    backup_post_cmd            => [
+      '-/bin/bash -c "/usr/local/bin/restic-prometheus-exporter | sponge /var/lib/prometheus/node-exporter/restic.prom"',
+    ],
   }
 }

puppet/modules/profiles/manifests/jenkins/controller.pp
@@ -63,6 +63,9 @@ class profiles::jenkins::controller (
       '--exclude', "${backup_path}/jobs/*/builds",
       '--exclude', "${backup_path}/plugins",
     ],
+    backup_post_cmd            => [
+      '-/bin/bash -c "/usr/local/bin/restic-prometheus-exporter | sponge /var/lib/prometheus/node-exporter/restic.prom"',
+    ],
   }
 
   if $jenkins_job_builder {

puppet/modules/profiles/manifests/puppetserver.pp
@@ -25,5 +25,8 @@ class profiles::puppetserver {
       '/opt/puppetlabs/server/data/puppetserver/ssh',
       '/opt/puppetlabs/server/data/puppetserver/foreman_cache_data',
     ],
+    backup_post_cmd            => [
+      '-/bin/bash -c "/usr/local/bin/restic-prometheus-exporter | sponge /var/lib/prometheus/node-exporter/restic.prom"',
+    ],
   }
 }

puppet/modules/profiles/manifests/redmine.pp
@@ -45,5 +45,8 @@ class profiles::redmine (
     backup_path                => $backup_path,
     backup_flags               => ['--exclude', "${backup_path}/git"],
     backup_pre_cmd             => ["/usr/bin/pg_dump --file=${backup_db_path}/redmine.sql ${redmine::db_name}"],
+    backup_post_cmd            => [
+      '-/bin/bash -c "/usr/local/bin/restic-prometheus-exporter | sponge /var/lib/prometheus/node-exporter/restic.prom"',
+    ],
   }
 }

commit b02cc5853b01082c39b01d539173785b336bc3e0

correct permissions on /var/lib/prometheus/node-exporter
Fixes: a4cda8f34f5b9a8e00de5a6f32f96fe6981f86fd

puppet/modules/profiles/manifests/base/monitoring.pp
@@ -38,7 +38,9 @@ class profiles::base::monitoring (
 
   file { '/var/lib/prometheus/node-exporter/':
     ensure => directory,
-    mode   => '0770',
+    owner  => 'prometheus',
+    group  => 'prometheus',
+    mode   => '0775',
   }
 
   $prom_context = {