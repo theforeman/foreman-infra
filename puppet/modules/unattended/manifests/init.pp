7d7783ea387cfd3dd13956ebde0ef1c9d04d6e48
Evgeni Golov <evgeni@golov.de>
2025-07-15 08:59:30 +0200

    configure reboot in unattended class
    also auto-reboot backup and website hosts

--- puppet/modules/unattended/manifests/init.pp
+++ puppet/modules/unattended/manifests/init.pp
@@ -16,10 +16,23 @@ class unattended {
   }
 
   if $facts['os']['family'] == 'RedHat' {
+    if $trusted['certname'] =~ /^node\d+\.jenkins\.[a-z]+\.theforeman\.org$/ {
+      $reboot = 'when-needed'
+      $reboot_command = '/usr/local/sbin/reboot-jenkins-node'
+    } elsif $trusted['certname'] =~ /(backup|website)\d+\.[a-z]+\.theforeman\.org$/ {
+      $reboot = 'when-needed'
+      $reboot_command = "shutdown -r +5 'Rebooting after applying package updates'"
+    } else {
+      $reboot = 'never'
+      $reboot_command = "shutdown -r +5 'Rebooting after applying package updates'"
+    }
+
     class { 'yum_cron':
       apply_updates    => true,
       mailto           => 'sysadmins',
       exclude_packages => ['java*', 'jenkins'],
+      reboot           => $reboot,
+      reboot_command   => $reboot_command,
     }
   }
 }
