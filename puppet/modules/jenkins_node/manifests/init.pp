ed0d73730b90ae65e01f38a490c4c8a2b7f21e67
Evgeni Golov <evgeni@golov.de>
2025-07-15 08:59:30 +0200

    configure reboot in unattended class
    also auto-reboot backup and website hosts

--- puppet/modules/jenkins_node/manifests/init.pp
+++ puppet/modules/jenkins_node/manifests/init.pp
@@ -145,14 +145,4 @@ class jenkins_node (
     mode    => '0755',
     content => file("${module_name}/reboot-jenkins-node"),
   }
-
-  if $facts['os']['family'] == 'RedHat' {
-    # https://github.com/treydock/puppet-yum_cron/pull/51
-    dnf_automatic_config { 'commands/reboot':
-      value => 'when-needed',
-    }
-    dnf_automatic_config { 'commands/reboot_command':
-      value => '/usr/local/sbin/reboot-jenkins-node',
-    }
-  }
 }
