abf348c230f55a22cf1bc2aa04389529d33308a4
Evgeni Golov <evgeni@golov.de>
2025-07-21 08:18:18 +0200

    drop old F66-add-nodesource-repos hook

--- puppet/spec/classes/jenkins_node_spec.rb
+++ puppet/spec/classes/jenkins_node_spec.rb
@@ -27,15 +27,12 @@ describe 'jenkins_node' do
           it { is_expected.to contain_file('/etc/pbuilder/bookworm64/hooks/F60addforemanrepo').with_ensure('present') }
           it { is_expected.to contain_file('/etc/pbuilder/bookworm64/hooks/F65-add-backport-repos').with_ensure('absent') }
           it { is_expected.to contain_file('/etc/pbuilder/bookworm64/hooks/F66-add-nodesource-nodistro-repos').with_ensure('absent') }
-          it { is_expected.to contain_file('/etc/pbuilder/bookworm64/hooks/F66-add-nodesource-repos').with_ensure('absent') }
           it { is_expected.to contain_file('/etc/pbuilder/bookworm64/hooks/F67-add-puppet-repos').with_ensure('present') }
           it { is_expected.to contain_file('/etc/pbuilder/bookworm64/hooks/F70aptupdate').with_ensure('present') }
           it { is_expected.to contain_file('/etc/pbuilder/bookworm64/hooks/F99printrepos').with_ensure('present') }
           it { is_expected.to contain_file('/etc/pbuilder/bullseye64/hooks/F66-add-nodesource-nodistro-repos').with_ensure('absent') }
-          it { is_expected.to contain_file('/etc/pbuilder/bullseye64/hooks/F66-add-nodesource-repos').with_ensure('absent') }
           it { is_expected.to contain_file('/etc/pbuilder/jammy64/hooks/A10nozstd').with_ensure('present') }
           it { is_expected.to contain_file('/etc/pbuilder/jammy64/hooks/F66-add-nodesource-nodistro-repos').with_ensure('present') }
-          it { is_expected.to contain_file('/etc/pbuilder/jammy64/hooks/F66-add-nodesource-repos').with_ensure('absent') }
         end
       end
 
