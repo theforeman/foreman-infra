7698e1ff1860f84f82b4a7e343625479d1eca461
Evgeni Golov <evgeni@golov.de>
2025-09-17 14:37:35 +0200

    add module to configure openvox repo
    also purges puppet repo

---
+++ puppet/modules/openvox_repo/manifests/apt.pp
@@ -0,0 +1,17 @@
+class openvox_repo::apt() {
+  include apt
+
+  $os_name = downcase($facts['os']['name'])
+  $release = "${os_name}${facts['os']['release']['major']}"
+
+  apt::source { 'openvox':
+    comment  => "OpenVox ${openvox_repo::release} ${release} Repository",
+    location => 'https://apt.voxpupuli.org',
+    release  => $release,
+    repos    => "openvox${openvox_repo::release}",
+    key      => {
+      'name'   => 'openvox-keyring.gpg',
+      'source' => 'https://apt.voxpupuli.org/openvox-keyring.gpg',
+    },
+  }
+}
