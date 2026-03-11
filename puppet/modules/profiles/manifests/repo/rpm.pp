325d5178ee307b0b28f4b273289208626505d1ed
Evgeni Golov <evgeni@golov.de>
2026-03-11 14:06:59 +0100

    clean up last remains of rpm.tfm.o

--- puppet/modules/profiles/manifests/repo/rpm.pp
+++ puppet/modules/profiles/manifests/repo/rpm.pp
@@ -14,18 +14,11 @@ class profiles::repo::rpm (
 
   contain web::vhost::stagingyum
 
-  class { 'web::vhost::rpm':
-    stable_foreman => $stable_foreman,
-  }
-  contain web::vhost::rpm
-
-  contain web::vhost::stagingrpm
-
   include profiles::backup::sender
 
   restic::repository { 'repo_rpm':
     backup_cap_dac_read_search => true,
-    backup_path                => [$web::vhost::rpm::rpm_directory, $web::vhost::yum::yum_directory],
+    backup_path                => [$web::vhost::yum::yum_directory],
     backup_post_cmd            => [
       '-/bin/bash -c "/usr/local/bin/restic-prometheus-exporter | sponge /var/lib/prometheus/node-exporter/restic.prom"',
     ],
